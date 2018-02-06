8468d4e9ded6391b0743d6866fe6dd1a15c3be20
Nikolay Volik <nvolik@gbif.org>
2018-02-06 14:36:57 +0100

    Changes after megre with master

--- demo/src/main/java/org/gbif/pipelines/demo/hdfs/DwcaToHdfsPipeline.java
+++ demo/src/main/java/org/gbif/pipelines/demo/hdfs/DwcaToHdfsPipeline.java
@@ -1,11 +1,9 @@
 package org.gbif.pipelines.demo.hdfs;
 
-import org.gbif.pipelines.common.beam.BeamFunctions;
 import org.gbif.pipelines.common.beam.Coders;
 import org.gbif.pipelines.common.beam.DwCAIO;
 import org.gbif.pipelines.core.config.DataProcessingPipelineOptions;
 import org.gbif.pipelines.core.config.TargetPath;
-import org.gbif.pipelines.core.functions.FunctionFactory;
 import org.gbif.pipelines.demo.utils.PipelineUtils;
 import org.gbif.pipelines.io.avro.ExtendedRecord;
 import org.gbif.pipelines.io.avro.UntypedOccurrence;
@@ -17,12 +15,14 @@ import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.coders.AvroCoder;
 import org.apache.beam.sdk.io.AvroIO;
 import org.apache.beam.sdk.io.FileSystems;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.hadoop.conf.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.gbif.pipelines.core.functions.FunctionFactory.untypedOccurrenceBuilder;
+import static org.gbif.pipelines.core.functions.descriptor.CustomTypeDescriptors.untypedOccurrencies;
 
 /**
  * Pipeline that transforms a Dwca file into an Avro file and writes it to HDFS.
@@ -62,12 +62,11 @@ public class DwcaToHdfsPipeline {
       pipeline.apply("Read from Darwin Core Archive", DwCAIO.Read.withPaths(options.getInputFile(), tmpDirDwca));
 
     // Convert the ExtendedRecord into an UntypedOccurrence record
-    DoFn<ExtendedRecord, UntypedOccurrence> fn = BeamFunctions.beamify(FunctionFactory.untypedOccurrenceBuilder());
-
     // TODO: Explore the generics as to whßy the coder registry does not find it and we need to set the coder explicitly
-    PCollection<UntypedOccurrence> verbatimRecords =
-      rawRecords.apply("Convert the objects into untyped DwC style records", ParDo.of(fn))
-        .setCoder(AvroCoder.of(UntypedOccurrence.class));
+    PCollection<UntypedOccurrence> verbatimRecords = rawRecords.apply(
+      "Convert the objects into untyped DwC style records",
+      MapElements.into(untypedOccurrencies()).via(untypedOccurrenceBuilder()::apply))
+      .setCoder(AvroCoder.of(UntypedOccurrence.class));
 
     verbatimRecords.apply("Write Avro files",
                           AvroIO.write(UntypedOccurrence.class)
--- demo/src/main/java/org/gbif/pipelines/demo/hdfs/DwcaToHdfsTestingPipeline.java
+++ demo/src/main/java/org/gbif/pipelines/demo/hdfs/DwcaToHdfsTestingPipeline.java
@@ -1,27 +1,27 @@
 package org.gbif.pipelines.demo.hdfs;
 
-import org.gbif.pipelines.common.beam.BeamFunctions;
 import org.gbif.pipelines.common.beam.Coders;
 import org.gbif.pipelines.common.beam.DwCAIO;
 import org.gbif.pipelines.core.config.DataProcessingPipelineOptions;
 import org.gbif.pipelines.core.config.TargetPath;
-import org.gbif.pipelines.core.functions.FunctionFactory;
 import org.gbif.pipelines.io.avro.ExtendedRecord;
 import org.gbif.pipelines.io.avro.UntypedOccurrence;
 
 import java.io.File;
-import java.util.Optional;
+import java.util.Objects;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.coders.AvroCoder;
 import org.apache.beam.sdk.io.AvroIO;
 import org.apache.beam.sdk.io.FileSystems;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.values.PCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.gbif.pipelines.core.functions.FunctionFactory.*;
+import static org.gbif.pipelines.core.functions.descriptor.CustomTypeDescriptors.*;
 
 /**
  * Pipeline that writes an Avro file to HDFS.
@@ -39,7 +39,7 @@ public class DwcaToHdfsTestingPipeline {
   }
 
   public void createAndRunPipeline() {
-    Optional.ofNullable(options).orElseThrow(() -> new IllegalArgumentException("Pipeline options cannot be null"));
+    Objects.requireNonNull(options, "Pipeline options cannot be null");
 
     String targetPath = TargetPath.getFullPath(options.getDefaultTargetDirectory(), options.getDatasetId());
 
@@ -58,12 +58,11 @@ public class DwcaToHdfsTestingPipeline {
       pipeline.apply("Read from Darwin Core Archive", DwCAIO.Read.withPaths(options.getInputFile(), tmpDirDwca));
 
     // Convert the ExtendedRecord into an UntypedOccurrence record
-    DoFn<ExtendedRecord, UntypedOccurrence> fn = BeamFunctions.beamify(FunctionFactory.untypedOccurrenceBuilder());
-
     // TODO: Explore the generics as to whßy the coder registry does not find it and we need to set the coder explicitly
-    PCollection<UntypedOccurrence> verbatimRecords =
-      rawRecords.apply("Convert the objects into untyped DwC style records", ParDo.of(fn))
-        .setCoder(AvroCoder.of(UntypedOccurrence.class));
+    PCollection<UntypedOccurrence> verbatimRecords = rawRecords.apply(
+      "Convert the objects into untyped DwC style records",
+      MapElements.into(untypedOccurrencies()).via(untypedOccurrenceBuilder()::apply))
+      .setCoder(AvroCoder.of(UntypedOccurrence.class));
 
     verbatimRecords.apply("Write Avro files",
                           AvroIO.write(UntypedOccurrence.class)
