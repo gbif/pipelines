b4ce26ddaa4ef082fdbf3979708f5d313ed342d1
aalbatross <rpathak@gbif.org>
2018-02-22 10:47:55 +0100

    #24 restructuring extendedoccurencetransform , putting the internal functions to the function package

--- gbif/src/main/java/org/gbif/pipelines/transforms/ExtendedOccurenceTransform.java
+++ gbif/src/main/java/org/gbif/pipelines/transforms/ExtendedOccurenceTransform.java
@@ -6,6 +6,8 @@ import org.gbif.dwca.avro.Location;
 import org.gbif.pipelines.core.functions.interpretation.error.Issue;
 import org.gbif.pipelines.core.functions.interpretation.error.IssueLineageRecord;
 import org.gbif.pipelines.core.functions.interpretation.error.Lineage;
+import org.gbif.pipelines.transforms.function.InterpretedIssueRecordTransform;
+import org.gbif.pipelines.transforms.function.InterpretedOccurenceTransform;
 
 import java.util.HashMap;
 import java.util.List;
@@ -54,8 +56,8 @@ public class ExtendedOccurenceTransform extends PTransform<PCollectionTuple, PCo
         .apply(CoGroupByKey.create());
 
     PCollection<IssueLineageRecord> interpretedIssueLineageRecords = joinedIssueCollection.apply(
-      "Aplying join on the issues and lineages obtained",
-      ParDo.of(new CoGbkResultToFlattenedInterpretedIssueRecord()));
+      "Applying join on the issues and lineages obtained",
+      ParDo.of(new InterpretedIssueRecordTransform(this)));
 
     /*
       Joining temporal category and spatial category to get the big flat interpreted record.
@@ -67,7 +69,7 @@ public class ExtendedOccurenceTransform extends PTransform<PCollectionTuple, PCo
 
     PCollection<ExtendedOccurence> interpretedRecords = joinedCollection.apply(
       "Applying join on interpreted category of records to make a flat big interpreted record",
-      ParDo.of(new CoGbkResultToFlattenedInterpretedRecord()));
+      ParDo.of(new InterpretedOccurenceTransform(this)));
 
     return PCollectionTuple.of(INTERPRETED_OCCURENCE, interpretedRecords)
       .and(INTERPRETED_ISSUE, interpretedIssueLineageRecords);
@@ -97,62 +99,4 @@ public class ExtendedOccurenceTransform extends PTransform<PCollectionTuple, PCo
     return INTERPRETED_ISSUE;
   }
 
-  /**
-   * Convert's Beam's represented Joined PCollection to an Interpreted Occurrence
-   */
-  private class CoGbkResultToFlattenedInterpretedRecord extends DoFn<KV<String, CoGbkResult>, ExtendedOccurence> {
-
-    @ProcessElement
-    public void processElement(ProcessContext ctx) {
-      KV<String, CoGbkResult> result = ctx.element();
-      //get temporal and spatial info from the joined beam collection with tags
-      Event evt = result.getValue().getOnly(TEMPORAL_TAG);
-      Location loc = result.getValue().getOnly(SPATIAL_TAG);
-
-      //create final interpreted record with values from the interpreted category
-      ExtendedOccurence occurence = new ExtendedOccurence();
-      occurence.setOccurrenceID(result.getKey());
-      occurence.setBasisOfRecord(evt.getBasisOfRecord());
-      occurence.setDay(evt.getDay());
-      occurence.setMonth(evt.getMonth());
-      occurence.setYear(evt.getYear());
-      occurence.setEventDate(evt.getEventDate());
-      occurence.setDecimalLatitude(loc.getDecimalLatitude());
-      occurence.setDecimalLongitude(loc.getDecimalLongitude());
-      occurence.setCountry(loc.getCountry());
-      occurence.setCountryCode(loc.getCountryCode());
-      occurence.setContinent(loc.getContinent());
-
-      ctx.output(occurence);
-    }
-  }
-
-  /**
-   * Convert's Beam's represented Joined Issues PCollection to an IssueAndLineageRecord
-   */
-  private class CoGbkResultToFlattenedInterpretedIssueRecord extends DoFn<KV<String, CoGbkResult>, IssueLineageRecord> {
-
-    @ProcessElement
-    public void processElement(ProcessContext ctx) {
-      KV<String, CoGbkResult> result = ctx.element();
-      //get temporal and spatial issues info from the joined beam collection with tags
-      IssueLineageRecord evt = result.getValue().getOnly(TEMPORAL_ISSUE_TAG);
-      IssueLineageRecord loc = result.getValue().getOnly(SPATIAL_ISSUE_TAG);
-
-      Map<CharSequence, List<Issue>> fieldIssueMap = new HashMap<>();
-      fieldIssueMap.putAll(evt.getFieldIssueMap());
-      fieldIssueMap.putAll(loc.getFieldIssueMap());
-
-      Map<CharSequence, List<Lineage>> fieldLineageMap = new HashMap<>();
-      fieldLineageMap.putAll(evt.getFieldLineageMap());
-      fieldLineageMap.putAll(loc.getFieldLineageMap());
-      //construct a final IssueLineageRecord for all categories
-      IssueLineageRecord record = IssueLineageRecord.newBuilder()
-        .setOccurenceId(evt.getOccurenceId())
-        .setFieldIssueMap(fieldIssueMap)
-        .setFieldLineageMap(fieldLineageMap)
-        .build();
-      ctx.output(record);
-    }
-  }
 }
---
+++ gbif/src/main/java/org/gbif/pipelines/transforms/function/InterpretedIssueRecordTransform.java
@@ -0,0 +1,49 @@
+package org.gbif.pipelines.transforms.function;
+
+import org.gbif.pipelines.core.functions.interpretation.error.Issue;
+import org.gbif.pipelines.core.functions.interpretation.error.IssueLineageRecord;
+import org.gbif.pipelines.core.functions.interpretation.error.Lineage;
+import org.gbif.pipelines.transforms.ExtendedOccurenceTransform;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.join.CoGbkResult;
+import org.apache.beam.sdk.values.KV;
+
+/**
+ * Convert's Beam's represented Joined Issues PCollection to an IssueAndLineageRecord
+ */
+public class InterpretedIssueRecordTransform extends DoFn<KV<String, CoGbkResult>, IssueLineageRecord> {
+
+  private final ExtendedOccurenceTransform extendedOccurenceTransform;
+
+  public InterpretedIssueRecordTransform(ExtendedOccurenceTransform extendedOccurenceTransform) {
+    this.extendedOccurenceTransform = extendedOccurenceTransform;
+  }
+
+  @DoFn.ProcessElement
+  public void processElement(ProcessContext ctx) {
+    KV<String, CoGbkResult> result = ctx.element();
+    //get temporal and spatial issues info from the joined beam collection with tags
+    IssueLineageRecord evt = result.getValue().getOnly(extendedOccurenceTransform.getTemporalIssueTag());
+    IssueLineageRecord loc = result.getValue().getOnly(extendedOccurenceTransform.getSpatialIssueTag());
+
+    Map<CharSequence, List<Issue>> fieldIssueMap = new HashMap<>();
+    fieldIssueMap.putAll(evt.getFieldIssueMap());
+    fieldIssueMap.putAll(loc.getFieldIssueMap());
+
+    Map<CharSequence, List<Lineage>> fieldLineageMap = new HashMap<>();
+    fieldLineageMap.putAll(evt.getFieldLineageMap());
+    fieldLineageMap.putAll(loc.getFieldLineageMap());
+    //construct a final IssueLineageRecord for all categories
+    IssueLineageRecord record = IssueLineageRecord.newBuilder()
+      .setOccurenceId(evt.getOccurenceId())
+      .setFieldIssueMap(fieldIssueMap)
+      .setFieldLineageMap(fieldLineageMap)
+      .build();
+    ctx.output(record);
+  }
+}
