aebc175c6f77472bcbc31c9395a1202ebe0c8d62
Nikolay Volik <nvolik@gbif.org>
2018-03-09 14:12:32 +0100

    Demo pipeline for SOLR date range

--- labs/src/main/java/org/gbif/pipelines/labs/DateRanges2EsPipeline.java
+++ labs/src/main/java/org/gbif/pipelines/labs/DateRanges2EsPipeline.java
@@ -11,19 +11,24 @@ import org.apache.beam.sdk.values.PCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * FOR DEMO ONLY
+ */
 public class DateRanges2EsPipeline {
 
   private static final Logger LOG = LoggerFactory.getLogger(DateRanges2EsPipeline.class);
 
   private static final String ES_IDX = "es-date-ranges";
   private static final String[] ES_ADDRESSES = {"http://c3n1.gbif.org:9200", "http://c3n2.gbif.org:9200", "http://c3n3.gbif.org:9200"};
-  private static final String FILE_PATH = "labs/data/data-ranges/es-date-ranges.txt";
+  private static final String FILE_PATH = "labs/data/data-ranges/es/es-date-ranges.txt";
+  private static final String DELETE_IDX = String.format("curl -X DELETE %s/%s", ES_ADDRESSES[0], ES_IDX);
+  private static final String CREATE_IDX = String.format("curl -X PUT %s/%s -d @labs/data/data-ranges/es/es-index-mapping.json", ES_ADDRESSES[0], ES_IDX);
 
   public static void main(String[] args) throws Exception {
 
-    // Prestep: Create ES index
-    Runtime.getRuntime().exec(String.format("curl -X DELETE %s/%s", ES_ADDRESSES[0], ES_IDX)).waitFor();
-    Runtime.getRuntime().exec(String.format("curl -X PUT %s/%s -d @labs/data/data-ranges/es-index-mapping.json", ES_ADDRESSES[0], ES_IDX)).waitFor();
+    // Prestep: Recreate ES index
+    Runtime.getRuntime().exec(DELETE_IDX).waitFor();
+    Runtime.getRuntime().exec(CREATE_IDX).waitFor();
 
     // Step 0: Options
     PipelineOptions options = PipelineOptionsFactory.create();
@@ -34,8 +39,8 @@ public class DateRanges2EsPipeline {
     // Step 1: Read file
     PCollection<String> rawCollection = p.apply(TextIO.read().from(FILE_PATH));
 
-    // Step 2: Create ES index
-    ElasticsearchIO.ConnectionConfiguration conn = ElasticsearchIO.ConnectionConfiguration.create(ES_ADDRESSES, ES_IDX, ES_IDX);
+    // Step 2: Write ES index
+    final ElasticsearchIO.ConnectionConfiguration conn = ElasticsearchIO.ConnectionConfiguration.create(ES_ADDRESSES, ES_IDX, ES_IDX);
     rawCollection.apply(ElasticsearchIO.write().withConnectionConfiguration(conn));
 
     // Step 3: Run pipeline
