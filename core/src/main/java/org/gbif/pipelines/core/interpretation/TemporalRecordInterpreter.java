1d1dbe6756fc1279119242e7267c0f6394113883
Nikolay Volik <nvolik@gbif.org>
2018-03-12 13:05:00 +0100

    Added inner the class for temporal to support ES

--- core/src/main/java/org/gbif/pipelines/core/interpretation/TemporalRecordInterpreter.java
+++ core/src/main/java/org/gbif/pipelines/core/interpretation/TemporalRecordInterpreter.java
@@ -4,6 +4,7 @@ import org.gbif.dwc.terms.DwcTerm;
 import org.gbif.pipelines.core.interpretation.Interpretation.Trace;
 import org.gbif.pipelines.core.parsers.TemporalParser;
 import org.gbif.pipelines.core.parsers.temporal.ParsedTemporalDates;
+import org.gbif.pipelines.io.avro.EventDate;
 import org.gbif.pipelines.io.avro.ExtendedRecord;
 import org.gbif.pipelines.io.avro.TemporalRecord;
 
@@ -11,7 +12,6 @@ import java.time.Month;
 import java.time.Year;
 import java.time.temporal.Temporal;
 import java.util.Optional;
-import java.util.StringJoiner;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -33,17 +33,15 @@ public interface TemporalRecordInterpreter extends Function<ExtendedRecord, Inte
       ParsedTemporalDates temporalDates = TemporalParser.parse(rawYear, rawMonth, rawDay, rawEventDate);
 
       // Get all parsed values and set
+      EventDate eventDate = new EventDate();
+
       temporalDates.getYear().map(Year::getValue).ifPresent(temporalRecord::setYear);
       temporalDates.getMonth().map(Month::getValue).ifPresent(temporalRecord::setMonth);
       temporalDates.getDay().ifPresent(temporalRecord::setDay);
+      temporalDates.getFrom().map(Temporal::toString).ifPresent(eventDate::setGte);
+      temporalDates.getTo().map(Temporal::toString).ifPresent(eventDate::setLte);
 
-      //TODO: move delimiter definition to a more reusable  class
-      //TODO: check with ES
-      StringJoiner eventDay = new StringJoiner("/");
-      temporalDates.getFrom().map(Temporal::toString).ifPresent(eventDay::add);
-      temporalDates.getTo().map(Temporal::toString).ifPresent(eventDay::add);
-
-      temporalRecord.setEventDate(eventDay.toString());
+      temporalRecord.setEventDate(eventDate);
 
       // Map to Interpretation
       Interpretation<ExtendedRecord> interpretation = Interpretation.of(extendedRecord);
--- labs/src/test/java/org/gbif/pipelines/labs/transform/ExtendedOccurrenceTransformTest.java
+++ labs/src/test/java/org/gbif/pipelines/labs/transform/ExtendedOccurrenceTransformTest.java
@@ -1,6 +1,7 @@
 package org.gbif.pipelines.labs.transform;
 
 import org.gbif.dwc.terms.DwcTerm;
+import org.gbif.pipelines.io.avro.EventDate;
 import org.gbif.pipelines.io.avro.ExtendedOccurrence;
 import org.gbif.pipelines.io.avro.ExtendedRecord;
 import org.gbif.pipelines.transform.Kv2Value;
@@ -106,7 +107,7 @@ public class ExtendedOccurrenceTransformTest {
         .setDay(Integer.valueOf(x[10]))
         .setMonth(Integer.valueOf(x[11]))
         .setYear(Integer.valueOf(x[12]))
-        .setEventDate(x[13])
+        .setEventDate(EventDate.newBuilder().setGte(x[13]).build())
         .setMinimumDepthInMeters(Double.valueOf(x[14]))
         .setMaximumDepthInMeters(Double.valueOf(x[15]))
         .setMinimumElevationInMeters(Double.valueOf(x[16]))
