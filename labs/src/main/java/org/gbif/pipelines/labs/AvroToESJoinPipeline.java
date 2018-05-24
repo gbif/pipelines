3e5dac2edec8011d169c499e47ca91c5597ce17c
aalbatross <rpathak@gbif.org>
2018-05-24 14:08:44 +0200

    Compilation error resolved

--- labs/src/main/java/org/gbif/pipelines/labs/AvroToESJoinPipeline.java
+++ labs/src/main/java/org/gbif/pipelines/labs/AvroToESJoinPipeline.java
@@ -7,61 +7,70 @@ import org.gbif.pipelines.io.avro.Location;
 import org.gbif.pipelines.io.avro.MultimediaRecord;
 import org.gbif.pipelines.io.avro.TaxonRecord;
 import org.gbif.pipelines.io.avro.TemporalRecord;
+import org.gbif.pipelines.labs.io.PatchedElasticsearchIO;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.AvroIO;
-import org.apache.beam.sdk.io.elasticsearch.ElasticsearchIO;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.values.TypeDescriptor;
 
 public class AvroToESJoinPipeline {
-  private static final String INPUT = "hdfs://ha-nn/user/hive/warehouse/gbif-data/00ddc92e-fde1-4dda-8712-ff46582a5e6b/1/";
+
+  private static final String INPUT =
+    "hdfs://ha-nn/user/hive/warehouse/gbif-data/00ddc92e-fde1-4dda-8712-ff46582a5e6b/1/";
   private static final String[] ES_ADDRESSES =
     {"http://c3n1.gbif.org:9200", "http://c3n2.gbif.org:9200", "http://c3n3.gbif.org:9200"};
   private static final String ES_IDX = "interpreted-dataset";
 
   public static void main(String[] args) {
 
-    DataProcessingPipelineOptions options =
-      DataPipelineOptionsFactory.create(args);
+    DataProcessingPipelineOptions options = DataPipelineOptionsFactory.create(args);
     Pipeline pipeline = Pipeline.create(options);
 
-    pipeline.apply(AvroIO.read(Location.class).from(INPUT+"location/interpreted*.avro"))
+    pipeline.apply(AvroIO.read(Location.class).from(INPUT + "location/interpreted*.avro"))
       .apply(MapElements.into(TypeDescriptor.of(String.class)).via((record) -> record.toString()))
-      .apply(ElasticsearchIO.write()
-               .withConnectionConfiguration(ElasticsearchIO.ConnectionConfiguration.create(ES_ADDRESSES,
-                                                                                           ES_IDX,
-                                                                                           ES_IDX)).withIdFn((node)->node.get("occurrenceID").textValue()));
+      .apply(PatchedElasticsearchIO.write()
+               .withConnectionConfiguration(PatchedElasticsearchIO.ConnectionConfiguration.create(ES_ADDRESSES,
+                                                                                                  ES_IDX,
+                                                                                                  ES_IDX))
+               .withIdFn((node) -> node.get("occurrenceID").textValue())
+               .withUsePartialUpdate(true));
 
-    pipeline.apply(AvroIO.read(TemporalRecord.class).from(INPUT+"temporal/interpreted*.avro"))
+    pipeline.apply(AvroIO.read(TemporalRecord.class).from(INPUT + "temporal/interpreted*.avro"))
       .apply(MapElements.into(TypeDescriptor.of(String.class)).via((record) -> record.toString()))
-      .apply(ElasticsearchIO.write()
-               .withConnectionConfiguration(ElasticsearchIO.ConnectionConfiguration.create(ES_ADDRESSES,
-                                                                                           ES_IDX,
-                                                                                           ES_IDX)).withIdFn((node)->node.get("id").textValue()));
+      .apply(PatchedElasticsearchIO.write()
+               .withConnectionConfiguration(PatchedElasticsearchIO.ConnectionConfiguration.create(ES_ADDRESSES,
+                                                                                                  ES_IDX,
+                                                                                                  ES_IDX))
+               .withIdFn((node) -> node.get("id").textValue())
+               .withUsePartialUpdate(true));
 
-    pipeline.apply(AvroIO.read(MultimediaRecord.class).from(INPUT+"multimedia/interpreted*.avro"))
+    pipeline.apply(AvroIO.read(MultimediaRecord.class).from(INPUT + "multimedia/interpreted*.avro"))
       .apply(MapElements.into(TypeDescriptor.of(String.class)).via((record) -> record.toString()))
-      .apply(ElasticsearchIO.write()
-               .withConnectionConfiguration(ElasticsearchIO.ConnectionConfiguration.create(ES_ADDRESSES,
-                                                                                           ES_IDX,
-                                                                                           ES_IDX)).withIdFn((node)->node.get("id").textValue()));
+      .apply(PatchedElasticsearchIO.write()
+               .withConnectionConfiguration(PatchedElasticsearchIO.ConnectionConfiguration.create(ES_ADDRESSES,
+                                                                                                  ES_IDX,
+                                                                                                  ES_IDX))
+               .withIdFn((node) -> node.get("id").textValue())
+               .withUsePartialUpdate(true));
 
-    pipeline.apply(AvroIO.read(TaxonRecord.class).from(INPUT+"taxonomy/interpreted*.avro"))
+    pipeline.apply(AvroIO.read(TaxonRecord.class).from(INPUT + "taxonomy/interpreted*.avro"))
       .apply(MapElements.into(TypeDescriptor.of(String.class)).via((record) -> record.toString()))
-      .apply(ElasticsearchIO.write()
-               .withConnectionConfiguration(ElasticsearchIO.ConnectionConfiguration.create(ES_ADDRESSES,
-                                                                                           ES_IDX,
-                                                                                           ES_IDX)).withIdFn((node)->node.get("id").textValue()));
+      .apply(PatchedElasticsearchIO.write()
+               .withConnectionConfiguration(PatchedElasticsearchIO.ConnectionConfiguration.create(ES_ADDRESSES,
+                                                                                                  ES_IDX,
+                                                                                                  ES_IDX))
+               .withIdFn((node) -> node.get("id").textValue())
+               .withUsePartialUpdate(true));
 
-    pipeline.apply(AvroIO.read(InterpretedExtendedRecord.class).from(INPUT+"common/interpreted*.avro"))
+    pipeline.apply(AvroIO.read(InterpretedExtendedRecord.class).from(INPUT + "common/interpreted*.avro"))
       .apply(MapElements.into(TypeDescriptor.of(String.class)).via((record) -> record.toString()))
-      .apply(ElasticsearchIO.write()
-               .withConnectionConfiguration(ElasticsearchIO.ConnectionConfiguration.create(ES_ADDRESSES,
-                                                                                           ES_IDX,
-                                                                                           ES_IDX)).withIdFn((node)->node.get("id").textValue()));
+      .apply(PatchedElasticsearchIO.write()
+               .withConnectionConfiguration(PatchedElasticsearchIO.ConnectionConfiguration.create(ES_ADDRESSES,
+                                                                                                  ES_IDX,
+                                                                                                  ES_IDX))
+               .withIdFn((node) -> node.get("id").textValue())
+               .withUsePartialUpdate(true));
 
     pipeline.run().waitUntilFinish();
 
