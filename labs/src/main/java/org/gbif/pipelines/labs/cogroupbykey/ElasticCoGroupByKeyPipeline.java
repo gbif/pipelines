21f37e50d05208d330dce0a9f39f5c321de5669b
Nikolay Volik <nvolik@gbif.org>
2018-05-28 11:21:06 +0200

    Changed ElasticCoGroupByKeyPipeline.java methods

--- labs/src/main/java/org/gbif/pipelines/labs/cogroupbykey/ElasticCoGroupByKeyPipeline.java
+++ labs/src/main/java/org/gbif/pipelines/labs/cogroupbykey/ElasticCoGroupByKeyPipeline.java
@@ -28,8 +28,9 @@ import org.slf4j.LoggerFactory;
 
 /**
  * How to run example:
- * sudo -u hdfs spark2-submit --properties-file /home/crap/config/ws.properties --conf
- * spark.default.parallelism=100 --conf spark.executor.memoryOverhead=2048 --class
+ *
+ * sudo -u hdfs spark2-submit --properties-file /home/crap/config/ws.properties
+ * --conf spark.default.parallelism=100 --conf spark.executor.memoryOverhead=2048 --class
  * org.gbif.pipelines.labs.cogroupbykey.ElasticCoGroupByKeyPipeline --master yarn --deploy-mode
  * cluster --executor-memory 12G --executor-cores 8 --num-executors 16 --driver-memory 1G
  * /home/crap/lib/labs-1.1-SNAPSHOT-shaded.jar --wsProperties=ws.properties
@@ -37,7 +38,7 @@ import org.slf4j.LoggerFactory;
  * --defaultTargetDirectory=hdfs://ha-nn/data/ingest/0645ccdb-e001-4ab0-9729-51f1755e007e/1/
  * --inputFile=hdfs://ha-nn/data/ingest/0645ccdb-e001-4ab0-9729-51f1755e007e/1/
  * --hdfsSiteConfig=/home/crap/config/hdfs-site.xml --coreSiteConfig=/home/crap/config/core-site.xml
- * --ESHosts=c3master1-vh.gbif.org:2181,c3master2-vh.gbif.org:2181,c3master3-vh.gbif.org:2181/solr5c2
+ * --ESHosts=http://c3n1.gbif.org:9200,http://c3n2.gbif.org:9200,http://c3n3.gbif.org:9200
  * --ESIndex=co-group-idx --ESType=co-group-idx --ESMaxBatchSize=1000
  */
 public class ElasticCoGroupByKeyPipeline {
