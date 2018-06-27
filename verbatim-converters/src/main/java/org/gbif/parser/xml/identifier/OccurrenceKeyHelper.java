db8bd2f1b2652f2d4919697a1b2e6d165f7e5b1e
Nikolay Volik <nvolik@gbif.org>
2018-06-27 12:40:26 +0200

    Moved dwca and xml to avro parts from crawler-cli Renamed package name

---
+++ verbatim-converters/src/main/java/org/gbif/parser/xml/identifier/OccurrenceKeyHelper.java
@@ -0,0 +1,108 @@
+package org.gbif.parser.xml.identifier;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.StringJoiner;
+import javax.annotation.Nullable;
+
+/**
+ * A helper class for building the row keys used in the occurrence lookup table/process. TODO: this
+ * is too similar to OccurrenceKeyBuilder - they should be merged
+ */
+public class OccurrenceKeyHelper {
+
+  private static final String DELIM = "|";
+
+  private OccurrenceKeyHelper() {}
+
+  public static String buildKeyPrefix(String datasetKey) {
+    return datasetKey + DELIM;
+  }
+
+  @Nullable
+  public static String buildKey(@Nullable Triplet triplet) {
+    if (Objects.isNull(triplet)
+        || Objects.isNull(triplet.getDatasetKey())
+        || Objects.isNull(triplet.getInstitutionCode())
+        || Objects.isNull(triplet.getCollectionCode())
+        || Objects.isNull(triplet.getCatalogNumber())) {
+      return null;
+    }
+
+    return join(
+        DELIM,
+        triplet.getDatasetKey().toString(),
+        triplet.getInstitutionCode(),
+        triplet.getCollectionCode(),
+        triplet.getCatalogNumber(),
+        triplet.getUnitQualifier());
+  }
+
+  @Nullable
+  public static String buildKey(@Nullable PublisherProvidedUniqueIdentifier pubProvided) {
+    if (Objects.isNull(pubProvided)
+        || Objects.isNull(pubProvided.getDatasetKey())
+        || Objects.isNull(pubProvided.getPublisherProvidedIdentifier())) {
+      return null;
+    }
+
+    return join(
+        DELIM,
+        pubProvided.getDatasetKey().toString(),
+        pubProvided.getPublisherProvidedIdentifier());
+  }
+
+  @Nullable
+  public static String buildUnscopedKey(
+      @Nullable PublisherProvidedUniqueIdentifier pubProvidedUniqueId) {
+    if (pubProvidedUniqueId == null) {
+      return null;
+    }
+
+    return pubProvidedUniqueId.getPublisherProvidedIdentifier();
+  }
+
+  @Nullable
+  public static String buildUnscopedKey(@Nullable Triplet triplet) {
+    if (Objects.isNull(triplet)
+        || Objects.isNull(triplet.getDatasetKey())
+        || Objects.isNull(triplet.getInstitutionCode())
+        || Objects.isNull(triplet.getCollectionCode())
+        || Objects.isNull(triplet.getCatalogNumber())) {
+      return null;
+    }
+
+    return join(
+        DELIM,
+        triplet.getInstitutionCode(),
+        triplet.getCollectionCode(),
+        triplet.getCatalogNumber(),
+        triplet.getUnitQualifier());
+  }
+
+  @Nullable
+  public static String toKey(@Nullable Triplet triplet) {
+    if (Objects.isNull(triplet)
+        || Objects.isNull(triplet.getInstitutionCode())
+        || Objects.isNull(triplet.getCollectionCode())
+        || Objects.isNull(triplet.getCatalogNumber())) {
+      return null;
+    }
+
+    // id format following the convention of DwC (http://rs.tdwg.org/dwc/terms/#occurrenceID)
+    return join(
+        ":",
+        "urn:catalog",
+        triplet.getInstitutionCode(),
+        triplet.getCollectionCode(),
+        triplet.getCatalogNumber());
+  }
+
+  private static String join(String delim, String... values) {
+    StringJoiner joiner = new StringJoiner(delim);
+    Arrays.stream(values)
+        .forEach(x -> Optional.ofNullable(x).filter(f -> !f.isEmpty()).ifPresent(joiner::add));
+    return joiner.toString();
+  }
+}
---
+++ verbatim-converters/src/main/java/org/gbif/parser/xml/parsing/xml/rules/Abcd12RuleSet.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2011 Global Biodiversity Information Facility (GBIF)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gbif.parser.xml.parsing.xml.rules;
+
+import org.gbif.api.vocabulary.OccurrenceSchemaType;
+import org.gbif.parser.xml.constants.PrioritizedPropertyNameEnum;
+import org.gbif.parser.xml.model.Identification;
+import org.gbif.parser.xml.model.ImageRecord;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
+import org.apache.commons.digester.Digester;
+
+public class Abcd12RuleSet extends AbstractRuleSet {
+
+  private final String mappingFile = "mapping/indexMapping_abcd_1_2.properties";
+
+  public Abcd12RuleSet() throws IOException {
+    mappingProps = new Properties();
+    URL url = ClassLoader.getSystemResource(mappingFile);
+    mappingProps.load(url.openStream());
+  }
+
+  @Override
+  public String getNamespaceURI() {
+    return OccurrenceSchemaType.ABCD_1_2.toString();
+  }
+
+  @Override
+  public void addRuleInstances(Digester digester) {
+    super.addRuleInstances(digester);
+
+    // abcd simple fields
+    addNonNullMethod(digester, "catalogueNumber", "setCatalogueNumber", 1);
+    addNonNullParam(digester, "catalogueNumber", 0);
+
+    addNonNullMethod(digester, "altitudePrecision", "setAltitudePrecision", 1);
+    addNonNullParam(digester, "altitudePrecision", 0);
+
+    addNonNullMethod(digester, "depthPrecision", "setDepthPrecision", 1);
+    addNonNullParam(digester, "depthPrecision", 0);
+
+    addNonNullMethod(digester, "locality", "setLocality", 1);
+    addNonNullParam(digester, "locality", 0);
+
+    addNonNullMethod(digester, "latitude", "setLatitude", 1);
+    addNonNullParam(digester, "latitude", 0);
+
+    addNonNullMethod(digester, "longitude", "setLongitude", 1);
+    addNonNullParam(digester, "longitude", 0);
+
+    addNonNullPrioritizedProperty(digester, "country", PrioritizedPropertyNameEnum.COUNTRY, 4);
+    addNonNullPrioritizedProperty(
+        digester, "collectorName", PrioritizedPropertyNameEnum.COLLECTOR_NAME, 3);
+    addNonNullPrioritizedProperty(
+        digester, "dateCollected", PrioritizedPropertyNameEnum.DATE_COLLECTED, 3);
+
+    // possibly many identifications
+    String pattern = mappingProps.getProperty("idElement");
+    if (pattern != null) {
+      pattern = pattern.trim();
+      digester.addObjectCreate(pattern, Identification.class);
+      digester.addSetNext(pattern, "addIdentification");
+
+      addNonNullMethod(digester, "idPreferredElement", "setPreferredAsString", 1);
+      addNonNullAttParam(digester, "idPreferredElement", "idPreferredAttribute", 0);
+
+      addNonNullMethod(digester, "idGenus", "setGenus", 1);
+      addNonNullParam(digester, "idGenus", 0);
+
+      addNonNullMethod(digester, "idGenus", "setGenus", 1);
+      addNonNullParam(digester, "idGenus", 0);
+
+      addNonNullMethod(digester, "idIdentifierName", "setIdentifierName", 1);
+      addNonNullParam(digester, "idIdentifierName", 0);
+
+      addNonNullPrioritizedProperty(
+          digester, "idDateIdentified", PrioritizedPropertyNameEnum.ID_DATE_IDENTIFIED, 3);
+      addNonNullPrioritizedProperty(
+          digester, "idScientificName", PrioritizedPropertyNameEnum.ID_SCIENTIFIC_NAME, 2);
+
+      // possibly many higher taxons for every identification
+      addNonNullMethod(digester, "higherTaxonElement", "addHigherTaxon", 2);
+      addNonNullParam(digester, "higherTaxonRank", 0);
+      addNonNullAttParam(digester, "higherTaxonRankElement", "higherTaxonRankAttribute", 0);
+      addNonNullParam(digester, "higherTaxonName", 1);
+    }
+
+    // possibly many images
+    pattern = mappingProps.getProperty("imageElement");
+    if (pattern != null) {
+      pattern = pattern.trim();
+      digester.addObjectCreate(pattern, ImageRecord.class);
+      digester.addSetNext(pattern, "addImage");
+
+      addNonNullMethod(digester, "imageType", "setRawImageType", 1);
+      addNonNullParam(digester, "imageType", 0);
+
+      addNonNullMethod(digester, "imageDescription", "setDescription", 1);
+      addNonNullParam(digester, "imageDescription", 0);
+
+      addNonNullMethod(digester, "imageRights", "setRights", 1);
+      addNonNullParam(digester, "imageRights", 0);
+
+      addNonNullMethod(digester, "imageUrl", "setUrl", 1);
+      addNonNullParam(digester, "imageUrl", 0);
+    }
+
+    // NOTE: no links in abcd 1.2
+  }
+}
