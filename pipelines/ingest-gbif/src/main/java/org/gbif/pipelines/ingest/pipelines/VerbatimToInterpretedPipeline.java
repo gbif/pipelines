1dd4e69d5e6899899b3b60aed8ab68c12254e5f1
Nikolay Volik <nvolik@gbif.org>
2018-09-19 11:45:46 +0200

    Changed pipeline names

--- pipelines/ingest-gbif/src/main/java/org/gbif/pipelines/ingest/pipelines/VerbatimToInterpretedPipeline.java
+++ pipelines/ingest-gbif/src/main/java/org/gbif/pipelines/ingest/pipelines/VerbatimToInterpretedPipeline.java
@@ -1,20 +1,19 @@
 package org.gbif.pipelines.ingest.pipelines;
 
-import org.gbif.pipelines.common.beam.DwcaIO;
 import org.gbif.pipelines.core.RecordType;
-import org.gbif.pipelines.ingest.options.DwcaPipelineOptions;
+import org.gbif.pipelines.ingest.options.InterpretationPipelineOptions;
 import org.gbif.pipelines.ingest.options.PipelinesOptionsFactory;
 import org.gbif.pipelines.ingest.utils.FsUtils;
 import org.gbif.pipelines.io.avro.ExtendedRecord;
-import org.gbif.pipelines.parsers.ws.config.WsConfig;
-import org.gbif.pipelines.parsers.ws.config.WsConfigFactory;
+import org.gbif.pipelines.transforms.CheckTransforms;
+import org.gbif.pipelines.transforms.ReadTransforms;
 import org.gbif.pipelines.transforms.RecordTransforms;
 import org.gbif.pipelines.transforms.UniqueIdTransform;
 import org.gbif.pipelines.transforms.WriteTransforms;
 
-import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
 import java.util.function.Function;
 
 import org.apache.beam.sdk.Pipeline;
@@ -34,7 +33,7 @@ import static org.gbif.pipelines.core.RecordType.TEMPORAL;
  * Pipeline sequence:
  *
  * <pre>
- *    1) Reads DwCA archive and converts to {@link org.gbif.pipelines.io.avro.ExtendedRecord}
+ *    1) Reads verbatim.avro file
  *    2) Interprets and converts avro {@link org.gbif.pipelines.io.avro.ExtendedRecord} file
  *        to {@link org.gbif.pipelines.io.avro.MetadataRecord}, {@link
  *        org.gbif.pipelines.io.avro.BasicRecord}, {@link org.gbif.pipelines.io.avro.TemporalRecord},
@@ -42,70 +41,91 @@ import static org.gbif.pipelines.core.RecordType.TEMPORAL;
  *        org.gbif.pipelines.io.avro.TaxonRecord}, {@link org.gbif.pipelines.io.avro.LocationRecord}
  *    3) Writes data to independent files
  * </pre>
+ *
+ * <p>How to run:
+ *
+ * <pre>{@code
+ * java -cp target/ingest-gbif-0.1-SNAPSHOT-shaded.jar org.gbif.pipelines.ingest.pipelines.VerbatimToInterpretedPipeline examples/configs/interpretation.properties
+ *
+ * or pass all parameters:
+ *
+ * java -cp target/ingest-gbif-0.1-SNAPSHOT-shaded.jar org.gbif.pipelines.ingest.pipelines.VerbatimToInterpretedPipeline
+ * --wsProperties=/some/path/to/output/ws.properties
+ * --datasetId=0057a720-17c9-4658-971e-9578f3577cf5
+ * --attempt=1
+ * --interpretationTypes=ALL
+ * --runner=SparkRunner
+ * --targetPath=/some/path/to/output/
+ * --inputPath=/some/path/to/output/0057a720-17c9-4658-971e-9578f3577cf5/1/verbatim.avro
+ *
+ * }</pre>
  */
-public class DwcaToIngestPipeline {
+public class VerbatimToInterpretedPipeline {
 
-  private static final Logger LOG = LoggerFactory.getLogger(DwcaToIngestPipeline.class);
+  private static final Logger LOG = LoggerFactory.getLogger(VerbatimToInterpretedPipeline.class);
 
-  private DwcaToIngestPipeline() {}
+  private VerbatimToInterpretedPipeline() {}
 
   public static void main(String[] args) {
-    DwcaPipelineOptions options = PipelinesOptionsFactory.create(DwcaPipelineOptions.class, args);
-    DwcaToIngestPipeline.createAndRun(options);
+    InterpretationPipelineOptions options = PipelinesOptionsFactory.createInterpretation(args);
+    createAndRun(options);
   }
 
-  public static void createAndRun(DwcaPipelineOptions options) {
+  public static void createAndRun(InterpretationPipelineOptions options) {
+    LOG.info("Running interpretation pipeline");
+    VerbatimToInterpretedPipeline.create(options).run().waitUntilFinish();
+    LOG.info("Interpretation pipeline has been finished");
+  }
 
-    WsConfig wsConfig = WsConfigFactory.create(options.getGbifApiUrl());
+  public static Pipeline create(InterpretationPipelineOptions options) {
+
+    List<String> types = options.getInterpretationTypes();
+    String wsProperties = options.getWsProperties();
     String id = Long.toString(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC));
 
     Function<RecordType, String> metaPathFn = t -> FsUtils.buildPath(options, t.name());
     Function<RecordType, String> pathFn = t -> FsUtils.buildPathInterpret(options, t.name(), id);
-
-    String inputPath = options.getInputPath();
-    boolean isDirectory = Paths.get(inputPath).toFile().isDirectory();
-
-    String tmpDir = FsUtils.getTempDir(options);
-
-    DwcaIO.Read reader =
-        isDirectory ? DwcaIO.Read.withPaths(inputPath) : DwcaIO.Read.withPaths(inputPath, tmpDir);
 
     LOG.info("Creating a pipeline from options");
     Pipeline p = Pipeline.create(options);
 
     LOG.info("Reading avro files");
     PCollection<ExtendedRecord> uniqueRecords =
-        p.apply("Read ExtendedRecords", reader)
+        p.apply("Read ExtendedRecords", ReadTransforms.extended(options.getInputPath()))
             .apply("Filter duplicates", UniqueIdTransform.create());
 
     LOG.info("Adding interpretations");
 
     p.apply("Create metadata collection", Create.of(options.getDatasetId()))
-        .apply("Interpret metadata", RecordTransforms.metadata(wsConfig))
+        .apply("Check metadata transform condition", CheckTransforms.metadata(types))
+        .apply("Interpret metadata", RecordTransforms.metadata(wsProperties))
         .apply("Write metadata to avro", WriteTransforms.metadata(metaPathFn.apply(METADATA)));
 
     uniqueRecords
+        .apply("Check basic transform condition", CheckTransforms.basic(types))
         .apply("Interpret basic", RecordTransforms.basic())
         .apply("Write basic to avro", WriteTransforms.basic(pathFn.apply(BASIC)));
 
     uniqueRecords
+        .apply("Check temporal transform condition", CheckTransforms.temporal(types))
         .apply("Interpret temporal", RecordTransforms.temporal())
         .apply("Write temporal to avro", WriteTransforms.temporal(pathFn.apply(TEMPORAL)));
 
     uniqueRecords
+        .apply("Check multimedia transform condition", CheckTransforms.multimedia(types))
         .apply("Interpret multimedia", RecordTransforms.multimedia())
         .apply("Write multimedia to avro", WriteTransforms.multimedia(pathFn.apply(MULTIMEDIA)));
 
     uniqueRecords
-        .apply("Interpret taxonomy", RecordTransforms.taxonomy(wsConfig))
+        .apply("Check taxonomy transform condition", CheckTransforms.taxon(types))
+        .apply("Interpret taxonomy", RecordTransforms.taxonomy(wsProperties))
         .apply("Write taxon to avro", WriteTransforms.taxon(pathFn.apply(TAXONOMY)));
 
     uniqueRecords
-        .apply("Interpret location", RecordTransforms.location(wsConfig))
+        .apply("Check location transform condition", CheckTransforms.location(types))
+        .apply("Interpret location", RecordTransforms.location(wsProperties))
         .apply("Write location to avro", WriteTransforms.location(pathFn.apply(LOCATION)));
 
-    LOG.info("Running interpretation pipeline");
-    p.run().waitUntilFinish();
-    LOG.info("Interpretation pipeline has been finished");
+    return p;
   }
 }
