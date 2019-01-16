52726538da95a25636c7ee60da021e97731e17d7
Nikolay Volik <nvolik@gbif.org>
2019-01-16 15:48:15 +0100

    Added workaround for a bug with stuck job

--- pipelines/ingest-gbif/src/main/java/org/gbif/pipelines/ingest/pipelines/InterpretedToEsIndexExtendedPipeline.java
+++ pipelines/ingest-gbif/src/main/java/org/gbif/pipelines/ingest/pipelines/InterpretedToEsIndexExtendedPipeline.java
@@ -5,6 +5,8 @@ import org.gbif.pipelines.ingest.options.PipelinesOptionsFactory;
 import org.gbif.pipelines.ingest.utils.EsIndexUtils;
 import org.gbif.pipelines.ingest.utils.FsUtils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
 /**
@@ -43,6 +45,8 @@ import org.slf4j.MDC;
  */
 public class InterpretedToEsIndexExtendedPipeline {
 
+  private static final Logger LOG = LoggerFactory.getLogger(InterpretedToEsIndexExtendedPipeline.class);
+
   private InterpretedToEsIndexExtendedPipeline() {}
 
   public static void main(String[] args) {
@@ -63,5 +67,9 @@ public class InterpretedToEsIndexExtendedPipeline {
     EsIndexUtils.swapIndexIfAliasExists(options);
 
     FsUtils.removeTmpDirecrory(options);
+    LOG.info("Finished main indexing pipeline");
+
+    LOG.info("Call - System.exit(0)"); // Workaround for a bug with stuck job
+    System.exit(0);
   }
 }
