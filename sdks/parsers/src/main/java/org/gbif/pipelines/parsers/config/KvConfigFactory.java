a758bb8e13d9ed4e270fa313c0909ef7d2f4c73a
nikolay.volik <nikolay.volik@hotmail.com>
2019-02-08 15:14:44 +0100

    Added geo properties prefix

--- sdks/parsers/src/main/java/org/gbif/pipelines/parsers/config/KvConfigFactory.java
+++ sdks/parsers/src/main/java/org/gbif/pipelines/parsers/config/KvConfigFactory.java
@@ -3,6 +3,7 @@ package org.gbif.pipelines.parsers.config;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Properties;
@@ -14,15 +15,19 @@ public class KvConfigFactory {
 
   // property suffixes
   private static final String WS_BASE_PATH_PROP = "gbif.api.url";
-  private static final String WS_TIMEOUT_PROP = "ws.timeout";
-  private static final String CACHE_SIZE_PROP = "ws.cache.sizeMb";
-  private static final String ZOOKEEPER_PROP = "zookeeper.url";
+  private static final String WS_TIMEOUT_PROP = "geocode.ws.timeout";
+  private static final String CACHE_SIZE_PROP = "geocode.ws.cache.sizeMb";
+  private static final String ZOOKEEPER_PROP = "geocode.zookeeper.url";
 
   // property defaults
   private static final String DEFAULT_TIMEOUT_SEC = "60";
   private static final String DEFAULT_CACHE_SIZE_MB = "64";
 
   private KvConfigFactory() {}
+
+  public static KvConfig create(String propertiesPath) {
+    return create(Paths.get(propertiesPath));
+  }
 
   public static KvConfig create(Path propertiesPath) {
     Objects.requireNonNull(propertiesPath);
@@ -47,12 +52,12 @@ public class KvConfigFactory {
     return new KvConfig(basePath, timeout, cacheSize, zookeeperUrl);
   }
 
-  public static KvConfig create(String basePath, String zookeeperUrl) {
-    return new KvConfig(basePath, Long.valueOf(DEFAULT_TIMEOUT_SEC), Long.valueOf(DEFAULT_CACHE_SIZE_MB), zookeeperUrl);
+  public static KvConfig create(String baseApiPath, String zookeeperUrl) {
+    return new KvConfig(baseApiPath, Long.valueOf(DEFAULT_TIMEOUT_SEC), Long.valueOf(DEFAULT_CACHE_SIZE_MB), zookeeperUrl);
   }
 
-  public static KvConfig create(String basePath, long timeoutInSec, long cacheInMb, String zookeeperUrl) {
-    return new KvConfig(basePath, timeoutInSec, cacheInMb, zookeeperUrl);
+  public static KvConfig create(String baseApiPath, long timeoutInSec, long cacheInMb, String zookeeperUrl) {
+    return new KvConfig(baseApiPath, timeoutInSec, cacheInMb, zookeeperUrl);
   }
 
   /**
