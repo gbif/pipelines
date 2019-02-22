164f97876bdeec747b8611b6785a04142ba41320
nikolay.volik <nikolay.volik@hotmail.com>
2019-02-22 14:24:44 +0100

    Updatet image extension

--- sdks/core/src/main/java/org/gbif/pipelines/core/ExtensionInterpretation.java
+++ sdks/core/src/main/java/org/gbif/pipelines/core/ExtensionInterpretation.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -43,9 +44,11 @@ public class ExtensionInterpretation {
 
     private final Supplier<T> supplier;
 
-    private Function<T, Optional<String>> checker;
-
     private Map<String, BiFunction<T, String, List<String>>> mapperMap = new HashMap<>();
+
+    private Set<Function<T, List<String>>> postMapperSet = new HashSet<>();
+
+    private Set<Function<T, Optional<String>>> checkerSet = new HashSet<>();
 
     private TargetHandler(Supplier<T> supplier) {
       this.supplier = supplier;
@@ -63,17 +66,8 @@ public class ExtensionInterpretation {
       return this;
     }
 
-    public TargetHandler<T> mapFn(Term key, BiFunction<T, String, String> function) {
-      mapperMap.put(key.qualifiedName(), (t, v) -> {
-        String r = function.apply(t, v);
-        return Strings.isNullOrEmpty(r) ? Collections.emptyList() : Collections.singletonList(r);
-      });
-      return this;
-    }
-
     public TargetHandler<T> map(Term key, BiFunction<T, String, List<String>> function) {
-      mapperMap.put(key.qualifiedName(), function);
-      return this;
+      return map(key.qualifiedName(), function);
     }
 
     public TargetHandler<T> map(String key, BiFunction<T, String, List<String>> function) {
@@ -86,35 +80,88 @@ public class ExtensionInterpretation {
       return this;
     }
 
-    public TargetHandler<T> skipIf(Function<T, Optional<String>> checker) {
-      this.checker = checker;
+    public TargetHandler<T> mapOne(Term key, BiFunction<T, String, String> function) {
+      return mapOne(key.qualifiedName(), function);
+    }
+
+    public TargetHandler<T> mapOne(String key, BiFunction<T, String, String> function) {
+      mapperMap.put(key, (t, v) -> {
+        String r = function.apply(t, v);
+        return Strings.isNullOrEmpty(r) ? Collections.emptyList() : Collections.singletonList(r);
+      });
       return this;
     }
 
-    public Result<T> convert(ExtendedRecord record) {
+    public TargetHandler<T> postMap(Function<T, List<String>> function) {
+      postMapperSet.add(function);
+      return this;
+    }
+
+    public TargetHandler<T> postMap(Consumer<T> consumer) {
+      return postMap(t -> {
+        consumer.accept(t);
+        return null;
+      });
+    }
+
+    public TargetHandler<T> postMapOne(Function<T, String> function) {
+      return postMap(t -> {
+        String r = function.apply(t);
+        return Strings.isNullOrEmpty(r) ? Collections.emptyList() : Collections.singletonList(r);
+      });
+    }
+
+    public TargetHandler<T> skipIf(Function<T, Optional<String>> checker) {
+      checkerSet.add(checker);
+      return this;
+    }
+
+    public Result<T> convert(List<Map<String, String>> extensions) {
       List<T> result = new ArrayList<>();
       Set<String> issues = new HashSet<>();
 
-      List<Map<String, String>> exts = record.getExtensions().get(extenstion);
-      Optional.ofNullable(exts).filter(e -> !e.isEmpty()).ifPresent(listExt ->
-          listExt.forEach(ext -> {
-            //
-            T t = supplier.get();
-            //
-            ext.forEach((k, v) -> {
-              BiFunction<T, String, List<String>> fn = mapperMap.get(k);
-              Optional.ofNullable(fn).map(c -> fn.apply(t, v)).ifPresent(issues::addAll);
-            });
-            //
-            Optional<String> skipIssue = checker.apply(t);
-            skipIssue.ifPresent(issues::add);
-            if (!skipIssue.isPresent()) {
-              result.add(t);
-            }
+      Optional.ofNullable(extensions)
+          .filter(e -> !e.isEmpty())
+          .ifPresent(listExt ->
+              listExt.forEach(ext -> {
+                //
+                T t = supplier.get();
 
-          }));
+                //
+                ext.forEach((k, v) -> {
+                  BiFunction<T, String, List<String>> fn = mapperMap.get(k);
+                  Optional.ofNullable(fn).map(c -> fn.apply(t, v)).ifPresent(issues::addAll);
+                });
+
+                //
+                postMapperSet.forEach(fn -> issues.addAll(fn.apply(t)));
+
+                //
+                Optional<String> first = checkerSet.stream()
+                    .map(x -> x.apply(t))
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .findFirst();
+
+                first.ifPresent(issues::add);
+
+                if (!first.isPresent()) {
+                  result.add(t);
+                }
+
+              }));
 
       return new Result<>(result, issues);
+    }
+
+    public Result<T> convert(Map<String, String> extension) {
+      List<Map<String, String>> exts = Collections.singletonList(extension);
+      return convert(exts);
+    }
+
+    public Result<T> convert(ExtendedRecord record) {
+      List<Map<String, String>> exts = record.getExtensions().get(extenstion);
+      return convert(exts);
     }
 
   }
--- sdks/core/src/main/java/org/gbif/pipelines/core/interpreters/extension/ImageInterpreter.java
+++ sdks/core/src/main/java/org/gbif/pipelines/core/interpreters/extension/ImageInterpreter.java
@@ -8,20 +8,23 @@ import java.util.Optional;
 
 import org.gbif.api.vocabulary.Extension;
 import org.gbif.common.parsers.MediaParser;
+import org.gbif.common.parsers.NumberParser;
 import org.gbif.common.parsers.UrlParser;
 import org.gbif.dwc.terms.DcTerm;
 import org.gbif.dwc.terms.DwcTerm;
+import org.gbif.kvs.geocode.LatLng;
 import org.gbif.pipelines.core.ExtensionInterpretation;
 import org.gbif.pipelines.core.ExtensionInterpretation.Result;
 import org.gbif.pipelines.core.ExtensionInterpretation.TargetHandler;
 import org.gbif.pipelines.io.avro.ExtendedRecord;
 import org.gbif.pipelines.io.avro.Image;
 import org.gbif.pipelines.io.avro.ImageRecord;
+import org.gbif.pipelines.parsers.parsers.common.ParsedField;
+import org.gbif.pipelines.parsers.parsers.location.legacy.CoordinateParseUtils;
 import org.gbif.pipelines.parsers.parsers.temporal.ParsedTemporal;
 import org.gbif.pipelines.parsers.parsers.temporal.TemporalParser;
 
-import com.google.common.base.Strings;
-
+import static org.gbif.api.vocabulary.OccurrenceIssue.COORDINATE_INVALID;
 import static org.gbif.api.vocabulary.OccurrenceIssue.MULTIMEDIA_URI_INVALID;
 
 /**
@@ -36,21 +39,25 @@ public class ImageInterpreter {
 
   //
   private static final TargetHandler<Image> HANDLER =
-      ExtensionInterpretation.extenstion(Extension.MULTIMEDIA)
+      ExtensionInterpretation.extenstion(Extension.IMAGE)
           .to(Image::new)
-          .map(DcTerm.references, ImageInterpreter::parseAndsetReferences)
           .map(DcTerm.identifier, ImageInterpreter::parseAndsetIdentifier)
-          .map(DcTerm.format, ImageInterpreter::parseAndSetFormat)
+          .map(DcTerm.references, ImageInterpreter::parseAndsetReferences)
           .map(DcTerm.created, ImageInterpreter::parseAndSetCreated)
           .map(DcTerm.title, Image::setTitle)
           .map(DcTerm.description, Image::setDescription)
+          .map(DcTerm.spatial, Image::setSpatial)
+          .map(DcTerm.format, Image::setFormat)
+          .map(DcTerm.creator, Image::setCreator)
           .map(DcTerm.contributor, Image::setContributor)
           .map(DcTerm.publisher, Image::setPublisher)
           .map(DcTerm.audience, Image::setAudience)
-          .map(DcTerm.creator, Image::setCreator)
           .map(DcTerm.license, Image::setLicense)
           .map(DcTerm.rightsHolder, Image::setRightsHolder)
           .map(DwcTerm.datasetID, Image::setDatasetId)
+          .mapOne("http://www.w3.org/2003/01/geo/wgs84_pos#longitude", ImageInterpreter::parseAndSetLongitude)
+          .mapOne("http://www.w3.org/2003/01/geo/wgs84_pos#latitude", ImageInterpreter::parseAndSetLatitude)
+          .postMap(ImageInterpreter::parseAndSetLatLng)
           .skipIf(ImageInterpreter::checkLinks);
 
   private ImageInterpreter() {}
@@ -69,28 +76,29 @@ public class ImageInterpreter {
     mr.getIssues().setIssueList(result.getIssuesAsList());
   }
 
+
   /**
    *
    */
-  private static void parseAndsetReferences(Image m, String v) {
+  private static void parseAndsetReferences(Image i, String v) {
     URI uri = UrlParser.parse(v);
-    Optional.ofNullable(uri).map(URI::toString).ifPresent(m::setReferences);
+    Optional.ofNullable(uri).map(URI::toString).ifPresent(i::setReferences);
   }
 
   /**
    *
    */
-  private static void parseAndsetIdentifier(Image m, String v) {
+  private static void parseAndsetIdentifier(Image i, String v) {
     URI uri = UrlParser.parse(v);
-    Optional.ofNullable(uri).map(URI::toString).ifPresent(m::setIdentifier);
+    Optional.ofNullable(uri).map(URI::toString).ifPresent(i::setIdentifier);
   }
 
   /**
    *
    */
-  private static List<String> parseAndSetCreated(Image m, String v) {
+  private static List<String> parseAndSetCreated(Image i, String v) {
     ParsedTemporal parsed = TemporalParser.parse(v);
-    parsed.getFrom().map(Temporal::toString).ifPresent(m::setCreated);
+    parsed.getFrom().map(Temporal::toString).ifPresent(i::setCreated);
 
     return parsed.getIssueList();
   }
@@ -98,19 +106,41 @@ public class ImageInterpreter {
   /**
    *
    */
-  private static void parseAndSetFormat(Image m, String v) {
-    String mimeType = MEDIA_PARSER.parseMimeType(v);
-    if (Strings.isNullOrEmpty(mimeType)) {
-      mimeType = MEDIA_PARSER.parseMimeType(m.getIdentifier());
-    }
-    m.setFormat(mimeType);
+  private static String parseAndSetLongitude(Image i, String v) {
+    Double lat = NumberParser.parseDouble(v);
+    Optional.ofNullable(lat).ifPresent(i::setLatitude);
+    return lat == null ? COORDINATE_INVALID.name() : null;
   }
 
   /**
    *
    */
-  private static Optional<String> checkLinks(Image m) {
-    if (m.getReferences() == null && m.getIdentifier() == null) {
+  private static String parseAndSetLatitude(Image i, String v) {
+    Double lng = NumberParser.parseDouble(v);
+    Optional.ofNullable(lng).ifPresent(i::setLongitude);
+    return lng == null ? COORDINATE_INVALID.name() : null;
+  }
+
+  /**
+   *
+   */
+  private static List<String> parseAndSetLatLng(Image i) {
+    String lat = i.getLatitude().toString();
+    String lng = i.getLongitude().toString();
+    ParsedField<LatLng> latLng = CoordinateParseUtils.parseLatLng(lat, lng);
+    if (latLng.isSuccessful()) {
+      LatLng result = latLng.getResult();
+      i.setLatitude(result.getLatitude());
+      i.setLongitude(result.getLongitude());
+    }
+    return latLng.getIssues();
+  }
+
+  /**
+   *
+   */
+  private static Optional<String> checkLinks(Image i) {
+    if (i.getReferences() == null && i.getIdentifier() == null) {
       return Optional.of(MULTIMEDIA_URI_INVALID.name());
     }
     return Optional.empty();
