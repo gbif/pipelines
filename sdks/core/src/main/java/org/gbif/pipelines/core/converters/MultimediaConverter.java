3681f9a619a50f46ec9123f4fcbddc884f010e95
nikolay.volik <nikolay.volik@hotmail.com>
2019-02-27 14:47:56 +0100

    Fixed NPE

--- sdks/core/src/main/java/org/gbif/pipelines/core/converters/MultimediaConverter.java
+++ sdks/core/src/main/java/org/gbif/pipelines/core/converters/MultimediaConverter.java
@@ -20,6 +20,16 @@ public class MultimediaConverter {
   private MultimediaConverter() {}
 
   public static MultimediaRecord merge(MultimediaRecord mr, ImageRecord ir, AudubonRecord ar) {
+    MultimediaRecord record = MultimediaRecord.newBuilder().setId(mr.getId()).build();
+
+    boolean isMrEmpty = mr.getMultimediaItems() == null && mr.getIssues().getIssueList().isEmpty();
+    boolean isIrEmpty = ir.getImageItems() == null && ir.getIssues().getIssueList().isEmpty();
+    boolean isArEmpty = ar.getAudubonItems() == null && ar.getIssues().getIssueList().isEmpty();
+
+    if (isMrEmpty && isIrEmpty && isArEmpty) {
+      return record;
+    }
+
     Set<String> issues = new HashSet<>();
     issues.addAll(mr.getIssues().getIssueList());
     issues.addAll(ir.getIssues().getIssueList());
@@ -29,8 +39,6 @@ public class MultimediaConverter {
     putAllMultimediaRecord(multimediaMap, mr);
     putAllImageRecord(multimediaMap, ir);
     putAllAudubonRecord(multimediaMap, ar);
-
-    MultimediaRecord record = new MultimediaRecord();
 
     if (!multimediaMap.isEmpty()) {
       record.setMultimediaItems(new ArrayList<>(multimediaMap.values()));
@@ -46,64 +54,61 @@ public class MultimediaConverter {
   private static void putAllMultimediaRecord(Map<String, Multimedia> map, MultimediaRecord mr) {
     Optional.ofNullable(mr.getMultimediaItems())
         .filter(i -> !i.isEmpty())
-        .get()
-        .stream()
-        .filter(m -> Strings.isNullOrEmpty(m.getReferences()) || Strings.isNullOrEmpty(m.getIdentifier()))
-        .forEach(r -> {
-          String key = Optional.ofNullable(r.getIdentifier()).orElse(r.getReferences());
-          map.put(key, r);
-        });
+        .ifPresent(c -> c.stream()
+            .filter(m -> Strings.isNullOrEmpty(m.getReferences()) || Strings.isNullOrEmpty(m.getIdentifier()))
+            .forEach(r -> {
+              String key = Optional.ofNullable(r.getIdentifier()).orElse(r.getReferences());
+              map.put(key, r);
+            }));
   }
 
   private static void putAllImageRecord(Map<String, Multimedia> map, ImageRecord ir) {
     Optional.ofNullable(ir.getImageItems())
         .filter(i -> !i.isEmpty())
-        .get()
-        .stream()
-        .filter(m -> Strings.isNullOrEmpty(m.getReferences()) || Strings.isNullOrEmpty(m.getIdentifier()))
-        .forEach(r -> {
-          String key = Optional.ofNullable(r.getIdentifier()).orElse(r.getReferences());
-          Multimedia multimedia = Multimedia.newBuilder()
-              .setAudience(r.getAudience())
-              .setContributor(r.getContributor())
-              .setCreated(r.getCreated())
-              .setCreator(r.getCreator())
-              .setDatasetId(r.getDatasetId())
-              .setDescription(r.getDescription())
-              .setFormat(r.getFormat())
-              .setIdentifier(r.getIdentifier())
-              .setLicense(r.getLicense())
-              .setPublisher(r.getPublisher())
-              .setReferences(r.getReferences())
-              .setRightsHolder(r.getRightsHolder())
-              .setTitle(r.getTitle())
-              .setType(MediaType.StillImage.name())
-              .build();
-          map.putIfAbsent(key, multimedia);
-        });
+        .ifPresent(c -> c.stream()
+            .filter(m -> Strings.isNullOrEmpty(m.getReferences()) || Strings.isNullOrEmpty(m.getIdentifier()))
+            .forEach(r -> {
+              String key = Optional.ofNullable(r.getIdentifier()).orElse(r.getReferences());
+              Multimedia multimedia = Multimedia.newBuilder()
+                  .setAudience(r.getAudience())
+                  .setContributor(r.getContributor())
+                  .setCreated(r.getCreated())
+                  .setCreator(r.getCreator())
+                  .setDatasetId(r.getDatasetId())
+                  .setDescription(r.getDescription())
+                  .setFormat(r.getFormat())
+                  .setIdentifier(r.getIdentifier())
+                  .setLicense(r.getLicense())
+                  .setPublisher(r.getPublisher())
+                  .setReferences(r.getReferences())
+                  .setRightsHolder(r.getRightsHolder())
+                  .setTitle(r.getTitle())
+                  .setType(MediaType.StillImage.name())
+                  .build();
+              map.putIfAbsent(key, multimedia);
+            }));
   }
 
   private static void putAllAudubonRecord(Map<String, Multimedia> map, AudubonRecord ar) {
     Optional.ofNullable(ar.getAudubonItems())
         .filter(i -> !i.isEmpty())
-        .get()
-        .stream()
-        .filter(m -> Strings.isNullOrEmpty(m.getAccessUri()))
-        .forEach(r -> {
-          String key = r.getAccessUri();
-          Multimedia multimedia = Multimedia.newBuilder()
-              .setCreated(r.getCreateDate())
-              .setCreator(r.getCreator())
-              .setDescription(r.getDescription())
-              .setFormat(r.getFormat())
-              .setIdentifier(r.getAccessUri())
-              .setLicense(r.getLicensingException())
-              .setRightsHolder(r.getRights())
-              .setSource(r.getSource())
-              .setTitle(r.getTitle())
-              .setType(r.getType())
-              .build();
-          map.putIfAbsent(key, multimedia);
-        });
+        .ifPresent(c -> c.stream()
+            .filter(m -> Strings.isNullOrEmpty(m.getAccessUri()))
+            .forEach(r -> {
+              String key = r.getAccessUri();
+              Multimedia multimedia = Multimedia.newBuilder()
+                  .setCreated(r.getCreateDate())
+                  .setCreator(r.getCreator())
+                  .setDescription(r.getDescription())
+                  .setFormat(r.getFormat())
+                  .setIdentifier(r.getAccessUri())
+                  .setLicense(r.getLicensingException())
+                  .setRightsHolder(r.getRights())
+                  .setSource(r.getSource())
+                  .setTitle(r.getTitle())
+                  .setType(r.getType())
+                  .build();
+              map.putIfAbsent(key, multimedia);
+            }));
   }
 }
