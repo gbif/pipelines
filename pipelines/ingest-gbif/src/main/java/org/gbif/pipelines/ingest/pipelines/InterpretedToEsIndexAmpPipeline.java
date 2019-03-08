530c5d2bbc3aa9c0cc5d63e1b9e79ed2727e0671
nvolik <nikolay.volik@hotmail.com>
2019-03-08 12:05:09 +0100

    Added Amplification pipelines

---
+++ pipelines/ingest-gbif/src/main/java/org/gbif/pipelines/ingest/pipelines/InterpretedToEsIndexAmpPipeline.java
@@ -0,0 +1,114 @@
+package org.gbif.pipelines.ingest.pipelines;
+
+import java.util.function.UnaryOperator;
+
+import org.gbif.pipelines.common.PipelinesVariables.Pipeline.Indexing;
+import org.gbif.pipelines.core.converters.GbifJsonConverter;
+import org.gbif.pipelines.ingest.options.EsIndexingPipelineOptions;
+import org.gbif.pipelines.ingest.options.PipelinesOptionsFactory;
+import org.gbif.pipelines.ingest.utils.FsUtils;
+import org.gbif.pipelines.io.avro.AmplificationRecord;
+import org.gbif.pipelines.transforms.extension.AmplificationTransform;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.io.elasticsearch.ElasticsearchIO;
+import org.apache.beam.sdk.metrics.Counter;
+import org.apache.beam.sdk.metrics.Metrics;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+import org.slf4j.MDC;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import static org.gbif.pipelines.common.PipelinesVariables.Metrics.AVRO_TO_JSON_COUNT;
+import static org.gbif.pipelines.common.PipelinesVariables.Pipeline.AVRO_EXTENSION;
+
+/**
+ * NOTE: REMEMBER THAT THIS PIPELINE MUST BE STARTED AFTER {@link InterpretedToEsIndexPipeline}
+ * <p>
+ * Pipeline sequence:
+ *
+ * <pre>
+ *    1) Reads avro files {@link org.gbif.pipelines.io.avro.AmplificationRecord}
+ *    3) Converts to json model (resources/elasticsearch/es-occurrence-schema.json)
+ *    4) Updates existing ES index
+ * </pre>
+ *
+ * <p>How to run:
+ *
+ * <pre>{@code
+ * java -cp target/ingest-gbif-BUILD_VERSION-shaded.jar org.gbif.pipelines.base.pipelines.InterpretedToEsIndexAmpPipeline some.properties
+ *
+ * or pass all parameters:
+ *
+ * java -cp target/ingest-gbif-BUILD_VERSION-shaded.jar org.gbif.pipelines.base.pipelines.InterpretedToEsIndexAmpPipeline
+ * --datasetId=9f747cff-839f-4485-83a1-f10317a92a82
+ * --attempt=1
+ * --runner=SparkRunner
+ * --targetPath=hdfs://ha-nn/output/
+ * --esIndexName=pipeline
+ * --esHosts=http://ADDRESS:9200,http://ADDRESS:9200,http://ADDRESS:9200
+ * --hdfsSiteConfig=/config/hdfs-site.xml
+ * --coreSiteConfig=/config/core-site.xml
+ *
+ * }</pre>
+ */
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class InterpretedToEsIndexAmpPipeline {
+
+  public static void main(String[] args) {
+    EsIndexingPipelineOptions options = PipelinesOptionsFactory.createIndexing(args);
+    run(options);
+  }
+
+  public static void run(EsIndexingPipelineOptions options) {
+
+    MDC.put("datasetId", options.getDatasetId());
+    MDC.put("attempt", options.getAttempt().toString());
+
+    log.info("Adding step 1: Options");
+    UnaryOperator<String> pathFn = t -> FsUtils.buildPathInterpret(options, t, "*" + AVRO_EXTENSION);
+
+    Pipeline p = Pipeline.create(options);
+
+    log.info("Adding step 2: Reading avros and converting into a json object");
+    PCollection<String> jsonCollection =
+        p.apply("Read Amplification", AmplificationTransform.read(pathFn))
+            .apply("Merging into json", ParDo.of(new DoFn<AmplificationRecord, String>() {
+              private final Counter counter = Metrics.counter(GbifJsonConverter.class, AVRO_TO_JSON_COUNT);
+
+              @ProcessElement
+              public void processElement(ProcessContext c) {
+                String json = GbifJsonConverter.create(c.element()).buildJson().toString();
+
+                c.output(json);
+
+                counter.inc();
+              }
+            }));
+
+    log.info("Adding step 4: Elasticsearch indexing");
+    ElasticsearchIO.ConnectionConfiguration esConfig =
+        ElasticsearchIO.ConnectionConfiguration.create(
+            options.getEsHosts(), options.getEsIndexName(), Indexing.INDEX_TYPE);
+
+    jsonCollection.apply(
+        ElasticsearchIO.write()
+            .withUsePartialUpdate(true)
+            .withConnectionConfiguration(esConfig)
+            .withMaxBatchSizeBytes(options.getEsMaxBatchSizeBytes())
+            .withMaxBatchSize(options.getEsMaxBatchSize())
+            .withIdFn(input -> input.get("id").asText()));
+
+    log.info("Running the pipeline");
+    PipelineResult result = p.run();
+    result.waitUntilFinish();
+
+    log.info("Pipeline has been finished");
+  }
+}
---
+++ pipelines/ingest-gbif/src/main/java/org/gbif/pipelines/ingest/pipelines/VerbatimToInterpretedAmpPipeline.java
@@ -0,0 +1,91 @@
+package org.gbif.pipelines.ingest.pipelines;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.function.UnaryOperator;
+
+import org.gbif.pipelines.ingest.options.InterpretationPipelineOptions;
+import org.gbif.pipelines.ingest.options.PipelinesOptionsFactory;
+import org.gbif.pipelines.ingest.utils.FsUtils;
+import org.gbif.pipelines.io.avro.ExtendedRecord;
+import org.gbif.pipelines.transforms.core.VerbatimTransform;
+import org.gbif.pipelines.transforms.extension.AmplificationTransform;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
+import org.slf4j.MDC;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * NOTE: REMEMBER THAT THIS PIPELINE MUST BE STARTED AFTER {@link VerbatimToInterpretedPipeline}
+ * <p>
+ * Pipeline sequence:
+ *
+ * <pre>
+ *    1) Reads verbatim.avro file
+ *    2) Interprets and converts avro {@link ExtendedRecord} file to {@link org.gbif.pipelines.io.avro.AmplificationRecord}
+ *    3) Writes data to independent files
+ * </pre>
+ *
+ * <p>How to run:
+ *
+ * <pre>{@code
+ * java -cp target/ingest-gbif-BUILD_VERSION-shaded.jar org.gbif.pipelines.ingest.pipelines.VerbatimToInterpretedPipeline some.properties
+ *
+ * or pass all parameters:
+ *
+ * java -cp target/ingest-gbif-BUILD_VERSION-shaded.jar org.gbif.pipelines.ingest.pipelines.VerbatimToInterpretedAmpPipeline
+ * --wsProperties=/some/path/to/output/ws.properties
+ * --datasetId=0057a720-17c9-4658-971e-9578f3577cf5
+ * --attempt=1
+ * --runner=SparkRunner
+ * --targetPath=/some/path/to/output/
+ * --inputPath=/some/path/to/output/0057a720-17c9-4658-971e-9578f3577cf5/1/
+ *
+ * }</pre>
+ */
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class VerbatimToInterpretedAmpPipeline {
+
+  public static void main(String[] args) {
+    InterpretationPipelineOptions options = PipelinesOptionsFactory.createInterpretation(args);
+    run(options);
+  }
+
+  public static void run(InterpretationPipelineOptions options) {
+
+    String datasetId = options.getDatasetId();
+    String attempt = options.getAttempt().toString();
+
+    FsUtils.deleteInterpretIfExist(options.getHdfsSiteConfig(), datasetId, attempt, options.getInterpretationTypes());
+
+    MDC.put("datasetId", datasetId);
+    MDC.put("attempt", attempt);
+
+    String id = Long.toString(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC));
+
+    UnaryOperator<String> pathFn = t -> FsUtils.buildPathInterpret(options, t, id);
+
+    log.info("Creating a pipeline from options");
+    Pipeline p = Pipeline.create(options);
+
+    log.info("Adding pipeline transforms");
+    p.apply("Read Verbatim", VerbatimTransform.read(pathFn))
+        .apply("Interpret amplification", AmplificationTransform.interpret())
+        .apply("Write amplification to avro", AmplificationTransform.write(pathFn));
+
+    log.info("Running the pipeline");
+    PipelineResult result = p.run();
+    result.waitUntilFinish();
+
+    log.info("Deleting beam temporal folders");
+    String tempPath = String.join("/", options.getTargetPath(), datasetId, attempt);
+    FsUtils.deleteDirectoryByPrefix(options.getHdfsSiteConfig(), tempPath, ".temp-beam");
+
+    log.info("Pipeline has been finished");
+  }
+}
