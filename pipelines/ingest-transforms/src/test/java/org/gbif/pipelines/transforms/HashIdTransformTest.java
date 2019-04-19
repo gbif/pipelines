48aad9cccb290362aa271a78bad3843c394928bd
Nikolay Volik <nvolik@gbif.org>
2019-04-19 10:30:40 +0200

    Added Ignore

---
+++ pipelines/ingest-transforms/src/test/java/org/gbif/pipelines/transforms/HashIdTransformTest.java
@@ -0,0 +1,7 @@
+package org.gbif.pipelines.transforms;
+
+import org.junit.Ignore;
+
+@Ignore("ADD TESTS!")
+public class HashIdTransformTest {
+}
--- pipelines/ingest-transforms/src/test/java/org/gbif/pipelines/transforms/converters/OccurrenceExtensionTransformTest.java
+++ pipelines/ingest-transforms/src/test/java/org/gbif/pipelines/transforms/converters/OccurrenceExtensionTransformTest.java
@@ -1,5 +1,8 @@
 package org.gbif.pipelines.transforms.converters;
 
+import org.junit.Ignore;
+
+@Ignore("ADD TESTS!")
 public class OccurrenceExtensionTransformTest {
 
 }
