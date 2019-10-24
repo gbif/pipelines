94a7330d5e7ae6cdbbc888bee52ca28223f592b7
nvolik <nikolay.volik@hotmail.com>
2019-10-24 10:22:27 +0200

    Fixed timezone parsing, added pattern without seconds

--- pipelines/ingest-hdfs-table/src/main/java/org/gbif/pipelines/transforms/hdfs/converters/OccurrenceHdfsRecordConverter.java
+++ pipelines/ingest-hdfs-table/src/main/java/org/gbif/pipelines/transforms/hdfs/converters/OccurrenceHdfsRecordConverter.java
@@ -90,7 +90,7 @@ public class OccurrenceHdfsRecordConverter {
   //Supported Date formats
   private static final DateTimeFormatter FORMATTER =
     DateTimeFormatter.ofPattern(
-      "[yyyy-MM-dd'T'HH:mm:ssXXX][yyyy-MM-dd'T'HH:mm:ss.SSS XXX][yyyy-MM-dd'T'HH:mm:ss.SSSXXX][yyyy-MM-dd'T'HH:mm:ss.SSS]"
+      "[yyyy-MM-dd'T'HH:mm:ssXXX][yyyy-MM-dd'T'HH:mmXXX][yyyy-MM-dd'T'HH:mm:ss.SSS XXX][yyyy-MM-dd'T'HH:mm:ss.SSSXXX][yyyy-MM-dd'T'HH:mm:ss.SSS]"
       + "[yyyy-MM-dd'T'HH:mm:ss][yyyy-MM-dd'T'HH:mm:ss XXX][yyyy-MM-dd'T'HH:mm:ssXXX][yyyy-MM-dd'T'HH:mm:ss]"
       + "[yyyy-MM-dd'T'HH:mm][yyyy-MM-dd][yyyy-MM][yyyy]");
 
