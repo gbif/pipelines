02f20d896bc542d23e46ea055d442dd5e15b79ae
nvolik <nikolay.volik@hotmail.com>
2019-11-25 12:25:15 +0100

    Added partial implementation of InterpretedToEsIndexPipeline.java

--- pipelines/ingest-gbif-java/src/main/java/org/gbif/pipelines/ingest/java/pipelines/InterpretedToEsIndexExtendedPipeline.java
+++ pipelines/ingest-gbif-java/src/main/java/org/gbif/pipelines/ingest/java/pipelines/InterpretedToEsIndexExtendedPipeline.java
@@ -1,5 +1,8 @@
 package org.gbif.pipelines.ingest.java.pipelines;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.gbif.api.model.pipelines.StepType;
 import org.gbif.pipelines.ingest.options.EsIndexingPipelineOptions;
 import org.gbif.pipelines.ingest.options.PipelinesOptionsFactory;
@@ -11,7 +14,6 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-
 @Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class InterpretedToEsIndexExtendedPipeline {
@@ -22,11 +24,21 @@ public class InterpretedToEsIndexExtendedPipeline {
   }
 
   public static void run(EsIndexingPipelineOptions options) {
+    ExecutorService executor = Executors.newWorkStealingPool();
+    try {
+      run(options, executor);
+    } finally {
+      executor.shutdown();
+    }
+  }
+
+  public static void run(EsIndexingPipelineOptions options, ExecutorService executor) {
     MDC.put("datasetId", options.getDatasetId());
     MDC.put("attempt", options.getAttempt().toString());
     MDC.put("step", StepType.INTERPRETED_TO_INDEX.name());
 
-    org.gbif.pipelines.ingest.pipelines.InterpretedToEsIndexExtendedPipeline.run(options, () -> InterpretedToEsIndexPipeline.run(options));
+    org.gbif.pipelines.ingest.pipelines.InterpretedToEsIndexExtendedPipeline.run(options,
+        () -> InterpretedToEsIndexPipeline.run(options, executor));
 
     FsUtils.removeTmpDirectory(options);
     log.info("Finished main indexing pipeline");
