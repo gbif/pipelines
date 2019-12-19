3535e91bd1e35ae367e2d930436c44567e810212
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2019-12-19 17:19:34 +0100

    fix tests

--- pipelines/ingest-hdfs-table/src/test/java/org/gbif/pipelines/transforms/hdfs/converters/OccurrenceHdfsRecordConverterTest.java
+++ pipelines/ingest-hdfs-table/src/test/java/org/gbif/pipelines/transforms/hdfs/converters/OccurrenceHdfsRecordConverterTest.java
@@ -1,6 +1,5 @@
 package org.gbif.pipelines.transforms.hdfs.converters;
 
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
@@ -364,21 +363,50 @@ public class OccurrenceHdfsRecordConverterTest {
     Assert.assertArrayEquals(issues, hdfsRecord.getIssue().toArray(new String[issues.length]));
   }
 
-  @Test(expected = Test.None.class)
-  public void dateParserTest() throws ParseException {
-    STRING_TO_DATE.apply("2019");
+  @Test
+  public void dateParserTest() {
+    Date date = STRING_TO_DATE.apply("2019");
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(date);
+    assertEquals(2019, cal.get(Calendar.YEAR));
+    assertEquals(0, cal.get(Calendar.MONTH));
+    assertEquals(1, cal.get(Calendar.DAY_OF_MONTH));
 
-    STRING_TO_DATE.apply("2019-04");
+    date = STRING_TO_DATE.apply("2019-04");
+    cal.setTime(date);
+    assertEquals(2019, cal.get(Calendar.YEAR));
+    assertEquals(3, cal.get(Calendar.MONTH));
+    assertEquals(1, cal.get(Calendar.DAY_OF_MONTH));
 
-    STRING_TO_DATE.apply("2019-04-01");
+    date = STRING_TO_DATE.apply("2019-04-02");
+    cal.setTime(date);
+    assertEquals(2019, cal.get(Calendar.YEAR));
+    assertEquals(3, cal.get(Calendar.MONTH));
+    assertEquals(2, cal.get(Calendar.DAY_OF_MONTH));
 
-    STRING_TO_DATE.apply("2019-04-15T17:17:48.191 +02:00");
+    date = STRING_TO_DATE.apply("2019-04-15T17:17:48.191 +02:00");
+    cal.setTime(date);
+    assertEquals(2019, cal.get(Calendar.YEAR));
+    assertEquals(3, cal.get(Calendar.MONTH));
+    assertEquals(15, cal.get(Calendar.DAY_OF_MONTH));
 
-    STRING_TO_DATE.apply("2019-04-15T17:17:48.191");
+    date = STRING_TO_DATE.apply("2019-04-15T17:17:48.191");
+    cal.setTime(date);
+    assertEquals(2019, cal.get(Calendar.YEAR));
+    assertEquals(3, cal.get(Calendar.MONTH));
+    assertEquals(15, cal.get(Calendar.DAY_OF_MONTH));
 
-    STRING_TO_DATE.apply("2019-04-15T17:17:48.023+02:00");
+    date = STRING_TO_DATE.apply("2019-04-15T17:17:48.023+02:00");
+    cal.setTime(date);
+    assertEquals(2019, cal.get(Calendar.YEAR));
+    assertEquals(3, cal.get(Calendar.MONTH));
+    assertEquals(15, cal.get(Calendar.DAY_OF_MONTH));
 
-    STRING_TO_DATE.apply("2019-11-12T13:24:56.963591");
+    date = STRING_TO_DATE.apply("2019-11-12T13:24:56.963591");
+    cal.setTime(date);
+    assertEquals(2019, cal.get(Calendar.YEAR));
+    assertEquals(10, cal.get(Calendar.MONTH));
+    assertEquals(12, cal.get(Calendar.DAY_OF_MONTH));
   }
 
   @Test
