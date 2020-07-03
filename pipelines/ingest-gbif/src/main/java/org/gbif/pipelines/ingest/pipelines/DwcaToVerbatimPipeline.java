549fa9ce9df30abbca1e3d290c9963a0672a6b60
Dave Martin <djtfmartin@users.noreply.github.com>
2020-07-03 13:08:01 +0100

    Fixes #318
    Fix for #318

--- pipelines/ingest-gbif/src/main/java/org/gbif/pipelines/ingest/pipelines/DwcaToVerbatimPipeline.java
+++ pipelines/ingest-gbif/src/main/java/org/gbif/pipelines/ingest/pipelines/DwcaToVerbatimPipeline.java
@@ -1,23 +1,19 @@
 package org.gbif.pipelines.ingest.pipelines;
 
-import java.nio.file.Paths;
-
+import lombok.extern.slf4j.Slf4j;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
 import org.gbif.api.model.pipelines.StepType;
-import org.gbif.pipelines.common.PipelinesVariables.Pipeline.Conversion;
+import org.gbif.pipelines.common.PipelinesVariables;
 import org.gbif.pipelines.common.beam.DwcaIO;
-import org.gbif.pipelines.ingest.options.BasePipelineOptions;
+import org.gbif.pipelines.ingest.options.InterpretationPipelineOptions;
 import org.gbif.pipelines.ingest.options.PipelinesOptionsFactory;
 import org.gbif.pipelines.ingest.utils.FsUtils;
 import org.gbif.pipelines.ingest.utils.MetricsHandler;
 import org.gbif.pipelines.transforms.core.VerbatimTransform;
-
-import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.PipelineResult;
 import org.slf4j.MDC;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.nio.file.Paths;
 
 /**
  * Pipeline sequence:
@@ -48,38 +44,39 @@ import lombok.extern.slf4j.Slf4j;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DwcaToVerbatimPipeline {
 
-  public static void main(String[] args) {
-    BasePipelineOptions options = PipelinesOptionsFactory.create(BasePipelineOptions.class, args);
-    run(options);
-  }
+    public static void main(String[] args) {
+        InterpretationPipelineOptions options = PipelinesOptionsFactory.createInterpretation(args);
+        run(options);
+    }
 
-  public static void run(BasePipelineOptions options) {
+    public static void run(InterpretationPipelineOptions options) {
 
-    MDC.put("datasetKey", options.getDatasetId());
-    MDC.put("attempt", options.getAttempt().toString());
-    MDC.put("step", StepType.DWCA_TO_VERBATIM.name());
+        MDC.put("datasetId", options.getDatasetId());
+        MDC.put("attempt", options.getAttempt().toString());
+        MDC.put("step", StepType.DWCA_TO_VERBATIM.name());
 
-    log.info("Adding step 1: Options");
-    String inputPath = options.getInputPath();
-    String targetPath = FsUtils.buildDatasetAttemptPath(options, Conversion.FILE_NAME, false);
-    String tmpPath = FsUtils.getTempDir(options);
+        log.info("Adding step 1: Options");
+        String inputPath = options.getInputPath();
+        String targetPath = FsUtils.buildDatasetAttemptPath(options, PipelinesVariables.Pipeline.Conversion.FILE_NAME, false);
 
-    boolean isDir = Paths.get(inputPath).toFile().isDirectory();
+        String tmpPath = FsUtils.getTempDir(options);
 
-    DwcaIO.Read reader = isDir ? DwcaIO.Read.fromLocation(inputPath) : DwcaIO.Read.fromCompressed(inputPath, tmpPath);
+        boolean isDir = Paths.get(inputPath).toFile().isDirectory();
 
-    log.info("Adding step 2: Pipeline steps");
-    Pipeline p = Pipeline.create(options);
+        DwcaIO.Read reader = isDir ? DwcaIO.Read.fromLocation(inputPath) : DwcaIO.Read.fromCompressed(inputPath, tmpPath);
 
-    p.apply("Read from Darwin Core Archive", reader)
-        .apply("Write to avro", VerbatimTransform.create().write(targetPath).withoutSharding());
+        log.info("Adding step 2: Pipeline steps");
+        Pipeline p = Pipeline.create(options);
 
-    log.info("Running the pipeline");
-    PipelineResult result = p.run();
-    result.waitUntilFinish();
+        p.apply("Read from Darwin Core Archive", reader)
+                .apply("Write to avro", VerbatimTransform.create().write(targetPath).withoutSharding());
 
-    MetricsHandler.saveCountersToTargetPathFile(options, result.metrics());
+        log.info("Running the pipeline");
+        PipelineResult result = p.run();
+        result.waitUntilFinish();
 
-    log.info("Pipeline has been finished");
-  }
+        MetricsHandler.saveCountersToTargetPathFile(options, result.metrics());
+
+        log.info("Pipeline has been finished");
+    }
 }
