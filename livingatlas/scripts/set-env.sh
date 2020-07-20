fd145ec25a20d82e5dbf4cf1cd23f5550602bf5f
Dave Martin <djtfmartin@gmail.com>
2020-07-20 13:27:13 +0100

    fixes and closes https://github.com/AtlasOfLivingAustralia/la-pipelines/issues/95 - hadoop "No FileSystem for scheme: hdfs" error added a "local-hdfs" YAML config file for reference. updated set-env.sh JAR version

--- livingatlas/scripts/set-env.sh
+++ livingatlas/scripts/set-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export PIPELINES_JAR="$PWD/../pipelines/target/pipelines-1.0-SNAPSHOT-shaded.jar"
+export PIPELINES_JAR="$PWD/../pipelines/target/pipelines-2.5.5-SNAPSHOT-shaded.jar"
 
 export SPARK_TMP="/data/spark-tmp"
 
