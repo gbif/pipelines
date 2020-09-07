2c8ee336faadb55509b064a7c5ca7a775c3bd016
Nikolay Volik <nvolik@gbif.org>
2020-09-07 14:23:23 +0200

    Use common spark and distributed config classes

--- gbif/coordinator/crawler/src/main/java/org/gbif/pipelines/crawler/hdfs/HdfsViewConfiguration.java
+++ gbif/coordinator/crawler/src/main/java/org/gbif/pipelines/crawler/hdfs/HdfsViewConfiguration.java
@@ -7,6 +7,8 @@ import javax.validation.constraints.NotNull;
 import lombok.ToString;
 import org.gbif.pipelines.common.PipelinesVariables.Pipeline;
 import org.gbif.pipelines.common.configs.BaseConfiguration;
+import org.gbif.pipelines.common.configs.DistributedConfiguration;
+import org.gbif.pipelines.common.configs.SparkConfiguration;
 import org.gbif.pipelines.common.configs.StepConfiguration;
 
 /** Configuration required to start Hdfs View processing */
@@ -15,72 +17,17 @@ public class HdfsViewConfiguration implements BaseConfiguration {
 
   @ParametersDelegate @Valid @NotNull public StepConfiguration stepConfig = new StepConfiguration();
 
+  @ParametersDelegate @Valid public SparkConfiguration sparkConfig = new SparkConfiguration();
+
+  @ParametersDelegate @Valid
+  public DistributedConfiguration distributedConfig = new DistributedConfiguration();
+
   @Parameter(names = "--repository-target-path")
   @NotNull
   public String repositoryTargetPath;
 
   @Parameter(names = "--meta-file-name")
   public String metaFileName = Pipeline.INTERPRETED_TO_HDFS + ".yml";
-
-  @Parameter(names = "--distributed-main-class")
-  public String distributedMainClass;
-
-  @Parameter(names = "--spark-parallelism-min")
-  public int sparkParallelismMin;
-
-  @Parameter(names = "--spark-parallelism-max")
-  public int sparkParallelismMax;
-
-  @Parameter(names = "--spark-memory-overhead")
-  public int sparkMemoryOverhead;
-
-  @Parameter(names = "--spark-executor-memory-gb-min")
-  public int sparkExecutorMemoryGbMin;
-
-  @Parameter(names = "--spark-executor-memory-gb-max")
-  public int sparkExecutorMemoryGbMax;
-
-  @Parameter(names = "--spark-executor-cores")
-  public int sparkExecutorCores;
-
-  @Parameter(names = "--spark-executor-numbers-min")
-  public int sparkExecutorNumbersMin;
-
-  @Parameter(names = "--spark-executor-numbers-max")
-  public int sparkExecutorNumbersMax;
-
-  @Parameter(names = "--spark-driver-memory")
-  public String sparkDriverMemory;
-
-  @Parameter(names = "--spark-records-per-thread")
-  public int sparkRecordsPerThread;
-
-  @Parameter(names = "--standalone-number-threads")
-  public Integer standaloneNumberThreads;
-
-  @Parameter(names = "--deploy-mode")
-  public String deployMode;
-
-  @Parameter(names = "--metrics-properties-path")
-  public String metricsPropertiesPath;
-
-  @Parameter(names = "--extra-class-path")
-  public String extraClassPath;
-
-  @Parameter(names = "--driver-java-options")
-  public String driverJavaOptions;
-
-  @Parameter(names = "--distributed-jar-path")
-  public String distributedJarPath;
-
-  @Parameter(names = "--other-user")
-  public String otherUser;
-
-  @Parameter(names = "--process-error-directory")
-  public String processErrorDirectory;
-
-  @Parameter(names = "--process-output-directory")
-  public String processOutputDirectory;
 
   @Parameter(names = "--hdfs-avro-coefficient-ratio")
   public int hdfsAvroCoefficientRatio = 75;
@@ -92,8 +39,8 @@ public class HdfsViewConfiguration implements BaseConfiguration {
   @NotNull
   public String processRunner;
 
-  @Parameter(names = "--yarn-queue")
-  public String yarnQueue;
+  @Parameter(names = "--standalone-number-threads")
+  public Integer standaloneNumberThreads;
 
   @Parameter(names = "--pipelines-config")
   @Valid
