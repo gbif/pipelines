49ad99434f63d6167e00fd78dd1d9e1d735147ce
Nikolay Volik <nvolik@gbif.org>
2020-10-09 11:55:14 +0200

    Fix config issue

--- gbif/coordinator/crawler/src/main/java/org/gbif/pipelines/common/configs/DistributedConfiguration.java
+++ gbif/coordinator/crawler/src/main/java/org/gbif/pipelines/common/configs/DistributedConfiguration.java
@@ -1,7 +1,9 @@
 package org.gbif.pipelines.common.configs;
 
 import com.beust.jcommander.Parameter;
+import lombok.ToString;
 
+@ToString
 public class DistributedConfiguration {
 
   @Parameter(names = "--yarn-queue")
