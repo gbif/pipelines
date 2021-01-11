7ea0e11de402b7b640bb6dd5096b7dedda9711be
Dave Martin <djtfmartin@gmail.com>
2021-01-11 11:13:56 +0000

    script fix

--- livingatlas/migration/scripts/uuid-export.sh
+++ livingatlas/migration/scripts/uuid-export.sh
@@ -1,2 +1,3 @@
 cqlsh -k occ -e "COPY occ_uuid TO '/data/uuid-exports/occ_uuid.csv'"
 cqlsh -k occ -e "COPY occ (rowkey, \"firstLoaded\") TO '/data/uuid-exports/occ_first_loaded_date.csv';"
+gzip /data/uuid-exports/*.csv
