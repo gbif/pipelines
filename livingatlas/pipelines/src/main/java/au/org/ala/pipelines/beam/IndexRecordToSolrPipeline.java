8e8c3e428fbfb41f4412b51643c89c224e98d593
Dave Martin <djtfmartin@gmail.com>
2021-01-19 12:26:42 +0000

    merge fixes for indexing pipeline

--- livingatlas/pipelines/src/main/java/au/org/ala/pipelines/beam/IndexRecordToSolrPipeline.java
+++ livingatlas/pipelines/src/main/java/au/org/ala/pipelines/beam/IndexRecordToSolrPipeline.java
@@ -1,5 +1,7 @@
 package au.org.ala.pipelines.beam;
 
+import static org.gbif.pipelines.common.PipelinesVariables.Pipeline.AVRO_EXTENSION;
+
 import au.org.ala.pipelines.options.ALASolrPipelineOptions;
 import au.org.ala.pipelines.transforms.IndexRecordTransform;
 import au.org.ala.pipelines.util.VersionInfo;
@@ -19,7 +21,9 @@ import org.apache.beam.sdk.transforms.join.CoGroupByKey;
 import org.apache.beam.sdk.transforms.join.KeyedPCollectionTuple;
 import org.apache.beam.sdk.values.*;
 import org.gbif.pipelines.common.beam.options.PipelinesOptionsFactory;
+import org.gbif.pipelines.common.beam.utils.PathBuilder;
 import org.gbif.pipelines.io.avro.IndexRecord;
+import org.gbif.pipelines.io.avro.JackKnifeOutlierRecord;
 import org.gbif.pipelines.io.avro.SampleRecord;
 import org.joda.time.Duration;
 import org.slf4j.MDC;
@@ -32,8 +36,7 @@ public class IndexRecordToSolrPipeline {
     VersionInfo.print();
     MDC.put("step", "INDEX_RECORD_TO_SOLR");
 
-    String[] combinedArgs =
-        new CombinedYamlConfiguration(args).toArgs("general", "speciesLists", "index");
+    String[] combinedArgs = new CombinedYamlConfiguration(args).toArgs("general", "index");
     ALASolrPipelineOptions options =
         PipelinesOptionsFactory.create(ALASolrPipelineOptions.class, combinedArgs);
     MDC.put("datasetId", options.getDatasetId() != null ? options.getDatasetId() : "ALL_RECORDS");
@@ -55,146 +58,153 @@ public class IndexRecordToSolrPipeline {
 
     if (options.getIncludeSampling()) {
 
-    // Load Samples
-    PCollection<SampleRecord> sampleRecords = loadSampleRecords(options, pipeline);
+      // Load Samples
+      PCollection<SampleRecord> sampleRecords = loadSampleRecords(options, pipeline);
 
-    // Load IndexRecords
-    PCollection<IndexRecord> indexRecordsCollection = loadIndexRecords(options, pipeline);
+      // Filter records with coordinates - we will join these to samples
+      PCollection<IndexRecord> recordsWithCoordinates =
+          indexRecordsCollection.apply(Filter.by(indexRecord -> hasCoordinates(indexRecord)));
 
-    // Filter records with coordinates - we will join these to samples
-    PCollection<IndexRecord> recordsWithCoordinates =
-        indexRecordsCollection.apply(Filter.by(indexRecord -> hasCoordinates(indexRecord)));
+      // Filter records without coordinates - we will index, but not sample these
+      PCollection<IndexRecord> recordsWithoutCoordinates =
+          indexRecordsCollection.apply(Filter.by(indexRecord -> !hasCoordinates(indexRecord)));
 
-    // Filter records without coordinates - we will index, but not sample these
-    PCollection<IndexRecord> recordsWithoutCoordinates =
-        indexRecordsCollection.apply(Filter.by(indexRecord -> !hasCoordinates(indexRecord)));
-
-    // Convert to KV <LatLng, IndexRecord>
-    PCollection<KV<String, IndexRecord>> recordsWithCoordinatesKeyedLatng =
-        recordsWithCoordinates.apply(
-            MapElements.via(
-                new SimpleFunction<IndexRecord, KV<String, IndexRecord>>() {
-                  @Override
-                  public KV<String, IndexRecord> apply(IndexRecord input) {
-                    return KV.of(input.getLatLng(), input);
-                  }
-                }));
-
-    // Convert to KV <LatLng, SampleRecord>
-    PCollection<KV<String, SampleRecord>> sampleRecordsKeyedLatng =
-        sampleRecords.apply(
-            MapElements.via(
-                new SimpleFunction<SampleRecord, KV<String, SampleRecord>>() {
-                  @Override
-                  public KV<String, SampleRecord> apply(SampleRecord input) {
-                    return KV.of(input.getLatLng(), input);
-                  }
-                }));
-
-    // Co group IndexRecords with coordinates with Sample data
-    final TupleTag<IndexRecord> indexRecordTag = new TupleTag<>();
-    final TupleTag<SampleRecord> samplingTag = new TupleTag<>();
-
-    // Join collections by LatLng string
-    PCollection<KV<String, CoGbkResult>> results =
-        KeyedPCollectionTuple.of(samplingTag, sampleRecordsKeyedLatng)
-            .and(indexRecordTag, recordsWithCoordinatesKeyedLatng)
-            .apply(CoGroupByKey.create());
-
-    // Create  collection which contains samples
-    PCollection<IndexRecord> indexRecordsWithSampling =
-        results.apply(
-            ParDo.of(
-                new DoFn<KV<String, CoGbkResult>, IndexRecord>() {
-                  @ProcessElement
-                  public void processElement(ProcessContext c) {
-
-                    KV<String, CoGbkResult> e = c.element();
-
-                    SampleRecord sampleRecord =
-                        e.getValue()
-                            .getOnly(
-                                samplingTag,
-                                SampleRecord.newBuilder().setLatLng("NO_VALUE").build());
-                    Iterable<IndexRecord> indexRecordIterable = e.getValue().getAll(indexRecordTag);
-
-                    if (sampleRecord.getStrings() == null && sampleRecord.getDoubles() == null) {
-                      log.error("Sampling was empty for point: {}", e.getKey());
+      // Convert to KV <LatLng, IndexRecord>
+      PCollection<KV<String, IndexRecord>> recordsWithCoordinatesKeyedLatng =
+          recordsWithCoordinates.apply(
+              MapElements.via(
+                  new SimpleFunction<IndexRecord, KV<String, IndexRecord>>() {
+                    @Override
+                    public KV<String, IndexRecord> apply(IndexRecord input) {
+                      return KV.of(input.getLatLng(), input);
                     }
+                  }));
 
-                    indexRecordIterable.forEach(
-                        indexRecord -> {
-                          Map<String, String> strings =
-                              indexRecord.getStrings() != null
-                                  ? indexRecord.getStrings()
-                                  : new HashMap<String, String>();
-                          Map<String, Double> doubles =
-                              indexRecord.getDoubles() != null
-                                  ? indexRecord.getDoubles()
-                                  : new HashMap<String, Double>();
+      // Convert to KV <LatLng, SampleRecord>
+      PCollection<KV<String, SampleRecord>> sampleRecordsKeyedLatng =
+          sampleRecords.apply(
+              MapElements.via(
+                  new SimpleFunction<SampleRecord, KV<String, SampleRecord>>() {
+                    @Override
+                    public KV<String, SampleRecord> apply(SampleRecord input) {
+                      return KV.of(input.getLatLng(), input);
+                    }
+                  }));
 
-                          Map<String, String> stringsToPersist =
-                              ImmutableMap.<String, String>builder()
-                                  .putAll(strings)
-                                  .putAll(sampleRecord.getStrings())
-                                  .build();
+      // Co group IndexRecords with coordinates with Sample data
+      final TupleTag<IndexRecord> indexRecordTag = new TupleTag<>();
+      final TupleTag<SampleRecord> samplingTag = new TupleTag<>();
 
-                          Map<String, Double> doublesToPersist =
-                              ImmutableMap.<String, Double>builder()
-                                  .putAll(doubles)
-                                  .putAll(sampleRecord.getDoubles())
-                                  .build();
+      // Join collections by LatLng string
+      PCollection<KV<String, CoGbkResult>> results =
+          KeyedPCollectionTuple.of(samplingTag, sampleRecordsKeyedLatng)
+              .and(indexRecordTag, recordsWithCoordinatesKeyedLatng)
+              .apply(CoGroupByKey.create());
 
-                          IndexRecord ir =
-                              IndexRecord.newBuilder()
-                                  .setId(indexRecord.getId())
-                                  .setTaxonID(indexRecord.getTaxonID())
-                                  .setLatLng(indexRecord.getLatLng())
-                                  .setMultiValues(indexRecord.getMultiValues())
-                                  .setDates(indexRecord.getDates())
-                                  .setLongs(indexRecord.getLongs())
-                                  .setBooleans(indexRecord.getBooleans())
-                                  .setInts(indexRecord.getInts())
-                                  .setStrings(stringsToPersist)
-                                  .setDoubles(doublesToPersist)
-                                  .build();
+      // Create collection which contains samples keyed with indexRecord.id
+      PCollection<KV<String, IndexRecord>> indexRecordsWithSampling =
+          results.apply(ParDo.of(joinSampling(indexRecordTag, samplingTag)));
 
-                          c.output(ir);
-                        });
-                  }
-                }));
+      log.info("Adding step 4: Create SOLR connection");
+      SolrIO.ConnectionConfiguration conn =
+          SolrIO.ConnectionConfiguration.create(options.getZkHost());
 
-    log.info("Adding step 4: SOLR indexing");
-    SolrIO.ConnectionConfiguration conn =
-        SolrIO.ConnectionConfiguration.create(options.getZkHost());
+      log.info("Adding step 5: Write records with coordinates to SOLR");
+      writeToSolr(options, indexRecordsWithSampling.apply(Values.create()), conn);
 
-    indexRecordsWithSampling
-        .apply(
-            "SOLR_indexRecordsWithSampling",
-            ParDo.of(new IndexRecordTransform.IndexRecordToSolrInputDocumentFcn()))
-        .apply(
-            SolrIO.write()
-                .to(options.getSolrCollection())
-                .withConnectionConfiguration(conn)
-                .withMaxBatchSize(options.getSolrBatchSize())
-                .withRetryConfiguration(
-                    SolrIO.RetryConfiguration.create(10, Duration.standardMinutes(3))));
+      log.info("Adding step 6: Write records without coordinates to SOLR");
+      writeToSolr(options, recordsWithoutCoordinates, conn);
 
-    recordsWithoutCoordinates
-        .apply(
-            "SOLR_recordsWithoutCoordinates",
-            ParDo.of(new IndexRecordTransform.IndexRecordToSolrInputDocumentFcn()))
-        .apply(
-            SolrIO.write()
-                .to(options.getSolrCollection())
-                .withConnectionConfiguration(conn)
-                .withMaxBatchSize(options.getSolrBatchSize())
-                .withRetryConfiguration(
-                    SolrIO.RetryConfiguration.create(10, Duration.standardMinutes(3))));
+    } else {
+      log.info("Adding step 4: Create SOLR connection");
+      SolrIO.ConnectionConfiguration conn =
+          SolrIO.ConnectionConfiguration.create(options.getZkHost());
+
+      log.info("Adding step 5: Write records without sampling to SOLR");
+      writeToSolr(options, indexRecordsCollection, conn);
+    }
 
     pipeline.run(options).waitUntilFinish();
 
     log.info("Solr indexing pipeline complete");
+  }
+
+  private static void writeToSolr(
+      ALASolrPipelineOptions options,
+      PCollection<IndexRecord> recordsWithoutCoordinates,
+      SolrIO.ConnectionConfiguration conn) {
+    recordsWithoutCoordinates
+        .apply("SOLR_doc", ParDo.of(new IndexRecordTransform.IndexRecordToSolrInputDocumentFcn()))
+        .apply(
+            SolrIO.write()
+                .to(options.getSolrCollection())
+                .withConnectionConfiguration(conn)
+                .withMaxBatchSize(options.getSolrBatchSize())
+                .withRetryConfiguration(
+                    SolrIO.RetryConfiguration.create(
+                        options.getSolrRetryMaxAttempts(),
+                        Duration.standardMinutes(options.getSolrRetryDurationInMins()))));
+  }
+
+  private static DoFn<KV<String, CoGbkResult>, KV<String, IndexRecord>> joinSampling(
+      TupleTag<IndexRecord> indexRecordTag, TupleTag<SampleRecord> samplingTag) {
+
+    return new DoFn<KV<String, CoGbkResult>, KV<String, IndexRecord>>() {
+      @ProcessElement
+      public void processElement(ProcessContext c) {
+
+        KV<String, CoGbkResult> e = c.element();
+
+        SampleRecord sampleRecord =
+            e.getValue()
+                .getOnly(samplingTag, SampleRecord.newBuilder().setLatLng("NO_VALUE").build());
+        Iterable<IndexRecord> indexRecordIterable = e.getValue().getAll(indexRecordTag);
+
+        if (sampleRecord.getStrings() == null && sampleRecord.getDoubles() == null) {
+          log.error("Sampling was empty for point: {}", e.getKey());
+        }
+
+        indexRecordIterable.forEach(
+            indexRecord -> {
+              Map<String, String> strings =
+                  indexRecord.getStrings() != null
+                      ? indexRecord.getStrings()
+                      : new HashMap<String, String>();
+              Map<String, Double> doubles =
+                  indexRecord.getDoubles() != null
+                      ? indexRecord.getDoubles()
+                      : new HashMap<String, Double>();
+
+              Map<String, String> stringsToPersist =
+                  ImmutableMap.<String, String>builder()
+                      .putAll(strings)
+                      .putAll(sampleRecord.getStrings())
+                      .build();
+
+              Map<String, Double> doublesToPersist =
+                  ImmutableMap.<String, Double>builder()
+                      .putAll(doubles)
+                      .putAll(sampleRecord.getDoubles())
+                      .build();
+
+              IndexRecord ir =
+                  IndexRecord.newBuilder()
+                      .setId(indexRecord.getId())
+                      .setTaxonID(indexRecord.getTaxonID())
+                      .setLatLng(indexRecord.getLatLng())
+                      .setMultiValues(indexRecord.getMultiValues())
+                      .setDates(indexRecord.getDates())
+                      .setLongs(indexRecord.getLongs())
+                      .setBooleans(indexRecord.getBooleans())
+                      .setInts(indexRecord.getInts())
+                      .setStrings(stringsToPersist)
+                      .setDoubles(doublesToPersist)
+                      .build();
+
+              c.output(KV.of(indexRecord.getId(), ir));
+            });
+      }
+    };
   }
 
   /**
