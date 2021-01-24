bc148a3baa00a536fdbf1b47e2cafcd654c1b040
Dave Martin <djtfmartin@gmail.com>
2021-01-24 11:25:32 +0000

    checkpoint #2 commit for beam port of occurrence clustering code

--- livingatlas/pipelines/src/main/java/au/org/ala/clustering/RelationshipAssertion.java
+++ livingatlas/pipelines/src/main/java/au/org/ala/clustering/RelationshipAssertion.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
+import org.gbif.pipelines.io.avro.OccurrenceFeatures;
 
 /**
  * Models reas between occurrence records.
