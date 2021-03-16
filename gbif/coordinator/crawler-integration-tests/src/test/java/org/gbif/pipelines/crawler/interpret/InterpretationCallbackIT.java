3c308aafc912432136bc73f4e8a963cbd6e07d7a
Nikolay Volik <nvolik@gbif.org>
2021-03-16 17:14:00 +0100

    Add initial IT for verbatim to interpreted

---
+++ gbif/coordinator/crawler-integration-tests/src/test/java/org/gbif/pipelines/crawler/interpret/InterpretationCallbackIT.java
@@ -0,0 +1,180 @@
+package org.gbif.pipelines.crawler.interpret;
+
+import static org.gbif.api.model.pipelines.StepRunner.DISTRIBUTED;
+import static org.gbif.api.model.pipelines.StepRunner.STANDALONE;
+import static org.gbif.api.model.pipelines.StepType.VERBATIM_TO_INTERPRETED;
+import static org.gbif.crawler.constants.PipelinesNodePaths.getPipelinesInfoPath;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryOneTime;
+import org.apache.curator.test.TestingServer;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.gbif.api.vocabulary.EndpointType;
+import org.gbif.common.messaging.api.messages.PipelinesVerbatimMessage;
+import org.gbif.common.messaging.api.messages.PipelinesVerbatimMessage.ValidationResult;
+import org.gbif.crawler.constants.PipelinesNodePaths.Fn;
+import org.gbif.pipelines.common.PipelinesVariables.Pipeline.Interpretation.RecordType;
+import org.gbif.pipelines.common.utils.ZookeeperUtils;
+import org.gbif.pipelines.crawler.MessagePublisherStub;
+import org.gbif.registry.ws.client.pipelines.PipelinesHistoryWsClient;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class InterpretationCallbackIT {
+
+  private static final String INTERPRETED_LABEL = VERBATIM_TO_INTERPRETED.getLabel();
+  private static final String DATASET_UUID = "9bed66b3-4caa-42bb-9c93-71d7ba109dad";
+  private static final long EXECUTION_ID = 1L;
+  private static CuratorFramework curator;
+  private static TestingServer server;
+  private static MessagePublisherStub publisher;
+  private static PipelinesHistoryWsClient historyWsClient;
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+
+    server = new TestingServer();
+    curator =
+        CuratorFrameworkFactory.builder()
+            .connectString(server.getConnectString())
+            .namespace("crawler")
+            .retryPolicy(new RetryOneTime(1))
+            .build();
+    curator.start();
+
+    publisher = MessagePublisherStub.create();
+
+    historyWsClient = Mockito.mock(PipelinesHistoryWsClient.class);
+  }
+
+  @AfterClass
+  public static void tearDown() throws IOException {
+    curator.close();
+    server.stop();
+    publisher.close();
+  }
+
+  @After
+  public void after() {
+    publisher.close();
+  }
+
+  @Test
+  public void testInvalidMessageRunner() {
+
+    // State
+    InterpreterConfiguration config = new InterpreterConfiguration();
+    config.stepConfig.repositoryPath = getClass().getResource("/dataset/interpreted/").getFile();
+    config.processRunner = STANDALONE.name();
+    config.pipelinesConfig = "pipelines.yaml";
+
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    InterpretationCallback callback =
+        new InterpretationCallback(
+            config, publisher, curator, historyWsClient, null, executorService);
+
+    UUID uuid = UUID.fromString(DATASET_UUID);
+    int attempt = 60;
+    String crawlId = DATASET_UUID;
+    ValidationResult validationResult = new ValidationResult(true, true, false, 0L);
+
+    PipelinesVerbatimMessage message =
+        new PipelinesVerbatimMessage(
+            uuid,
+            attempt,
+            Collections.singleton(RecordType.ALL.name()),
+            Collections.singleton(VERBATIM_TO_INTERPRETED.name()),
+            DISTRIBUTED.name(),
+            EndpointType.DWC_ARCHIVE,
+            null,
+            validationResult,
+            null,
+            EXECUTION_ID);
+
+    // When
+    callback.handleMessage(message);
+
+    // Should
+    Path path =
+        Paths.get(config.stepConfig.repositoryPath + DATASET_UUID + "/" + attempt + "/interpreted");
+    assertFalse(path.toFile().exists());
+    assertFalse(checkExists(curator, crawlId, INTERPRETED_LABEL));
+    assertFalse(checkExists(curator, crawlId, Fn.SUCCESSFUL_MESSAGE.apply(INTERPRETED_LABEL)));
+    assertFalse(checkExists(curator, crawlId, Fn.MQ_CLASS_NAME.apply(INTERPRETED_LABEL)));
+    assertFalse(checkExists(curator, crawlId, Fn.MQ_MESSAGE.apply(INTERPRETED_LABEL)));
+    assertEquals(0, publisher.getMessages().size());
+  }
+
+  @Ignore
+  @Test
+  public void testNormalCase() throws Exception {
+
+    // State
+    InterpreterConfiguration config = new InterpreterConfiguration();
+    config.stepConfig.repositoryPath = getClass().getResource("/dataset/interpreted/").getFile();
+    config.processRunner = STANDALONE.name();
+    config.pipelinesConfig = "pipelines.yaml";
+
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    CloseableHttpClient closeableHttpClient = new CloseableHttpClientStub(200, "[]");
+
+    InterpretationCallback callback =
+        new InterpretationCallback(
+            config, publisher, curator, historyWsClient, closeableHttpClient, executorService);
+
+    UUID uuid = UUID.fromString(DATASET_UUID);
+    int attempt = 60;
+    String crawlId = DATASET_UUID;
+    ValidationResult validationResult = new ValidationResult(true, true, false, 0L);
+
+    PipelinesVerbatimMessage message =
+        new PipelinesVerbatimMessage(
+            uuid,
+            attempt,
+            Collections.singleton(RecordType.ALL.name()),
+            Collections.singleton(VERBATIM_TO_INTERPRETED.name()),
+            STANDALONE.name(),
+            EndpointType.DWC_ARCHIVE,
+            null,
+            validationResult,
+            null,
+            EXECUTION_ID);
+
+    // When
+    callback.handleMessage(message);
+
+    // Should
+    assertTrue(checkExists(curator, crawlId, INTERPRETED_LABEL));
+    assertTrue(checkExists(curator, crawlId, Fn.SUCCESSFUL_MESSAGE.apply(INTERPRETED_LABEL)));
+    assertTrue(checkExists(curator, crawlId, Fn.MQ_CLASS_NAME.apply(INTERPRETED_LABEL)));
+    assertTrue(checkExists(curator, crawlId, Fn.MQ_MESSAGE.apply(INTERPRETED_LABEL)));
+    assertEquals(1, publisher.getMessages().size());
+
+    // Clean
+    // HdfsUtils.deleteDirectory(null, null, path.toString());
+    curator
+        .delete()
+        .deletingChildrenIfNeeded()
+        .forPath(getPipelinesInfoPath(crawlId, INTERPRETED_LABEL));
+  }
+
+  private boolean checkExists(CuratorFramework curator, String id, String path) {
+    return ZookeeperUtils.checkExists(curator, getPipelinesInfoPath(id, path));
+  }
+}
