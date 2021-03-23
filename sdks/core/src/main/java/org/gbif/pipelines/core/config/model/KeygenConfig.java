5dbf4374a94de8896397e7cba051c4350a292bfd
Nikolay Volik <nvolik@gbif.org>
2021-03-23 14:42:09 +0100

    Exclude libs from shaded lib

--- sdks/core/src/main/java/org/gbif/pipelines/core/config/model/KeygenConfig.java
+++ sdks/core/src/main/java/org/gbif/pipelines/core/config/model/KeygenConfig.java
@@ -1,6 +1,6 @@
 package org.gbif.pipelines.core.config.model;
 
-import com.cloudera.org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
 import lombok.Data;
 import lombok.NoArgsConstructor;
