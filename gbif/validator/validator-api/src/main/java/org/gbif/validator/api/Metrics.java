4bb96fce9fe239ac2ea87e88aed71c31d4d0c1c6
nvolik <nikolay.volik@hotmail.com>
2021-09-20 15:42:49 +0200

    #542 NOTE:Broken commit. Improve Metrics class

--- gbif/validator/validator-api/src/main/java/org/gbif/validator/api/Metrics.java
+++ gbif/validator/validator-api/src/main/java/org/gbif/validator/api/Metrics.java
@@ -1,21 +1,18 @@
 package org.gbif.validator.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import java.io.IOException;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.gbif.api.model.checklistbank.NameUsage;
-import org.gbif.api.model.checklistbank.VerbatimNameUsage;
-import org.gbif.api.model.crawler.GenericValidationReport;
-import org.gbif.api.model.crawler.OccurrenceValidationReport;
+import lombok.Singular;
 import org.gbif.api.model.pipelines.StepType;
 import org.gbif.validator.api.Validation.Status;
 
@@ -26,112 +23,68 @@ import org.gbif.validator.api.Validation.Status;
 @JsonDeserialize(builder = Metrics.MetricsBuilder.class)
 public class Metrics {
 
-  @Builder.Default private Map<StepType, Status> stepTypes = new HashMap<>();
-  @Builder.Default private Core core = Core.builder().build();
-  @Builder.Default private List<Extension> extensions = Collections.emptyList();
-  private ChecklistValidationReport checklistValidationReport;
+  @Singular private List<ValidationStep> stepTypes = new LinkedList<>();
 
-  @Builder.Default
-  private ArchiveValidationReport archiveValidationReport =
-      ArchiveValidationReport.builder().build();
+  @JsonProperty("files")
+  @Singular
+  private List<FileInfo> fileInfos;
 
-  @Builder.Default
-  private XmlSchemaValidatorResult xmlSchemaValidatorResult =
-      XmlSchemaValidatorResult.builder().build();
-
-  public void addStepType(StepType stepType, Status status) {
-    stepTypes.put(stepType, status);
+  @Data
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @JsonDeserialize(builder = ValidationStep.ValidationStepBuilder.class)
+  public static class ValidationStep {
+    private StepType stepType;
+    private Status status;
   }
 
   @Data
   @Builder
   @NoArgsConstructor
   @AllArgsConstructor
-  @JsonDeserialize(builder = Core.CoreBuilder.class)
-  public static class Core {
-    @Builder.Default private String fileName = null;
-    @Builder.Default private Long fileCount = 0L;
-    @Builder.Default private Long indexedCount = 0L;
-
-    @Builder.Default private Set<TermInfo> indexedCoreTerms = Collections.emptySet();
-
-    @Builder.Default private Set<IssueInfo> occurrenceIssues = Collections.emptySet();
-
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @JsonDeserialize(builder = Core.TermInfo.TermInfoBuilder.class)
-    public static class TermInfo {
-      private String term;
-      @Builder.Default private Long rawIndexed = 0L;
-      @Builder.Default private Long interpretedIndexed = null;
-    }
-
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @JsonDeserialize(builder = Core.IssueInfo.IssueInfoBuilder.class)
-    public static class IssueInfo {
-      private String issue;
-      @Builder.Default private Long count = null;
-      @Builder.Default private List<IssueSample> samples = Collections.emptyList();
-      @Builder.Default private EvaluationCategory issueCategory = null;
-
-      @Data
-      @Builder
-      @NoArgsConstructor
-      @AllArgsConstructor
-      @JsonDeserialize(builder = Core.IssueInfo.IssueSample.IssueSampleBuilder.class)
-      public static class IssueSample {
-        private String recordId;
-        @Builder.Default private Map<String, String> relatedData = Collections.emptyMap();
-      }
-    }
+  @JsonDeserialize(builder = FileInfo.FileInfoBuilder.class)
+  public static class FileInfo {
+    private String fileName;
+    private DwcFileType fileType;
+    private Long count;
+    private Long indexedCount;
+    private String rowType;
+    @Builder.Default private List<TermInfo> terms = Collections.emptyList();
+    @Builder.Default private List<IssueInfo> issues = Collections.emptyList();
   }
 
   @Data
   @Builder
   @NoArgsConstructor
   @AllArgsConstructor
-  @JsonDeserialize(builder = Extension.ExtensionBuilder.class)
-  public static class Extension {
-    @Builder.Default private String fileName = null;
-    @Builder.Default private Long fileCount = null;
-    @Builder.Default private String rowType = null;
-    @Builder.Default private Map<String, Long> extensionsTermsCounts = Collections.emptyMap();
+  @JsonDeserialize(builder = TermInfo.TermInfoBuilder.class)
+  public static class TermInfo {
+    private String term;
+    private Long rawIndexed;
+    private Long interpretedIndexed;
   }
 
   @Data
   @Builder
   @NoArgsConstructor
   @AllArgsConstructor
-  @JsonDeserialize(builder = ArchiveValidationReport.ArchiveValidationReportBuilder.class)
-  public static class ArchiveValidationReport {
-    private OccurrenceValidationReport occurrenceReport;
-    private GenericValidationReport genericReport;
-    private String invalidationReason;
+  @JsonDeserialize(builder = IssueInfo.IssueInfoBuilder.class)
+  public static class IssueInfo {
+    private String issue;
+    private EvaluationCategory issueCategory;
+    private Long count;
+    @Builder.Default private List<IssueSample> samples = Collections.emptyList();
   }
 
   @Data
   @Builder
   @NoArgsConstructor
   @AllArgsConstructor
-  @JsonDeserialize(builder = ChecklistValidationReport.ChecklistValidationReportBuilder.class)
-  public static class ChecklistValidationReport {
-
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @JsonDeserialize(builder = ChecklistValidationResult.ChecklistValidationResultBuilder.class)
-    public static class ChecklistValidationResult {
-      private NameUsage nameUsage;
-      private VerbatimNameUsage verbatimNameUsage;
-    }
-
-    private List<ChecklistValidationResult> results;
+  @JsonDeserialize(builder = IssueSample.IssueSampleBuilder.class)
+  public static class IssueSample {
+    private String recordId;
+    private Map<String, String> relatedData = Collections.emptyMap();
   }
 
   @Override
