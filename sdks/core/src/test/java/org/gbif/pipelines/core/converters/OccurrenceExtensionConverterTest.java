ca743ff9c53d85e23abd54868d56829d9da52c0e
Nikolay Volik <nvolik@gbif.org>
2021-11-02 15:30:32 +0100

    #618 Extract all extensions from event data record

--- sdks/core/src/test/java/org/gbif/pipelines/core/converters/OccurrenceExtensionConverterTest.java
+++ sdks/core/src/test/java/org/gbif/pipelines/core/converters/OccurrenceExtensionConverterTest.java
@@ -1,8 +1,14 @@
 package org.gbif.pipelines.core.converters;
 
+import static org.gbif.dwc.terms.DwcTerm.MeasurementOrFact;
+import static org.gbif.dwc.terms.DwcTerm.Occurrence;
+import static org.gbif.dwc.terms.DwcTerm.ResourceRelationship;
+
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import org.gbif.dwc.terms.DwcTerm;
 import org.gbif.pipelines.io.avro.ExtendedRecord;
 import org.junit.Assert;
@@ -18,22 +24,48 @@ public class OccurrenceExtensionConverterTest {
     String somethingCore = "somethingCore";
     String somethingExt = "somethingExt";
 
+    // Core
     Map<String, String> coreMap = new HashMap<>(2);
     coreMap.put(DwcTerm.occurrenceID.qualifiedName(), idCore);
     coreMap.put(somethingCore, somethingCore);
+
+    // Ext
+    Map<String, String> extCoreMap = new HashMap<>(2);
+    extCoreMap.put(DwcTerm.occurrenceID.qualifiedName(), idExt);
+    extCoreMap.put(somethingExt, somethingExt);
 
     Map<String, String> extMap = new HashMap<>(2);
     extMap.put(DwcTerm.occurrenceID.qualifiedName(), idExt);
     extMap.put(somethingExt, somethingExt);
 
+    // Set
+    Map<String, List<Map<String, String>>> exts = new HashMap<>(2);
+    exts.put(Occurrence.qualifiedName(), Collections.singletonList(extCoreMap));
+    exts.put(MeasurementOrFact.qualifiedName(), Arrays.asList(extMap, extMap));
+    exts.put(ResourceRelationship.qualifiedName(), Arrays.asList(extMap, extMap));
+
+    ExtendedRecord extendedRecord =
+        ExtendedRecord.newBuilder().setId("id").setCoreTerms(coreMap).setExtensions(exts).build();
+
     // When
-    Optional<ExtendedRecord> result = OccurrenceExtensionConverter.convert(coreMap, extMap);
+    List<ExtendedRecord> result = OccurrenceExtensionConverter.convert(extendedRecord);
 
     // Should
-    Assert.assertTrue(result.isPresent());
-    ExtendedRecord erResult = result.get();
+    Assert.assertFalse(result.isEmpty());
+
+    ExtendedRecord erResult = result.get(0);
+
     Assert.assertEquals(idExt, erResult.getId());
     Assert.assertEquals(somethingCore, erResult.getCoreTerms().get(somethingCore));
     Assert.assertEquals(somethingExt, erResult.getCoreTerms().get(somethingExt));
+
+    Assert.assertEquals(2, erResult.getExtensions().get(MeasurementOrFact.qualifiedName()).size());
+    Assert.assertEquals(
+        2, erResult.getExtensions().get(MeasurementOrFact.qualifiedName()).get(0).size());
+
+    Assert.assertEquals(
+        2, erResult.getExtensions().get(ResourceRelationship.qualifiedName()).size());
+    Assert.assertEquals(
+        2, erResult.getExtensions().get(ResourceRelationship.qualifiedName()).get(0).size());
   }
 }
