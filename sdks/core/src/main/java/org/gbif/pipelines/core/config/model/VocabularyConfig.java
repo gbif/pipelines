323d424c8ea0c938b0b08d0257d879bdd211f5e0
Nikolay Volik <nvolik@gbif.org>
2021-12-10 12:07:52 +0100

    #581 #582 Add missed vocabularies configs

--- sdks/core/src/main/java/org/gbif/pipelines/core/config/model/VocabularyConfig.java
+++ sdks/core/src/main/java/org/gbif/pipelines/core/config/model/VocabularyConfig.java
@@ -17,18 +17,29 @@ public class VocabularyConfig implements Serializable {
   // directory where the vocabulary files are stored
   private String vocabulariesPath;
 
-  // name of the life stage vocabulary
+  // name of the lifeStage vocabulary
   private String lifeStageVocabName;
 
-  // name of the life stage vocabulary
+  // name of the establishmentMeans vocabulary
   private String establishmentMeansVocabName;
+
+  // name of the pathway vocabulary
+  private String pathwayVocabName;
+
+  // name of the degreeOfEstablishment vocabulary
+  private String degreeOfEstablishmentVocabName;
 
   public String getVocabularyFileName(Term term) {
     if (term == DwcTerm.lifeStage) {
       return lifeStageVocabName;
     } else if (term == DwcTerm.establishmentMeans) {
       return establishmentMeansVocabName;
+    } else if (term == DwcTerm.pathway) {
+      return pathwayVocabName;
+    } else if (term == DwcTerm.degreeOfEstablishment) {
+      return degreeOfEstablishmentVocabName;
     }
-    return null;
+    throw new IllegalArgumentException(
+        "Can't find associated vocabulary for term" + term.qualifiedName());
   }
 }
