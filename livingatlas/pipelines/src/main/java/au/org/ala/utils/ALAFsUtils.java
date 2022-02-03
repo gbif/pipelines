1fbdf912bba72c4ac83a7b7e3a1ac6b3cf24439a
Dave Martin <djtfmartin@gmail.com>
2022-02-03 14:46:37 +0000

    Fixes for outlier pipeline to ensure all records are written out and use of metrics instead of worklogs. Renaming of SOLR index field to keep readable camelcase convention

--- livingatlas/pipelines/src/main/java/au/org/ala/utils/ALAFsUtils.java
+++ livingatlas/pipelines/src/main/java/au/org/ala/utils/ALAFsUtils.java
@@ -77,19 +77,20 @@ public class ALAFsUtils {
    */
   public static String buildPathOutlierUsingTargetPath(
       AllDatasetsPipelinesOptions options, boolean delete) throws IOException {
+
     // default: {fsPath}/pipelines-outlier
     FileSystem fs =
         FileSystemFactory.getInstance(options.getHdfsSiteConfig(), options.getCoreSiteConfig())
             .getFs(options.getTargetPath());
 
-    String outputPath = PathBuilder.buildPath(options.getTargetPath()).toString();
+    String outputPath = options.getTargetPath();
 
     // {fsPath}/pipelines-outlier/{datasetId}
     if (options.getDatasetId() != null && !"all".equalsIgnoreCase(options.getDatasetId())) {
-      outputPath = PathBuilder.buildPath(outputPath, options.getDatasetId()).toString();
+      outputPath = outputPath + "/" + options.getDatasetId();
     } else {
       // {fsPath}/pipelines-outlier/all
-      outputPath = PathBuilder.buildPath(outputPath, "all").toString();
+      outputPath = outputPath + "/" + "all";
     }
     // delete previous runs
     if (delete)
@@ -105,21 +106,16 @@ public class ALAFsUtils {
    * Get an output path to outlier records. {fsPath}/pipelines-outlier/{datasetId}
    * {fsPath}/pipelines-outlier/all
    */
-  public static String getOutlierTargetPath(AllDatasetsPipelinesOptions options)
-      throws IOException {
-    // default: {fsPath}/pipelines-outlier
-    FileSystem fs =
-        FileSystemFactory.getInstance(options.getHdfsSiteConfig(), options.getCoreSiteConfig())
-            .getFs(options.getTargetPath());
+  public static String getOutlierTargetPath(AllDatasetsPipelinesOptions options) {
 
-    String outputPath = PathBuilder.buildPath(options.getTargetPath()).toString();
+    String outputPath = options.getTargetPath();
 
     // {fsPath}/pipelines-outlier/{datasetId}
     if (options.getDatasetId() != null && !"all".equalsIgnoreCase(options.getDatasetId())) {
-      outputPath = PathBuilder.buildPath(outputPath, options.getDatasetId()).toString();
+      outputPath = PathBuilder.buildPath(outputPath, options.getDatasetId(), "*.avro").toString();
     } else {
       // {fsPath}/pipelines-outlier/all
-      outputPath = PathBuilder.buildPath(outputPath, "all").toString();
+      outputPath = PathBuilder.buildPath(outputPath, "all", "*.avro").toString();
     }
 
     return outputPath;
@@ -207,8 +203,8 @@ public class ALAFsUtils {
    * @param directoryPath path to some directory
    */
   public static boolean hasAvro(FileSystem fs, String directoryPath, boolean recurse) {
+    if (directoryPath.startsWith("hdfs:///")) directoryPath = directoryPath.substring(7);
     Path path = PathBuilder.buildPath(directoryPath);
-    Path avroFilePath = PathBuilder.buildPath(directoryPath, "*.avro");
     try {
       if (fs.exists(path)) {
         RemoteIterator<LocatedFileStatus> it = fs.listFiles(path, recurse);
