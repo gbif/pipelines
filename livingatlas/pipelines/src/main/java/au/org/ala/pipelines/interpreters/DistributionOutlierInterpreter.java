fd02ee35fcb9ad065b71c09c84e2c10b4997d3b9
Dave Martin <djtfmartin@gmail.com>
2022-02-03 16:08:33 +0000

    Ensure all outlier records are written to AVRO (not just new ones) Remove unused code Changed index term to distanceFromExpertDistribution to follow readable camelCase convention Use metrics instead of worklog code

--- livingatlas/pipelines/src/main/java/au/org/ala/pipelines/interpreters/DistributionOutlierInterpreter.java
+++ livingatlas/pipelines/src/main/java/au/org/ala/pipelines/interpreters/DistributionOutlierInterpreter.java
@@ -2,15 +2,9 @@ package au.org.ala.pipelines.interpreters;
 
 import static org.gbif.pipelines.core.utils.ModelUtils.*;
 
-import au.org.ala.pipelines.parser.CoordinatesParser;
-import com.google.common.base.Strings;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.gbif.dwc.terms.DwcTerm;
-import org.gbif.kvs.geocode.LatLng;
-import org.gbif.pipelines.core.parsers.common.ParsedField;
 import org.gbif.pipelines.io.avro.DistributionOutlierRecord;
-import org.gbif.pipelines.io.avro.ExtendedRecord;
 import org.gbif.pipelines.io.avro.IndexRecord;
 
 /*
@@ -32,33 +26,5 @@ public class DistributionOutlierInterpreter {
 
   public static void interpretSpeciesId(IndexRecord ir, DistributionOutlierRecord dr) {
     dr.setSpeciesID(ir.getTaxonID());
-  }
-
-  /*
-   * Interprete from verbatim
-   */
-  public static void interpretOccurrenceID(ExtendedRecord er, DistributionOutlierRecord dr) {
-    String value = extractNullAwareValue(er, DwcTerm.occurrenceID);
-    if (!Strings.isNullOrEmpty(value)) {
-      dr.setId(value);
-    }
-  }
-
-  public static void interpretLocation(ExtendedRecord er, DistributionOutlierRecord dr) {
-    ParsedField<LatLng> parsedLatLon = CoordinatesParser.parseCoords(er);
-    addIssue(dr, parsedLatLon.getIssues());
-
-    if (parsedLatLon.isSuccessful()) {
-      LatLng latlng = parsedLatLon.getResult();
-      dr.setDecimalLatitude(latlng.getLatitude());
-      dr.setDecimalLongitude(latlng.getLongitude());
-    }
-  }
-
-  public static void interpretSpeciesId(ExtendedRecord er, DistributionOutlierRecord dr) {
-    String value = extractNullAwareValue(er, DwcTerm.taxonConceptID);
-    if (!Strings.isNullOrEmpty(value)) {
-      dr.setSpeciesID(value);
-    }
   }
 }
