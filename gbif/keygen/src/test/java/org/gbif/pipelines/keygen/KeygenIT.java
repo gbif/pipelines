6d5fd048efbc2f3edcce16bb3bad04e62dd9fd22
nvolik <nikolay.volik@hotmail.com>
2022-06-21 09:11:40 +0200

    #675 Add testGenerateOccurrenceTripletKey into the test

--- gbif/keygen/src/test/java/org/gbif/pipelines/keygen/KeygenIT.java
+++ gbif/keygen/src/test/java/org/gbif/pipelines/keygen/KeygenIT.java
@@ -29,6 +29,44 @@ public class KeygenIT extends HBaseIT {
   }
 
   @Test
+  public void testGenerateOccurrenceTripletKey() {
+
+    // State
+    String occurrenceId = "occurrenceId";
+    String triplet = "triplet";
+
+    SimpleOccurrenceRecord occurrenceRecord = SimpleOccurrenceRecord.create();
+    occurrenceRecord.setOccurrenceId(occurrenceId);
+    occurrenceRecord.setTriplet(triplet);
+
+    // When
+
+    // Check that key is empty
+    Optional<Long> nullKey = Keygen.getKey(keyService, true, true, false, occurrenceRecord);
+
+    // Generate
+    Optional<Long> newKey = Keygen.getKey(keyService, true, true, true, occurrenceRecord);
+
+    // Get key by occurrenceId
+    SimpleOccurrenceRecord occurrenceOnlyRecord = SimpleOccurrenceRecord.create();
+    occurrenceOnlyRecord.setOccurrenceId(occurrenceId);
+    Optional<Long> occurrenceIdKey = Keygen.getKey(keyService, true, true, false, occurrenceOnlyRecord);
+
+    // Get key by triplet
+    SimpleOccurrenceRecord tripletOnlyRecord = SimpleOccurrenceRecord.create();
+    tripletOnlyRecord.setTriplet(triplet);
+    Optional<Long> tripletKey = Keygen.getKey(keyService, true, true, false, tripletOnlyRecord);
+
+    // Should
+    assertFalse(nullKey.isPresent());
+    assertFalse(tripletKey.isPresent());
+
+    assertTrue(newKey.isPresent());
+    assertTrue(occurrenceIdKey.isPresent());
+    assertEquals(newKey.get(), occurrenceIdKey.get());
+  }
+
+  @Test
   public void testExistingOccurrenceTripletKey() {
 
     // State
