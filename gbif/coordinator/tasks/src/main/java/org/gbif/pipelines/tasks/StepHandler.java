4d1e2b0be17d849a55b55f14d8e133115cadab47
Nikolay Volik <nvolik@gbif.org>
2022-07-01 11:20:51 +0200

    Add the comment

--- gbif/coordinator/tasks/src/main/java/org/gbif/pipelines/tasks/StepHandler.java
+++ gbif/coordinator/tasks/src/main/java/org/gbif/pipelines/tasks/StepHandler.java
@@ -13,6 +13,10 @@ public interface StepHandler<I extends PipelineBasedMessage, O extends PipelineB
 
   boolean isMessageCorrect(I message);
 
+  /**
+   * Routing key to listen for MQ client, be aware it is not used anyway, you need to call the
+   * method manually
+   */
   default String getRouting() {
     throw new PipelinesException("MQ routing key is not specified");
   }
