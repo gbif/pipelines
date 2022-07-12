d711591a882b9280fc6945bb671c19f736339b3b
fmendezh <federicomh@gmail.com>
2022-07-12 13:16:41 +0200

    fixing table directory name

--- gbif/pipelines/ingest-gbif-java/src/test/java/org/gbif/pipelines/ingest/java/transforms/TableRecordWriterTest.java
+++ gbif/pipelines/ingest-gbif-java/src/test/java/org/gbif/pipelines/ingest/java/transforms/TableRecordWriterTest.java
@@ -86,7 +86,7 @@ public class TableRecordWriterTest {
     File result =
         new File(
             outputFile
-                + "/d596fccb-2319-42eb-b13b-986c932780ad/146/occurrence/occurrence/d596fccb-2319-42eb-b13b-986c932780ad_146.avro");
+                + "/d596fccb-2319-42eb-b13b-986c932780ad/146/occurrence_table/occurrence/d596fccb-2319-42eb-b13b-986c932780ad_146.avro");
     DatumReader<OccurrenceHdfsRecord> datumReader =
         new SpecificDatumReader<>(OccurrenceHdfsRecord.class);
     try (DataFileReader<OccurrenceHdfsRecord> dataFileReader =
@@ -162,7 +162,7 @@ public class TableRecordWriterTest {
     File result =
         new File(
             outputFile
-                + "/d596fccb-2319-42eb-b13b-986c932780ad/146/occurrence/occurrence/d596fccb-2319-42eb-b13b-986c932780ad_146.avro");
+                + "/d596fccb-2319-42eb-b13b-986c932780ad/146/occurrence_table/occurrence/d596fccb-2319-42eb-b13b-986c932780ad_146.avro");
     DatumReader<OccurrenceHdfsRecord> datumReader =
         new SpecificDatumReader<>(OccurrenceHdfsRecord.class);
     try (DataFileReader<OccurrenceHdfsRecord> dataFileReader =
