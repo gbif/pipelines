4b821c5cd4a55c4653a719f0538d43aeaa87a3c9
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2022-07-15 10:04:52 +0200

    spotless

--- sdks/core/src/main/java/org/gbif/pipelines/core/converters/ParentJsonConverter.java
+++ sdks/core/src/main/java/org/gbif/pipelines/core/converters/ParentJsonConverter.java
@@ -1,10 +1,14 @@
 package org.gbif.pipelines.core.converters;
 
+import static org.gbif.pipelines.core.utils.ModelUtils.extractOptValue;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-
+import lombok.SneakyThrows;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.gbif.dwc.terms.DwcTerm;
 import org.gbif.pipelines.core.factory.SerDeFactory;
 import org.gbif.pipelines.core.utils.HashConverter;
@@ -28,12 +32,6 @@ import org.gbif.pipelines.io.avro.json.OccurrenceJsonRecord;
 import org.gbif.pipelines.io.avro.json.Parent;
 import org.gbif.pipelines.io.avro.json.ParentJsonRecord;
 import org.gbif.pipelines.io.avro.json.TemporalInheritedRecord;
-
-import lombok.SneakyThrows;
-import lombok.experimental.SuperBuilder;
-import lombok.extern.slf4j.Slf4j;
-
-import static org.gbif.pipelines.core.utils.ModelUtils.extractOptValue;
 
 @Slf4j
 @SuperBuilder
