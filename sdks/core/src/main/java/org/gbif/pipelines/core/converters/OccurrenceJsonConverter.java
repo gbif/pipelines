6b91239bab44568e12e822d225347535aad6b5f0
Nikolay Volik <nvolik@gbif.org>
2022-08-19 11:37:02 +0200

    Fix the bug during Beam indexing: failed to parse field [coordinates] of type [geo_point] (mapper_parsing_exception)

--- sdks/core/src/main/java/org/gbif/pipelines/core/converters/OccurrenceJsonConverter.java
+++ sdks/core/src/main/java/org/gbif/pipelines/core/converters/OccurrenceJsonConverter.java
@@ -5,10 +5,8 @@ import static org.gbif.pipelines.core.utils.ModelUtils.extractOptValue;
 import java.util.Arrays;
 import java.util.Optional;
 import lombok.Builder;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.gbif.dwc.terms.DwcTerm;
-import org.gbif.pipelines.core.factory.SerDeFactory;
 import org.gbif.pipelines.io.avro.BasicRecord;
 import org.gbif.pipelines.io.avro.ClusteringRecord;
 import org.gbif.pipelines.io.avro.ExtendedRecord;
@@ -58,9 +56,8 @@ public class OccurrenceJsonConverter {
     return builder.build();
   }
 
-  @SneakyThrows
   public String toJson() {
-    return SerDeFactory.avroMapperNonNulls().writeValueAsString(convert());
+    return convert().toString();
   }
 
   private void mapMetadataRecord(OccurrenceJsonRecord.Builder builder) {
