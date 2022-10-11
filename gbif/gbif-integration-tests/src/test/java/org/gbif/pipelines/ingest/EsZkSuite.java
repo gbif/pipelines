5198ba06ac3b06efec37d52d4ae2e541e7507a26
nvolik <nikolay.volik@hotmail.com>
2022-10-11 08:30:50 +0200

    #790 Use one suit per module

--- gbif/gbif-integration-tests/src/test/java/org/gbif/pipelines/ingest/EsZkSuite.java
+++ gbif/gbif-integration-tests/src/test/java/org/gbif/pipelines/ingest/EsZkSuite.java
@@ -1,8 +1,12 @@
 package org.gbif.pipelines.ingest;
 
 import org.gbif.pipelines.ingest.pipelines.EventToEsIndexPipelineIT;
+import org.gbif.pipelines.ingest.pipelines.HdfsViewPipelineIT;
 import org.gbif.pipelines.ingest.pipelines.InterpretedToEsIndexExtendedPipelineIT;
 import org.gbif.pipelines.ingest.pipelines.OccurrenceToEsIndexPipelineIT;
+import org.gbif.pipelines.ingest.pipelines.VerbatimToEventPipelineIT;
+import org.gbif.pipelines.ingest.pipelines.VerbatimToIdentifierPipelineIT;
+import org.gbif.pipelines.ingest.pipelines.VerbatimToOccurrencePipelineIT;
 import org.gbif.pipelines.ingest.utils.EsServer;
 import org.gbif.pipelines.ingest.utils.ZkServer;
 import org.junit.ClassRule;
@@ -12,10 +16,18 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
+  // ZK and ES
   org.gbif.pipelines.ingest.java.pipelines.InterpretedToEsIndexExtendedPipelineIT.class,
   EventToEsIndexPipelineIT.class,
   InterpretedToEsIndexExtendedPipelineIT.class,
   OccurrenceToEsIndexPipelineIT.class,
+  // ZK
+  org.gbif.pipelines.ingest.java.pipelines.HdfsViewPipelineIT.class,
+  org.gbif.pipelines.ingest.java.pipelines.VerbatimToOccurrencePipelineIT.class,
+  HdfsViewPipelineIT.class,
+  VerbatimToEventPipelineIT.class,
+  VerbatimToIdentifierPipelineIT.class,
+  VerbatimToOccurrencePipelineIT.class
 })
 public class EsZkSuite {
   @ClassRule public static final EsServer ES_SERVER = EsServer.getInstance();
