e73050d262ba0798621d2ccd393a42303d30c52a
nvolik <nikolay.volik@hotmail.com>
2022-10-12 10:55:11 +0200

    #790 Run IT in parallel. Use random UUIDs for datasetKeys

--- gbif/gbif-integration-tests/src/test/java/org/gbif/pipelines/ingest/utils/EsTestUtils.java
+++ gbif/gbif-integration-tests/src/test/java/org/gbif/pipelines/ingest/utils/EsTestUtils.java
@@ -18,17 +18,6 @@ import org.gbif.pipelines.ingest.resources.EsServer;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EsTestUtils {
-
-  // datasets to test
-  public static final String DATASET_TEST = "82ceb6ba-f762-11e1-a439-00145eb45e9a";
-  public static final String DATASET_TEST_2 = "96c7660c-1356-4732-8a7c-0b3eeb20fb27";
-  public static final String DATASET_TEST_3 = "faf8b3c3-21dc-48b6-8a5c-e1f037789f74";
-  public static final String DATASET_TEST_4 = "a180b0b4-f527-4da5-81fb-ee085e3ce4f4";
-  public static final String DATASET_TEST_5 = "197908d0-5565-11d8-b290-b8a03c50a862";
-  public static final String DATASET_TEST_6 = "b5441d39-7564-4c70-bd9d-b90236808892";
-  public static final String DATASET_TEST_7 = "902c8fe7-8f38-45b0-854e-c324fed36303";
-  public static final String DATASET_TEST_8 = "7e3ec3b3-de71-4389-b3ea-71d0cae64631";
-  public static final String DATASET_TEST_9 = "758478a0-f762-11e1-a439-00145eb45e9a";
   public static final String MATCH_QUERY = "{\"query\":{\"match\":{\"%s\":\"%s\"}}}";
   // default number of records per dataset
   public static final int DEFAULT_REC_DATASET = 10;
