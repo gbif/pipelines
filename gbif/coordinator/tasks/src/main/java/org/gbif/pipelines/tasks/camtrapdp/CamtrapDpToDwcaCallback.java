94096c3bf6761cb131bc667735e87039dec8f8be
fmendezh <federicomh@gmail.com>
2022-10-27 14:56:45 +0200

    reverting unused messages https://github.com/gbif/pipelines/issues/803

--- gbif/coordinator/tasks/src/main/java/org/gbif/pipelines/tasks/camtrapdp/CamtrapDpToDwcaCallback.java
+++ gbif/coordinator/tasks/src/main/java/org/gbif/pipelines/tasks/camtrapdp/CamtrapDpToDwcaCallback.java
@@ -8,8 +8,8 @@ import org.gbif.api.model.registry.Dataset;
 import org.gbif.api.vocabulary.EndpointType;
 import org.gbif.common.messaging.AbstractMessageCallback;
 import org.gbif.common.messaging.api.MessagePublisher;
+import org.gbif.common.messaging.api.messages.CamtrapDpDownloadFinishedMessage;
 import org.gbif.common.messaging.api.messages.DwcaDownloadFinishedMessage;
-import org.gbif.common.messaging.api.messages.PipelinesArchiveValidatorMessage;
 import org.gbif.common.messaging.api.messages.PipelinesBalancerMessage;
 import org.gbif.common.messaging.api.messages.PipelinesCamtrapDpMessage;
 import org.gbif.registry.ws.client.DatasetClient;
@@ -17,20 +17,21 @@ import org.gbif.registry.ws.client.DatasetClient;
 /** Callback which is called when the {@link PipelinesCamtrapDpMessage} is received. */
 @Slf4j
 @AllArgsConstructor
-public class CamtrapDpToDwcaCallback extends AbstractMessageCallback<PipelinesCamtrapDpMessage> {
+public class CamtrapDpToDwcaCallback
+    extends AbstractMessageCallback<CamtrapDpDownloadFinishedMessage> {
 
   private final CamtrapToDwcaConfiguration config;
   private final MessagePublisher publisher;
   private final DatasetClient datasetClient;
 
   @Override
-  public void handleMessage(PipelinesCamtrapDpMessage message) {
+  public void handleMessage(CamtrapDpDownloadFinishedMessage message) {
     toDwca(message);
     notifyNextStep(message);
   }
 
   /** Calls the Camtraptor Service to transform the data package into DwC-A. */
-  private void toDwca(PipelinesCamtrapDpMessage message) {
+  private void toDwca(CamtrapDpDownloadFinishedMessage message) {
     Dataset dataset = datasetClient.get(message.getDatasetUuid());
     CamtraptorWsClient camtraptorWsClient = new CamtraptorWsClient(config.camtraptorWsUrl);
     camtraptorWsClient.toDwca(dataset.getKey(), dataset.getTitle());
@@ -38,15 +39,16 @@ public class CamtrapDpToDwcaCallback extends AbstractMessageCallback<PipelinesCa
 
   /** Creates and sends a message to the next step: DwC-A validation. */
   @SneakyThrows
-  private void notifyNextStep(PipelinesCamtrapDpMessage message) {
+  private void notifyNextStep(CamtrapDpDownloadFinishedMessage message) {
     publisher.send(
         new PipelinesBalancerMessage(
-            PipelinesArchiveValidatorMessage.class.getSimpleName(),
+            DwcaDownloadFinishedMessage.class.getSimpleName(),
             createOutgoingMessage(message).toString()));
   }
 
   /** Builds the message to be sent to the next processing stage: DwC-A validation. */
-  private DwcaDownloadFinishedMessage createOutgoingMessage(PipelinesCamtrapDpMessage message) {
+  private DwcaDownloadFinishedMessage createOutgoingMessage(
+      CamtrapDpDownloadFinishedMessage message) {
     return new DwcaDownloadFinishedMessage(
         message.getDatasetUuid(),
         message.getSource(),
