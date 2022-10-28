d8b1a345c68e0df89a23ee7c77539a88d5729bcb
fmendezh <federicomh@gmail.com>
2022-10-28 15:03:40 +0200

    Fixing client calls and adding a Retry mechanism https://github.com/gbif/pipelines/issues/803

--- gbif/coordinator/tasks/src/main/java/org/gbif/pipelines/tasks/camtrapdp/CamtraptorWsClient.java
+++ gbif/coordinator/tasks/src/main/java/org/gbif/pipelines/tasks/camtrapdp/CamtraptorWsClient.java
@@ -1,14 +1,34 @@
 package org.gbif.pipelines.tasks.camtrapdp;
 
+import io.github.resilience4j.retry.IntervalFunction;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
-/** Simple client to perform requests to the Camtraptor server. */
+/**
+ * Simple client to perform requests to the Camtraptor server.
+ *
+ * @see <a href="https://github.com/inbo/camtraptor/">Camtraptor</a> for more detail.
+ */
 @AllArgsConstructor
+@Slf4j
 public class CamtraptorWsClient {
+
+  private static final Retry RETRY =
+      Retry.of(
+          "camtraptorCall",
+          RetryConfig.custom()
+              .maxAttempts(3)
+              .intervalFunction(IntervalFunction.ofExponentialBackoff(Duration.ofSeconds(2)))
+              .build());
 
   private final String camtraptorWsUrl;
 
@@ -16,10 +36,10 @@ public class CamtraptorWsClient {
    * Converts a CamrtapDP package in the location identified by its datasetKey. The dataset title is
    * required by the Camtraptor to name the package file.
    */
-  @SneakyThrows
   public void toDwca(UUID datasetKey, String datasetTitle) {
     URL url = buildUrl(datasetKey, datasetTitle);
-    doRequest(url);
+    log.info("Querying Camtraptor server " + url);
+    Retry.decorateRunnable(RETRY, () -> doRequest(url)).run();
   }
 
   /** Performs the GET request to the Camtraptor server. */
@@ -42,6 +62,6 @@ public class CamtraptorWsClient {
             + "?dataset_key="
             + datasetKey.toString()
             + "&dataset_title="
-            + datasetTitle);
+            + URLEncoder.encode(datasetTitle, StandardCharsets.UTF_8.name()));
   }
 }
