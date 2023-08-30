cb8a4159b73d227443b98a99915875f72cb36cc3
nvolik <nikolay.volik@hotmail.com>
2023-08-30 16:46:17 +0200

    Use latest Beam version Clean-up ingestion pom

--- sdks/beam-common/src/main/java/org/gbif/pipelines/common/beam/Slf4jSink.java
+++ sdks/beam-common/src/main/java/org/gbif/pipelines/common/beam/Slf4jSink.java
@@ -2,12 +2,10 @@ package org.gbif.pipelines.common.beam;
 
 import com.codahale.metrics.MetricRegistry;
 import java.util.Properties;
-import org.apache.beam.runners.spark.metrics.AggregatorMetric;
 import org.apache.beam.runners.spark.metrics.WithMetricsSupport;
 
 /**
- * A Spark {@link org.apache.spark.metrics.sink.Sink} that is tailored to report {@link
- * AggregatorMetric} metrics to Slf4j.
+ * A Spark {@link org.apache.spark.metrics.sink.Sink} that is tailored to report metrics to Slf4j.
  */
 public class Slf4jSink extends org.apache.spark.metrics.sink.Slf4jSink {
 
