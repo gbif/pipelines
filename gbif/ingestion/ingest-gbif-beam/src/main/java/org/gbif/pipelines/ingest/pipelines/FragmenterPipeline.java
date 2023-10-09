26e56e58ee76f56d9bd1eba0da856da2b3c8439e
Nikolay Volik <nvolik@gbif.org>
2023-10-09 14:51:44 +0200

    #948 Use ExtendedRecort insted of DWCA

--- gbif/ingestion/ingest-gbif-beam/src/main/java/org/gbif/pipelines/ingest/pipelines/FragmenterPipeline.java
+++ gbif/ingestion/ingest-gbif-beam/src/main/java/org/gbif/pipelines/ingest/pipelines/FragmenterPipeline.java
@@ -1,11 +1,7 @@
 package org.gbif.pipelines.ingest.pipelines;
 
-import static org.gbif.pipelines.common.PipelinesVariables.Metrics.FRAGMENTER_COUNT;
-
 import java.util.function.Function;
-import java.util.function.Predicate;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -13,31 +9,25 @@ import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.io.hbase.HBaseIO;
 import org.apache.beam.sdk.io.hbase.HBaseIO.Write;
-import org.apache.beam.sdk.metrics.Counter;
-import org.apache.beam.sdk.metrics.Metrics;
-import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.client.Mutation;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Table;
 import org.gbif.api.model.pipelines.StepType;
 import org.gbif.api.vocabulary.EndpointType;
+import org.gbif.pipelines.common.PipelinesVariables;
+import org.gbif.pipelines.common.PipelinesVariables.Pipeline.Conversion;
 import org.gbif.pipelines.common.beam.metrics.MetricsHandler;
 import org.gbif.pipelines.common.beam.options.InterpretationPipelineOptions;
 import org.gbif.pipelines.common.beam.options.PipelinesOptionsFactory;
+import org.gbif.pipelines.common.beam.utils.PathBuilder;
 import org.gbif.pipelines.core.config.model.PipelinesConfig;
-import org.gbif.pipelines.core.functions.SerializableSupplier;
-import org.gbif.pipelines.fragmenter.common.HbaseStore;
-import org.gbif.pipelines.fragmenter.common.RawRecord;
-import org.gbif.pipelines.fragmenter.record.DwcaOccurrenceRecord;
-import org.gbif.pipelines.fragmenter.record.OccurrenceRecordConverter;
-import org.gbif.pipelines.ingest.pipelines.fragmenter.DwcaOccurrenceRecordIO;
-import org.gbif.pipelines.ingest.pipelines.fragmenter.DwcaOccurrenceRecordIO.Read;
+import org.gbif.pipelines.ingest.pipelines.fragmenter.DwcaOccurrenceRecordConverterFn;
+import org.gbif.pipelines.ingest.pipelines.fragmenter.MutationConverterFn;
 import org.gbif.pipelines.ingest.pipelines.fragmenter.RawRecordCoder;
+import org.gbif.pipelines.ingest.pipelines.fragmenter.RawRecordFilterFn;
+import org.gbif.pipelines.ingest.pipelines.fragmenter.RawRecordFn;
 import org.gbif.pipelines.ingest.pipelines.interpretation.TransformsFactory;
-import org.gbif.pipelines.keygen.HBaseLockingKey;
+import org.gbif.pipelines.transforms.core.VerbatimTransform;
 import org.slf4j.MDC;
 
 @Slf4j
@@ -70,8 +60,13 @@ public class FragmenterPipeline {
     TransformsFactory transformsFactory = TransformsFactory.create(options);
     PipelinesConfig config = transformsFactory.getConfig();
 
-    Read dwcaOccurrenceRecordReader =
-        DwcaOccurrenceRecordIO.Read.fromLocation(options.getInputPath());
+    String verbatimAvroFile = Conversion.FILE_NAME + PipelinesVariables.Pipeline.AVRO_EXTENSION;
+    String pathToAvro = PathBuilder.buildDatasetAttemptPath(options, verbatimAvroFile, true);
+
+    VerbatimTransform verbatimTransform = transformsFactory.createVerbatimTransform();
+
+    DwcaOccurrenceRecordConverterFn dwcaOccurrenceRecordConverterFn =
+        DwcaOccurrenceRecordConverterFn.create();
 
     RawRecordFn rawRecordFn =
         RawRecordFn.builder()
@@ -99,10 +94,15 @@ public class FragmenterPipeline {
             .withTableId(config.getFragmentsTable());
 
     log.info("Adding step 2: Creating pipeline steps");
+
     Pipeline p = pipelinesFn.apply(options);
 
-    p.apply("Read from Darwin Core Archive", dwcaOccurrenceRecordReader)
-        .apply("Convert to RawRecord", ParDo.of(rawRecordFn))
+    p.apply("Read verbatim", verbatimTransform.read(pathToAvro))
+        .apply(
+            "Read occurrences from extension",
+            transformsFactory.createOccurrenceExtensionTransform())
+        .apply("Convert to DwcaOccurrenceRecord", ParDo.of(dwcaOccurrenceRecordConverterFn))
+        .apply("Get keys and convert to RawRecord", ParDo.of(rawRecordFn))
         .setCoder(RawRecordCoder.of())
         .apply("Filter RawRecord", ParDo.of(rawRecordFilterFn))
         .apply("Convert to Mutation", ParDo.of(mutationConverterFn))
@@ -121,105 +121,5 @@ public class FragmenterPipeline {
     hbaseConfig.set("hbase.zookeeper.quorum", config.getZkConnectionString());
     hbaseConfig.set("zookeeper.session.timeout", "360000");
     return hbaseConfig;
-  }
-
-  private static class RawRecordFn extends DoFn<DwcaOccurrenceRecord, RawRecord> {
-    private final boolean useTriplet;
-    private final boolean useOccurrenceId;
-    private final boolean generateIdIfAbsent;
-    private final SerializableSupplier<HBaseLockingKey> keygenServiceSupplier;
-    private HBaseLockingKey keygenService;
-
-    @Builder(buildMethodName = "create")
-    private RawRecordFn(
-        boolean useTriplet,
-        boolean useOccurrenceId,
-        boolean generateIdIfAbsent,
-        SerializableSupplier<HBaseLockingKey> keygenServiceSupplier) {
-      this.useTriplet = useTriplet;
-      this.useOccurrenceId = useOccurrenceId;
-      this.generateIdIfAbsent = generateIdIfAbsent; // for IT tests
-      this.keygenServiceSupplier = keygenServiceSupplier;
-    }
-
-    /** Beam @Setup initializes resources */
-    @Setup
-    public void setup() {
-      if (keygenService == null && keygenServiceSupplier != null) {
-        keygenService = keygenServiceSupplier.get();
-      }
-    }
-
-    /** Beam @Teardown closes initialized resources */
-    @SneakyThrows
-    @Teardown
-    public void tearDown() {
-      if (keygenService != null) {
-        keygenService.close();
-      }
-    }
-
-    @ProcessElement
-    public void processElement(@Element DwcaOccurrenceRecord dor, OutputReceiver<RawRecord> out) {
-      Predicate<String> emptyValidator = s -> true;
-      OccurrenceRecordConverter.convert(
-              keygenService, emptyValidator, useTriplet, useOccurrenceId, generateIdIfAbsent, dor)
-          .ifPresent(out::output);
-    }
-  }
-
-  private static class RawRecordFilterFn extends DoFn<RawRecord, RawRecord> {
-
-    private final SerializableSupplier<Table> tableSupplier;
-    private Table table;
-
-    @Builder(buildMethodName = "create")
-    public RawRecordFilterFn(SerializableSupplier<Table> tableSupplier) {
-      this.tableSupplier = tableSupplier;
-    }
-
-    @Setup
-    public void setup() {
-      if (table == null && tableSupplier != null) {
-        table = tableSupplier.get();
-      }
-    }
-
-    /** Beam @Teardown closes initialized resources */
-    @SneakyThrows
-    @Teardown
-    public void tearDown() {
-      if (table != null) {
-        table.close();
-      }
-    }
-
-    @ProcessElement
-    public void processElement(@Element RawRecord rr, OutputReceiver<RawRecord> out) {
-      boolean isNewRawRecord = HbaseStore.isNewRawRecord(table, rr);
-      if (isNewRawRecord) {
-        HbaseStore.populateCreatedDate(table, rr);
-        out.output(rr);
-      }
-    }
-  }
-
-  @Builder(buildMethodName = "create")
-  private static class MutationConverterFn extends DoFn<RawRecord, Mutation> {
-
-    private final Counter counter = Metrics.counter(MutationConverterFn.class, FRAGMENTER_COUNT);
-
-    String datasetKey;
-    Integer attempt;
-    String protocol;
-
-    @ProcessElement
-    public void processElement(@Element RawRecord rr, OutputReceiver<Mutation> out) {
-      Put fragmentPut = HbaseStore.createFragmentPut(datasetKey, attempt, protocol, rr);
-
-      out.output(fragmentPut);
-
-      counter.inc();
-    }
   }
 }
--- gbif/ingestion/ingestion-integration-tests-hbase/src/test/java/org/gbif/pipelines/ingest/pipelines/FragmenterPipelineIT.java
+++ gbif/ingestion/ingestion-integration-tests-hbase/src/test/java/org/gbif/pipelines/ingest/pipelines/FragmenterPipelineIT.java
@@ -1,15 +1,27 @@
 package org.gbif.pipelines.ingest.pipelines;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.SneakyThrows;
 import org.apache.beam.sdk.testing.NeedsRunner;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.gbif.api.vocabulary.EndpointType;
+import org.gbif.api.vocabulary.Extension;
+import org.gbif.dwc.terms.DwcTerm;
 import org.gbif.pipelines.common.beam.options.InterpretationPipelineOptions;
 import org.gbif.pipelines.common.beam.options.PipelinesOptionsFactory;
+import org.gbif.pipelines.core.io.SyncDataFileWriter;
+import org.gbif.pipelines.ingest.java.transforms.InterpretedAvroWriter;
 import org.gbif.pipelines.ingest.resources.HbaseServer;
 import org.gbif.pipelines.ingest.resources.TableAssert;
+import org.gbif.pipelines.io.avro.ExtendedRecord;
+import org.gbif.pipelines.transforms.core.VerbatimTransform;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -26,7 +38,10 @@ public class FragmenterPipelineIT {
 
   @ClassRule public static final HbaseServer HBASE_SERVER = new HbaseServer();
 
-  private final Path regularDwca = Paths.get(getClass().getResource("/dwca/regular").getFile());
+  private static final String DATASET_KEY = "50c9509d-22c7-4a22-a47d-8c48425ef4a8";
+  private static final DwcTerm CORE_TERM = DwcTerm.Occurrence;
+  private static final String ID = "777";
+  private final Path outputFile = Paths.get(getClass().getResource("/dwca").getFile());
   private final Path properties = Paths.get(getClass().getResource("/data7/ingest").getFile());
 
   @Before
@@ -39,72 +54,164 @@ public class FragmenterPipelineIT {
 
     // State
     int expSize = 210;
-    String datasetKey = "50c9509d-22c7-4a22-a47d-8c48425ef4a8";
     int attempt = 231;
     EndpointType endpointType = EndpointType.DWC_ARCHIVE;
 
-    // When
     String[] args = {
-      "--datasetId=" + datasetKey,
+      "--datasetId=" + DATASET_KEY,
       "--attempt=" + attempt,
       "--runner=SparkRunner",
       "--metaFileName=fragmenter.yml",
-      "--inputPath=" + regularDwca,
-      "--targetPath=" + properties,
+      "--inputPath=" + outputFile,
+      "--targetPath=" + outputFile,
       "--properties=" + properties + "/pipelines.yaml",
       "--testMode=true"
     };
 
     InterpretationPipelineOptions options = PipelinesOptionsFactory.createInterpretation(args);
+    createVerbatimAvro(options, "attempt_" + attempt);
+
+    // When
     FragmenterPipeline.run(options, opt -> p);
 
     // Should
     TableAssert.assertTable(
-        HBASE_SERVER.getConnection(), expSize, datasetKey, attempt, endpointType);
+        HBASE_SERVER.getConnection(), expSize, DATASET_KEY, attempt, endpointType);
   }
 
   @Test
   public void repeatPipelineTest() throws Exception {
 
     // State
-    int expSize = 210;
-    String datasetKey = "50c9509d-22c7-4a22-a47d-8c48425ef4a8";
+    int expSize = 0;
+
     int attempt = 231;
     int attempt2 = 232;
     EndpointType endpointType = EndpointType.DWC_ARCHIVE;
 
-    // When
     String[] args = {
-        "--datasetId=" + datasetKey,
+      "--datasetId=" + DATASET_KEY,
+      "--attempt=" + attempt,
+      "--runner=SparkRunner",
+      "--metaFileName=fragmenter.yml",
+      "--targetPath=" + outputFile,
+      "--properties=" + properties + "/pipelines.yaml",
+      "--testMode=true"
+    };
+
+    InterpretationPipelineOptions options = PipelinesOptionsFactory.createInterpretation(args);
+    createVerbatimAvro(options, "attempt_" + attempt);
+
+    // When
+    FragmenterPipeline.run(options, opt -> p);
+
+    // State
+    String[] args2 = {
+      "--datasetId=" + DATASET_KEY,
+      "--attempt=" + attempt2,
+      "--runner=SparkRunner",
+      "--metaFileName=fragmenter.yml",
+      "--targetPath=" + outputFile,
+      "--properties=" + properties + "/pipelines.yaml",
+      "--testMode=true"
+    };
+
+    InterpretationPipelineOptions options2 = PipelinesOptionsFactory.createInterpretation(args2);
+
+    // When
+    FragmenterPipeline.run(options2, opt -> p);
+
+    // Should
+    TableAssert.assertTable(
+        HBASE_SERVER.getConnection(), expSize, DATASET_KEY, attempt2, endpointType);
+  }
+
+  @Test
+  public void newDataPipelineTest() throws Exception {
+
+    // State
+    int expSize = 0;
+
+    int attempt = 231;
+    int attempt2 = 232;
+    EndpointType endpointType = EndpointType.DWC_ARCHIVE;
+
+    String[] args = {
+        "--datasetId=" + DATASET_KEY,
         "--attempt=" + attempt,
         "--runner=SparkRunner",
         "--metaFileName=fragmenter.yml",
-        "--inputPath=" + regularDwca,
-        "--targetPath=" + properties,
+        "--targetPath=" + outputFile,
         "--properties=" + properties + "/pipelines.yaml",
         "--testMode=true"
     };
 
     InterpretationPipelineOptions options = PipelinesOptionsFactory.createInterpretation(args);
-    FragmenterPipeline.run(options, opt -> p);
+    createVerbatimAvro(options, "attempt_" + attempt);
 
     // When
+    FragmenterPipeline.run(options, opt -> p);
+
+    // State
     String[] args2 = {
-        "--datasetId=" + datasetKey,
+        "--datasetId=" + DATASET_KEY,
         "--attempt=" + attempt2,
         "--runner=SparkRunner",
         "--metaFileName=fragmenter.yml",
-        "--inputPath=" + regularDwca,
-        "--targetPath=" + properties,
+        "--targetPath=" + outputFile,
         "--properties=" + properties + "/pipelines.yaml",
         "--testMode=true"
     };
 
     InterpretationPipelineOptions options2 = PipelinesOptionsFactory.createInterpretation(args2);
+
+    // When
+    createVerbatimAvro(options, "attempt_" + attempt2);
+
+    // When
     FragmenterPipeline.run(options2, opt -> p);
 
     // Should
     TableAssert.assertTable(
-        HBASE_SERVER.getConnection(), expSize, datasetKey, attempt, endpointType);
+        HBASE_SERVER.getConnection(), expSize, DATASET_KEY, attempt2, endpointType);
+  }
+
+  @SneakyThrows
+  private void createVerbatimAvro(InterpretationPipelineOptions options, String value) {
+    // Create varbatim.avro
+    try (SyncDataFileWriter<ExtendedRecord> writer =
+        InterpretedAvroWriter.createAvroWriter(
+            options, VerbatimTransform.create(), CORE_TERM, ID)) {
+
+      Map<String, String> ext1 = new HashMap<>(5);
+      ext1.put(DwcTerm.measurementID.qualifiedName(), value);
+      ext1.put(DwcTerm.measurementType.qualifiedName(), value);
+      ext1.put(DwcTerm.measurementValue.qualifiedName(), value);
+      ext1.put(DwcTerm.measurementAccuracy.qualifiedName(), value);
+      ext1.put(DwcTerm.measurementUnit.qualifiedName(), value);
+
+      Map<String, List<Map<String, String>>> ext = new HashMap<>();
+      ext.put(Extension.MEASUREMENT_OR_FACT.getRowType(), Collections.singletonList(ext1));
+
+      Map<String, String> coreTerm = new HashMap<>(5);
+      coreTerm.put(DwcTerm.occurrenceID.qualifiedName(), value);
+      coreTerm.put(DwcTerm.institutionCode.qualifiedName(), value);
+      coreTerm.put(DwcTerm.collectionCode.qualifiedName(), value);
+      coreTerm.put(DwcTerm.catalogNumber.qualifiedName(), value);
+      coreTerm.put(DwcTerm.recordedBy.qualifiedName(), value);
+
+      ExtendedRecord extendedRecord =
+          ExtendedRecord.newBuilder().setCoreTerms(coreTerm).setId(ID).setExtensions(ext).build();
+      writer.append(extendedRecord);
+    }
+
+    String datasetId = options.getDatasetId();
+    String attempt = options.getAttempt().toString();
+    String outputFileString = outputFile.toString();
+    Path from =
+        Paths.get(outputFileString, datasetId, attempt, "occurrence/verbatim/interpret-777.avro");
+    Path to = Paths.get(outputFileString, datasetId, attempt, "verbatim.avro");
+    Files.deleteIfExists(to);
+    Files.move(from, to);
   }
 }
