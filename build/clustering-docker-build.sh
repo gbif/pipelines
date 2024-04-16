8ead0ad102862265e6bf08fd333fa50ee936c388
nvolik <nikolay.volik@hotmail.com>
2024-04-16 13:37:36 +0200

    #1034 Add extra logging

--- build/clustering-docker-build.sh
+++ build/clustering-docker-build.sh
@@ -1,6 +1,10 @@
 #Simple script for pushing a image containing the named modules build artifact
 MODULE="clustering-gbif"
+
+echo "Building Docker image module:version - ${MODULE}:${POM_VERSION}"
 docker build -f ./gbif/ingestion/${MODULE}/docker/Dockerfile ./gbif/ingestion/${MODULE} --build-arg JAR_FILE=${MODULE}-${POM_VERSION}-shaded.jar -t docker.gbif.org/${MODULE}:${POM_VERSION}
+
+echo "Pushing Docker image to the repository"
 docker push docker.gbif.org/${MODULE}:${POM_VERSION}
 if [[ $IS_M2RELEASEBUILD = true ]]; then
   echo "Updated latest tag poiting to the newly released docker.gbif.org/${MODULE}:${POM_VERSION}"
