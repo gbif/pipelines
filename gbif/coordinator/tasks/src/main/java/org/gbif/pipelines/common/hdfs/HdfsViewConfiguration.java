8eaea54d97043ce90858b7514b5bc90d1ee62399
Nikolay Volik <nikolay.volik@hotmail.com>
2024-04-25 15:31:17 +0200

    #1055 Lower initial file size

--- gbif/coordinator/tasks/src/main/java/org/gbif/pipelines/common/hdfs/HdfsViewConfiguration.java
+++ gbif/coordinator/tasks/src/main/java/org/gbif/pipelines/common/hdfs/HdfsViewConfiguration.java
@@ -35,7 +35,7 @@ public class HdfsViewConfiguration implements BaseConfiguration {
   public int hdfsAvroCoefficientRatio = 75;
 
   @Parameter(names = "--hdfs-avro-expected-file-size-in-mb")
-  public int hdfsAvroExpectedFileSizeInMb = 10_240; // 10GB
+  public int hdfsAvroExpectedFileSizeInMb = 2_048; // 2GB
 
   @Parameter(names = "--process-runner")
   @NotNull
