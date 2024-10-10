2e7e1fd7d2e3478e517eff2ab27eeb311ea20bb7
nvolik <nikolay.volik@hotmail.com>
2024-10-10 11:00:54 +0200

    Add IMAGE variable

--- build/ingestion-docker-build.sh
+++ build/ingestion-docker-build.sh
@@ -2,20 +2,21 @@
 MODULE="ingest-gbif-beam"
 
 POM_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
+IMAGE=docker.gbif.org/${MODULE}:${POM_VERSION}
 
-echo "Building Docker image: docker.gbif.org/${MODULE}:${POM_VERSION}"
-docker build -f ./gbif/ingestion/${MODULE}/docker/Dockerfile ./gbif/ingestion/${MODULE} --build-arg JAR_FILE=${MODULE}-${POM_VERSION}-shaded.jar -t docker.gbif.org/${MODULE}:${POM_VERSION}
+echo "Building Docker image: ${IMAGE}"
+docker build -f ./gbif/ingestion/${MODULE}/docker/Dockerfile ./gbif/ingestion/${MODULE} --build-arg JAR_FILE=${MODULE}-${POM_VERSION}-shaded.jar -t ${IMAGE}
 
 echo "Pushing Docker image to the repository"
-docker push docker.gbif.org/${MODULE}:${POM_VERSION}
+docker push ${IMAGE}
 if [[ $IS_M2RELEASEBUILD = true ]]; then
-  echo "Updated latest tag poiting to the newly released docker.gbif.org/${MODULE}:${POM_VERSION}"
-  docker tag docker.gbif.org/${MODULE}:${POM_VERSION} docker.gbif.org/${MODULE}:latest
+  echo "Updated latest tag poiting to the newly released ${IMAGE}"
+  docker tag ${IMAGE} docker.gbif.org/${MODULE}:latest
   docker push docker.gbif.org/${MODULE}:latest
 fi
 
-echo "Removing local Docker image: docker.gbif.org/${MODULE}:${POM_VERSION}"
-docker rmi docker.gbif.org/${MODULE}:${POM_VERSION}
+echo "Removing local Docker image: ${IMAGE}"
+docker rmi ${IMAGE}
 
 if [[ $IS_M2RELEASEBUILD = true ]]; then
   echo "Removing local Docker image with latest tag: docker.gbif.org/${MODULE}:latest"
