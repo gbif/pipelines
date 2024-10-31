ab1459afac3d60018c922885dcd3b8788953ce48
Matthew Blissett <matt@blissett.me.uk>
2024-10-31 10:56:37 +0100

    Align DWCA metadata location more closely with dwca-io library.
    Part of https://github.com/gbif/portal-feedback/issues/5543

--- gbif/validator/validator-eml/src/main/java/org/gbif/dwca/validation/MetadataPath.java
+++ gbif/validator/validator-eml/src/main/java/org/gbif/dwca/validation/MetadataPath.java
@@ -3,58 +3,52 @@ package org.gbif.dwca.validation;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MetadataPath {
 
-  private static final String DEFUALT_NAME = "eml.xml";
-  private static final String DEFUALT_NAME_UPPER = "EML.xml";
-  private static final Pattern PATTERN = Pattern.compile("(metadata=\")(\\w+.xml)(\")");
+  private static final String DEFAULT_NAME = "eml.xml";
+  private static final String DEFAULT_NAME_UPPER = "metadata.xml";
+  private static final Pattern PATTERN = Pattern.compile("(metadata=[\"'])([^\"']+)([\"'])", Pattern.CASE_INSENSITIVE);
 
-  /** Scans root directory for xml files and parses and gets value from metadata tag */
+  /** Attempt to read the DWCA meta.xml file for the metadata location, but otherwise try the two
+   * defaults used by the dwca-io library. */
   public static Optional<Path> parsePath(Path rootDirectory) throws IOException {
-    try (Stream<Path> stream =
-        Files.find(
-            rootDirectory, 1, (path, basicFileAttributes) -> path.toString().endsWith(".xml"))) {
+    // Look for metadata at the location specified in meta.xml, and at eml.xml and metadata.xml.
+    Path metaXml = rootDirectory.resolve("meta.xml");
+    if (Files.exists(metaXml)) {
 
-      List<Path> xmlPaths = stream.collect(Collectors.toList());
-      for (Path path : xmlPaths) {
+      Optional<Path> fromMetaXml =
+        Files.readAllLines(metaXml).stream()
+          .filter(lines -> PATTERN.matcher(lines).find())
+          .findFirst()
+          .map(
+            str -> {
+              Matcher matcher = PATTERN.matcher(str);
+              return matcher.find() ? matcher.group(2) : "";
+            })
+          .map(rootDirectory::resolve);
 
-        Optional<Path> first =
-            Files.readAllLines(path).stream()
-                .filter(lines -> PATTERN.matcher(lines).find())
-                .findFirst()
-                .map(
-                    str -> {
-                      Matcher matcher = PATTERN.matcher(str);
-                      return matcher.find() ? matcher.group(2) : "";
-                    })
-                .map(rootDirectory::resolve);
-
-        if (first.isPresent()) {
-          return first;
-        }
+      if (fromMetaXml.isPresent()) {
+        return fromMetaXml;
       }
-
-      Path defualtNamePath = rootDirectory.resolve(DEFUALT_NAME);
-      if (Files.exists(defualtNamePath)) {
-        return Optional.of(defualtNamePath);
-      }
-
-      Path defualtUpperNamePath = rootDirectory.resolve(DEFUALT_NAME_UPPER);
-      if (Files.exists(defualtUpperNamePath)) {
-        return Optional.of(defualtUpperNamePath);
-      }
-
-      return Optional.empty();
     }
+
+    Path defaultNamePath = rootDirectory.resolve(DEFAULT_NAME);
+    if (Files.exists(defaultNamePath)) {
+      return Optional.of(defaultNamePath);
+    }
+
+    Path defaultUpperNamePath = rootDirectory.resolve(DEFAULT_NAME_UPPER);
+    if (Files.exists(defaultUpperNamePath)) {
+      return Optional.of(defaultUpperNamePath);
+    }
+
+    return Optional.empty();
   }
 }
