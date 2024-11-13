2ec053cca3608eaf263220a4230d523bd5d4dcf9
Stefan Van Dyck <vandyck.stefan@gmail.com>
2024-11-13 08:30:36 +0100

    inbo/vlaams-biodiversiteitsportaal#61: Use relative paths to allow using path-based routing for services

--- livingatlas/pipelines/src/main/java/au/org/ala/specieslists/SpeciesListService.java
+++ livingatlas/pipelines/src/main/java/au/org/ala/specieslists/SpeciesListService.java
@@ -7,9 +7,9 @@ import retrofit2.http.Path;
 
 public interface SpeciesListService {
 
-  @GET("/ws/speciesList?isAuthoritative=eq:true&max=1000")
+  @GET("ws/speciesList?isAuthoritative=eq:true&max=1000")
   Call<ListSearchResponse> getAuthoritativeLists();
 
-  @GET("/speciesListItem/downloadList/{dataResourceUid}?fetch=%7BkvpValues%3Dselect%7")
+  @GET("speciesListItem/downloadList/{dataResourceUid}?fetch=%7BkvpValues%3Dselect%7")
   Call<ResponseBody> downloadList(@Path("dataResourceUid") String dataResourceUid);
 }
