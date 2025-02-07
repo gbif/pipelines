3ead8c9ba067429812f5ac89760a3e5ea15c3276
Dave Martin <djtfmartin@users.noreply.github.com>
2025-02-07 17:38:53 +0000

    merge 3.0.5 changes from master (#1118)
    * Use singltone VocabularyService to avoid memory issues
    
    * sex vocab prefilter not to remove zeros
    
    * Apply spotless
    
    * Add extra logging
    
    * Fix extra logging
    
    * Improve Retry function to avoid count some issues as retry exceptions
    
    * Remove Retry waitDuration in favor of intervalFunction
    
    * Skip error if file doesn't exist for cases when old export data is used
    
    * Don't fail if errors occure during releasing
    
    * [maven-release-plugin] prepare release pipelines-parent-3.0.2
    
    * Remove withoutSharding to improve writing speed
    
    * Bump the project version
    
    * build fix with change from dev
    
    * [maven-release-plugin] prepare release pipelines-parent-3.0.3
    
    * [maven-release-plugin] prepare for next development iteration
    
    * reverted FileVocabularyFactory changes from the dev branch merge
    
    * [maven-release-plugin] prepare release pipelines-parent-3.0.5
    
    * [maven-release-plugin] prepare for next development iteration
    
    ---------
    
    Co-authored-by: nvolik <nikolay.volik@hotmail.com>
    Co-authored-by: Marcos Lopez Gonzalez <marcoslg@gmail.com>
    Co-authored-by: gbif-jenkins <dev@gbif.org>

--- sdks/beam-transforms/src/main/java/org/gbif/pipelines/factory/FileVocabularyFactory.java
+++ sdks/beam-transforms/src/main/java/org/gbif/pipelines/factory/FileVocabularyFactory.java
@@ -6,7 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
-import lombok.Builder;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.fs.FileSystem;
@@ -30,11 +29,37 @@ import org.gbif.vocabulary.lookup.VocabularyLookup;
  * vocabulary.
  */
 @Slf4j
-@Builder
 public class FileVocabularyFactory implements Serializable {
 
-  private final PipelinesConfig config;
-  private final HdfsConfigs hdfsConfigs;
+  private final VocabularyService vocabularyService;
+  private static volatile FileVocabularyFactory instance;
+  private static final Object MUTEX = new Object();
+
+  @SneakyThrows
+  private FileVocabularyFactory(HdfsConfigs hdfsConfigs, PipelinesConfig config) {
+    this.vocabularyService = getVocabularyService(hdfsConfigs, config);
+  }
+
+  public static VocabularyService getInstance(HdfsConfigs hdfsConfigs, PipelinesConfig config) {
+    if (instance == null) {
+      synchronized (MUTEX) {
+        if (instance == null) {
+          instance = new FileVocabularyFactory(hdfsConfigs, config);
+        }
+      }
+    }
+    return instance.vocabularyService;
+  }
+
+  public static SerializableSupplier<VocabularyService> createSupplier(
+      HdfsConfigs hdfsConfigs, PipelinesConfig config) {
+    return () -> new FileVocabularyFactory(hdfsConfigs, config).vocabularyService;
+  }
+
+  public static SerializableSupplier<VocabularyService> getInstanceSupplier(
+      HdfsConfigs hdfsConfigs, PipelinesConfig config) {
+    return () -> FileVocabularyFactory.getInstance(hdfsConfigs, config);
+  }
 
   /**
    * Creates instances of {@link VocabularyLookup} from a file containing an exported vocabulary.
@@ -47,35 +72,33 @@ public class FileVocabularyFactory implements Serializable {
    *       the number characters that are present at the beginning of the value.
    * </ul>
    */
-  public SerializableSupplier<VocabularyService> getInstanceSupplier() {
+  private VocabularyService getVocabularyService(
+      HdfsConfigs hdfsConfigs, PipelinesConfig pipelinesConfig) {
+    VocabularyConfig vocabularyConfig = requireNonNull(pipelinesConfig.getVocabularyConfig());
+    String path = vocabularyConfig.getVocabulariesPath();
 
-    return () -> {
-      VocabularyConfig vocabularyConfig = requireNonNull(config.getVocabularyConfig());
-      String path = vocabularyConfig.getVocabulariesPath();
+    VocabularyServiceBuilder serviceBuilder = VocabularyService.builder();
 
-      VocabularyServiceBuilder serviceBuilder = VocabularyService.builder();
-
-      vocabularyConfig
-          .getVocabulariesNames()
-          .forEach(
-              (term, name) -> {
-                try (InputStream is = readFile(hdfsConfigs, path, name)) {
-                  InMemoryVocabularyLookupBuilder builder =
-                      InMemoryVocabularyLookup.newBuilder().from(is);
-                  if (term.equals(DwcTerm.lifeStage.qualifiedName())) {
-                    builder.withPrefilter(PreFilters.REMOVE_NUMERIC_PREFIX);
-                  } else if (term.equals(DwcTerm.sex.qualifiedName())) {
-                    builder.withPrefilter(PreFilters.REMOVE_POSITIVE_NUMERIC);
-                  }
-
-                  serviceBuilder.vocabularyLookup(term, builder.build());
-                } catch (IOException ex) {
-                  throw new PipelinesException(ex);
+    vocabularyConfig
+        .getVocabulariesNames()
+        .forEach(
+            (term, name) -> {
+              try (InputStream is = readFile(hdfsConfigs, path, name)) {
+                InMemoryVocabularyLookupBuilder builder =
+                    InMemoryVocabularyLookup.newBuilder().from(is);
+                if (term.equals(DwcTerm.lifeStage.qualifiedName())) {
+                  builder.withPrefilter(PreFilters.REMOVE_NUMERIC_PREFIX);
+                } else if (term.equals(DwcTerm.sex.qualifiedName())) {
+                  builder.withPrefilter(PreFilters.REMOVE_POSITIVE_NUMERIC);
                 }
-              });
 
-      return serviceBuilder.build();
-    };
+                serviceBuilder.vocabularyLookup(term, builder.build());
+              } catch (IOException ex) {
+                throw new PipelinesException(ex);
+              }
+            });
+
+    return serviceBuilder.build();
   }
 
   /**
