d6f0b88b2d9aac276da25928e82e1303c771b02f
Dave Martin <djtfmartin@gmail.com>
2025-07-02 22:50:57 +0100

    spotless

--- gbif/validator/validator-ws-client/src/main/java/org/gbif/validator/ws/client/ValidationWsClient.java
+++ gbif/validator/validator-ws-client/src/main/java/org/gbif/validator/ws/client/ValidationWsClient.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.time.Duration;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
 import org.gbif.api.model.common.paging.PagingResponse;
 import org.gbif.api.model.registry.Dataset;
