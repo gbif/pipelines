361acf8ad2b2cc82eed3379e2a5ffac225d8d20c
Dave Martin <djtfmartin@gmail.com>
2025-11-14 20:53:09 +0000

    spotless

--- sdks/core/src/main/java/org/gbif/pipelines/core/converters/OccurrenceHdfsRecordConverter.java
+++ sdks/core/src/main/java/org/gbif/pipelines/core/converters/OccurrenceHdfsRecordConverter.java
@@ -353,7 +353,10 @@ public class OccurrenceHdfsRecordConverter {
             .collect(
                 Collectors.toMap(
                     TaxonRecord::getDatasetKey,
-                    tr -> tr.getIssues() != null && tr.getIssues().getIssueList() != null ? tr.getIssues().getIssueList() : List.of())));
+                    tr ->
+                        tr.getIssues() != null && tr.getIssues().getIssueList() != null
+                            ? tr.getIssues().getIssueList()
+                            : List.of())));
 
     occurrenceHdfsRecord.setClassificationdetails(
         multiTaxonRecord.getTaxonRecords().stream()
