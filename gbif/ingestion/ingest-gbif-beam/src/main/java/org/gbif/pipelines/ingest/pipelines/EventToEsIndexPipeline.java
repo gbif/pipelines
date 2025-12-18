b5ef4697ac04167a1022e7e3aabb60fd4a7ffa0e
Marcos Lopez Gonzalez <marcoslg@gmail.com>
2025-12-18 15:08:36 +0100

    Revert "Remove unused event multi-taxon records processing from DerivedMetadat"
    This reverts commit 60dae98eb98480de8331a71daaaddedcda8b73a2.

--- gbif/ingestion/ingest-gbif-beam/src/main/java/org/gbif/pipelines/ingest/pipelines/EventToEsIndexPipeline.java
+++ gbif/ingestion/ingest-gbif-beam/src/main/java/org/gbif/pipelines/ingest/pipelines/EventToEsIndexPipeline.java
@@ -201,6 +201,10 @@ public class EventToEsIndexPipeline {
         p.apply("Read Event Location", locationTransform.read(pathFn))
             .apply("Map Location to KV", locationTransform.toKv());
 
+    PCollection<KV<String, MultiTaxonRecord>> taxonCollection =
+        p.apply("Read event multi-taxon records", multiTaxonomyTransform.readIfExists(pathFn))
+            .apply("Map event multi-taxon records to KV", multiTaxonomyTransform.toKv());
+
     InheritedFields inheritedFields =
         InheritedFields.builder()
             .inheritedFieldsTransform(InheritedFieldsTransform.builder().build())
@@ -233,6 +237,7 @@ public class EventToEsIndexPipeline {
             .verbatimCollection(verbatimCollection)
             .temporalCollection(temporalCollection)
             .locationCollection(locationCollection)
+            .multiTaxonCollection(taxonCollection)
             .eventCoreCollection(eventCoreCollection)
             .occurrencesPathFn(occurrencesPathFn)
             .datasetHasOccurrences(datasetHasOccurrences)
@@ -363,6 +368,7 @@ public class EventToEsIndexPipeline {
     private final PCollection<KV<String, ExtendedRecord>> verbatimCollection;
     private final PCollection<KV<String, TemporalRecord>> temporalCollection;
     private final PCollection<KV<String, LocationRecord>> locationCollection;
+    private final PCollection<KV<String, MultiTaxonRecord>> multiTaxonCollection;
     private final PCollection<KV<String, EventCoreRecord>> eventCoreCollection;
     private final UnaryOperator<String> occurrencesPathFn;
     private final boolean datasetHasOccurrences;
@@ -448,7 +454,16 @@ public class EventToEsIndexPipeline {
                   "Create empty eventOccurrencesMultiTaxonCollection",
                   Create.empty(new TypeDescriptor<KV<String, MultiTaxonRecord>>() {}));
 
-      return PCollectionList.of(eventOccurrencesTaxonCollection)
+      PCollection<KV<String, MultiTaxonRecord>> multiTaxonRecordsOfSubEvents =
+          ParentEventExpandTransform.createTaxonTransform(
+                  multiTaxonomyTransform.getTag(),
+                  eventCoreTransform.getTag(),
+                  multiTaxonomyTransform.getEdgeTag())
+              .toSubEventsRecords("MultiTaxon", multiTaxonCollection, eventCoreCollection);
+
+      return PCollectionList.of(multiTaxonCollection)
+          .and(eventOccurrencesTaxonCollection)
+          .and(multiTaxonRecordsOfSubEvents)
           .apply("Join event and occurrence taxon records", Flatten.pCollections())
           .apply("Select a sample of taxon records", Sample.fixedSizePerKey(MAX_TAXON_PER_EVENTS));
     }
