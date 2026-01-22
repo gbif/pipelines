59652158adb12f5601b70b78fcd5d58545da6a0d
Marcos Lopez Gonzalez <marcoslg@gmail.com>
2026-01-22 13:37:22 +0100

    Add EventsMessageHandler to process and send PipelinesEventsMessage

---
+++ gbif/coordinator/tasks/src/main/java/org/gbif/pipelines/tasks/balancer/handler/EventsMessageHandler.java
@@ -0,0 +1,76 @@
+package org.gbif.pipelines.tasks.balancer.handler;
+
+import static org.gbif.pipelines.common.ValidatorPredicate.isValidator;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.gbif.api.model.pipelines.InterpretationType.RecordType;
+import org.gbif.api.model.pipelines.StepRunner;
+import org.gbif.api.vocabulary.DatasetType;
+import org.gbif.common.messaging.api.MessagePublisher;
+import org.gbif.common.messaging.api.messages.PipelinesBalancerMessage;
+import org.gbif.common.messaging.api.messages.PipelinesEventsMessage;
+import org.gbif.common.messaging.api.messages.PipelinesInterpretedMessage;
+import org.gbif.pipelines.common.PipelinesVariables.Pipeline;
+import org.gbif.pipelines.common.PipelinesVariables.Pipeline.Conversion;
+import org.gbif.pipelines.common.configs.StepConfiguration;
+import org.gbif.pipelines.common.utils.HdfsUtils;
+import org.gbif.pipelines.core.pojo.HdfsConfigs;
+import org.gbif.pipelines.tasks.balancer.BalancerConfiguration;
+
+/**
+ * Populates and sends the {@link PipelinesInterpretedMessage} message, the main method is {@link
+ * EventsMessageHandler#handle}
+ */
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EventsMessageHandler {
+
+  private static final ObjectMapper MAPPER = new ObjectMapper();
+
+  /** Main handler, basically computes the runner type and sends to the same consumer */
+  public static void handle(
+      BalancerConfiguration config, MessagePublisher publisher, PipelinesBalancerMessage message)
+      throws IOException {
+
+    log.info("Process PipelinesEventsMessage - {}", message);
+
+    PipelinesEventsMessage m =
+      MAPPER.readValue(message.getPayload(), PipelinesEventsMessage.class);
+
+    // TODO: Remove and control message sending via RabbitMQ routing
+    if (config.stepConfig.eventsEnabled
+        && m.getDatasetType() == DatasetType.SAMPLING_EVENT
+        && !isValidator(m.getPipelineSteps())) {
+
+      Set<String> interpretationTypes = new HashSet<>(m.getInterpretTypes());
+      interpretationTypes.add(RecordType.EVENT.name());
+      interpretationTypes.remove(RecordType.OCCURRENCE.name());
+
+      PipelinesEventsMessage eventsMessage =
+        new PipelinesEventsMessage(
+          m.getDatasetUuid(),
+          m.getAttempt(),
+          m.getPipelineSteps(),
+          m.getNumberOfEventRecords(),
+          m.getNumberOfOccurrenceRecords(),
+          StepRunner.DISTRIBUTED.name(),
+          m.isRepeatAttempt(),
+          m.getResetPrefix(),
+          m.getExecutionId(),
+          m.getEndpointType(),
+          m.getValidationResult(),
+          interpretationTypes,
+          DatasetType.SAMPLING_EVENT);
+
+      publisher.send(eventsMessage);
+      log.info("The events message has been sent - {}", eventsMessage);
+    }
+  }
+}
