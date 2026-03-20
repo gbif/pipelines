8c94865f03339c9e7d44012cf5d90a455ba744ed
Dave Martin <djtfmartin@gmail.com>
2026-03-20 18:42:41 +0000

    change in the distinct coordinates

--- gbif/ingestion/spark-jobs/src/main/java/org/gbif/pipelines/spark/util/DerivedMetadataUtil.java
+++ gbif/ingestion/spark-jobs/src/main/java/org/gbif/pipelines/spark/util/DerivedMetadataUtil.java
@@ -643,7 +643,7 @@ public class DerivedMetadataUtil implements Serializable {
 
     log.info("distinct coordinates - union-ed {}", unioned.count());
 
-      log.info("distinct coordinates - unioned {}", unioned.count());
+    log.info("distinct coordinates - unioned {}", unioned.count());
 
     // drop duplicates using explicit columns (faster/clearer than distinct())
     Dataset<EventCoordinate> eventIdEventCoordinates =
