b2e519bd35473b1fa9cb371db0d4beb63aae348c
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2026-04-14 09:53:54 +0200

    #1257 Empty callback

---
+++ gbif/ingestion/spark-coordinator/src/main/java/org/gbif/pipelines/coordinator/ValidatorIndexingCallback.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gbif.pipelines.coordinator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.sql.SparkSession;
+import org.gbif.api.model.pipelines.StepType;
+import org.gbif.common.messaging.api.MessageCallback;
+import org.gbif.common.messaging.api.MessagePublisher;
+import org.gbif.common.messaging.api.messages.PipelinesIndexedMessage;
+import org.gbif.common.messaging.api.messages.PipelinesInterpretedMessage;
+import org.gbif.pipelines.core.config.model.PipelinesConfig;
+import org.gbif.pipelines.spark.IndexingPipeline;
+
+@Slf4j
+public class ValidatorIndexingCallback
+    extends PipelinesCallback<PipelinesInterpretedMessage, PipelinesIndexedMessage>
+    implements MessageCallback<PipelinesInterpretedMessage> {
+
+  public ValidatorIndexingCallback(
+      PipelinesConfig pipelinesConfig, MessagePublisher publisher, String master) {
+    super(pipelinesConfig, publisher, master);
+  }
+
+  @Override
+  protected StepType getStepType() {
+    return StepType.INTERPRETED_TO_INDEX;
+  }
+
+  @Override
+  protected void configSparkSession(SparkSession.Builder sparkBuilder, PipelinesConfig config) {
+    IndexingPipeline.configSparkSession(sparkBuilder, config);
+  }
+
+  @Override
+  protected void runPipeline(PipelinesInterpretedMessage message) throws Exception {
+    // TODO: implement
+  }
+
+  @Override
+  protected String getMetaFileName() {
+    return IndexingPipeline.METRICS_FILENAME;
+  }
+
+  @Override
+  public Class<PipelinesInterpretedMessage> getMessageClass() {
+    return PipelinesInterpretedMessage.class;
+  }
+
+  @Override
+  public PipelinesIndexedMessage createOutgoingMessage(PipelinesInterpretedMessage message) {
+    // TODO: implement
+    return null;
+  }
+}
