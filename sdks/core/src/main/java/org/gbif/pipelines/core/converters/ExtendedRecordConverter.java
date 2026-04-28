a7921fd87780100eac9f60141442d7f197bcdef4
timrobertson100 <timrobertson100@gmail.com>
2026-04-28 16:54:40 +0200

    #1344 avoid building broken avro

--- sdks/core/src/main/java/org/gbif/pipelines/core/converters/ExtendedRecordConverter.java
+++ sdks/core/src/main/java/org/gbif/pipelines/core/converters/ExtendedRecordConverter.java
@@ -39,7 +39,8 @@ public class ExtendedRecordConverter {
   public static ExtendedRecord from(Record core, Map<Term, List<Record>> extensions) {
     ExtendedRecord.Builder builder = ExtendedRecord.newBuilder();
     Optional.ofNullable(core.rowType()).ifPresent(x -> builder.setCoreRowType(x.qualifiedName()));
-    builder.setCoreTerms(convertToMap(core));
+    Map<String, String> coreTerms = convertToMap(core);
+    builder.setCoreTerms(coreTerms);
     builder.setExtensions(
         extensions.entrySet().stream()
             .collect(
@@ -52,20 +53,19 @@ public class ExtendedRecordConverter {
                     (a, b) -> a,
                     LinkedHashMap::new)));
 
-    builder.setId(getId(core, builder));
+    builder.setId(getId(core, coreTerms));
     return builder.build();
   }
 
   /** If id is null, use triplet as an id */
-  private static String getId(Record core, ExtendedRecord.Builder builder) {
+  private static String getId(Record core, Map<String, String> coreTerms) {
     if (StringUtils.isNotBlank(core.id())) {
       return core.id();
     }
 
-    ExtendedRecord partial = builder.build(); // build the partial record to access core terms
-    String institutionCode = partial.getCoreTerms().get(DwcTerm.institutionCode.qualifiedName());
-    String collectionCode = partial.getCoreTerms().get(DwcTerm.collectionCode.qualifiedName());
-    String catalogNumber = partial.getCoreTerms().get(DwcTerm.catalogNumber.qualifiedName());
+    String institutionCode = coreTerms.get(DwcTerm.institutionCode.qualifiedName());
+    String collectionCode = coreTerms.get(DwcTerm.collectionCode.qualifiedName());
+    String catalogNumber = coreTerms.get(DwcTerm.catalogNumber.qualifiedName());
 
     if (institutionCode == null || collectionCode == null || catalogNumber == null) {
       return RECORD_ID_ERROR;
