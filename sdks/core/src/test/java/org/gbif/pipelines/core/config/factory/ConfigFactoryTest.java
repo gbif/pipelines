83bf1ac2866469956ba83986ab9d764d4593a269
Dave Martin <djtfmartin@gmail.com>
2026-05-28 17:00:38 +0100

    chore(pipelines): separate module for config #1388

--- sdks/core/src/test/java/org/gbif/pipelines/core/config/factory/ConfigFactoryTest.java
+++ sdks/core/src/test/java/org/gbif/pipelines/core/config/factory/ConfigFactoryTest.java
@@ -7,13 +7,11 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Set;
 import org.gbif.common.parsers.date.DateComponentOrdering;
-import org.gbif.dwc.terms.DwcTerm;
 import org.gbif.pipelines.core.config.model.ContentConfig;
 import org.gbif.pipelines.core.config.model.KeygenConfig;
 import org.gbif.pipelines.core.config.model.KvConfig;
 import org.gbif.pipelines.core.config.model.LockConfig;
 import org.gbif.pipelines.core.config.model.PipelinesConfig;
-import org.gbif.pipelines.core.config.model.VocabularyConfig;
 import org.gbif.pipelines.core.config.model.WsConfig;
 import org.junit.Test;
 
@@ -116,10 +114,5 @@ public class ConfigFactoryTest {
     assertTrue(
         extensionsAllowedForVerbatimSet.contains(
             "http://data.ggbn.org/schemas/ggbn/terms/Amplification"));
-
-    VocabularyConfig vocabularyConfig = config.getVocabularyConfig();
-    assertEquals(
-        "degreeOfEstablishment",
-        vocabularyConfig.getVocabularyFileName(DwcTerm.degreeOfEstablishment));
   }
 }
