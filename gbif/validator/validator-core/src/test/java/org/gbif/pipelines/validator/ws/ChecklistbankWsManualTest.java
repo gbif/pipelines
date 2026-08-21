afe0e2fa8a2bd886bc30a88d7deda9cf815906af
Marcos Lopez Gonzalez <marcoslg@gmail.com>
2026-08-21 16:21:21 +0200

    fixed tests

--- gbif/validator/validator-core/src/test/java/org/gbif/pipelines/validator/ws/ChecklistbankWsManualTest.java
+++ gbif/validator/validator-core/src/test/java/org/gbif/pipelines/validator/ws/ChecklistbankWsManualTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.UUID;
 import org.gbif.pipelines.validator.ChecklistValidator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +14,8 @@ public class ChecklistbankWsManualTest {
   @Test
   public void wsManualTest() throws IOException {
     ChecklistValidator checklistValidator =
-        new ChecklistValidator("https://api.dev.checklistbank.org", "user", "pwd", "");
+        new ChecklistValidator(
+            "https://api.dev.checklistbank.org", "user", "pwd", "http://test.com");
 
     int datasetKey =
         checklistValidator.submitValidation(
@@ -21,5 +23,7 @@ public class ChecklistbankWsManualTest {
                 ClassLoader.getSystemResource("checklists/archive_without_extensions.zip")
                     .getFile()),
             UUID.randomUUID());
+
+    Assertions.assertTrue(datasetKey > 0);
   }
 }
